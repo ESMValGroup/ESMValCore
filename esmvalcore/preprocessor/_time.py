31226e4e98b2cd9ad27d84e18a49f8c8dc436633
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-06-18 10:59:20 +0200

    Fix format

--- esmvalcore/preprocessor/_time.py
+++ esmvalcore/preprocessor/_time.py
@@ -453,7 +453,7 @@ def decadal_statistics(cube, operator='mean'):
     ----------
     cube: iris.cube.Cube
         input cube.
-        
+
     operator: str, optional
         Select operator to apply.
         Available operators: 'mean', 'median', 'std_dev', 'variance', 'min',
