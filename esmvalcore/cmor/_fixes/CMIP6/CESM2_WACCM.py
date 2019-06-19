b347947e132d828a773ec519282621540955696c
Manuel Schlund <manuel.schlund@dlr.de>
2019-06-19 17:53:48 +0200

    Added fixes for CMIP6 models

---
+++ esmvalcore/cmor/_fixes/CMIP6/CESM2_WACCM.py
@@ -0,0 +1,23 @@
+"""Fixes for CESM2-WACCM model."""
+from ..fix import Fix
+from . import add_height_coord
+
+
+class tas(Fix):
+    """Fixes for tas."""
+
+    def fix_metadata(self, cubes):
+        """Add height (2m) coordinate.
+
+        Parameters
+        ----------
+        cube : iris.cube.CubeList
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        cube = self.get_cube_from_list(cubes)
+        add_height_coord(cube)
+        return cubes
