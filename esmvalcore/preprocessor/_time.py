5d35366ebe1cadd0e5b9a7c630c29c118bfc8bc4
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-06-19 17:27:47 +0200

    Update esmvalcore/preprocessor/_time.py
    Co-Authored-By: Bouwe Andela <bouweandela@users.noreply.github.com>

--- esmvalcore/preprocessor/_time.py
+++ esmvalcore/preprocessor/_time.py
@@ -443,7 +443,7 @@ def annual_statistics(cube, operator='mean'):
 
 def decadal_statistics(cube, operator='mean'):
     """
-    Compute annual or decadal statistics.
+    Compute decadal statistics.
 
     Note that this function does not weight the annual or decadal mean if
     uneven time periods are present. Ie, all data inside the year/decade
