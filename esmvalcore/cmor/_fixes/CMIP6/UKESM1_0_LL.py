6afa21e310c3af816cd8ed378d6c48cc9394c615
Valeriu Predoi <valeriu.predoi@gmail.com>
2019-06-19 15:22:04 +0100

    added fix file for ULESM1-00-LL mrsos

---
+++ esmvalcore/cmor/_fixes/CMIP6/UKESM1_0_LL.py
@@ -0,0 +1,28 @@
+# pylint: disable=invalid-name, no-self-use, too-few-public-methods
+"""Fixes for UKESM1-0-LL model."""
+from ..fix import Fix
+
+
+class mrsos(Fix):
+    """Fixes for mrsos."""
+
+    def fix_metadata(self, cube):
+        """
+        Fix metadata.
+
+        Fixes wrong standard name.
+        Original error: mrsos: standard_name should be
+        moisture_content_of_soil_layer,
+        not mass_content_of_water_in_soil_layer.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        cube.standard_name = 'moisture_content_of_soil_layer'
+        return cube
