78031050da970921364200e289a700fd7ad33c2c
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-06-21 18:11:04 +0200

    Fix more docstrings

--- tests/integration/cmor/_fixes/primavera/test_ec_earth3_hr.py
+++ tests/integration/cmor/_fixes/primavera/test_ec_earth3_hr.py
@@ -1,4 +1,4 @@
-"""Test for the fixes for EC-Earth3-HR model from PRIMAVERA project"""
+"""Test for the fixes for EC-Earth3-HR model from PRIMAVERA project."""
 import unittest
 
 from iris.coords import DimCoord
@@ -8,9 +8,10 @@ from esmvalcore.cmor._fixes.primavera.ec_earth3_hr import AllVars
 
 
 class TestAllVars(unittest.TestCase):
-    """Test for the common fixes for all the variables"""
+    """Test for the common fixes for all the variables."""
+
     def setUp(self):
-        """Prepare to test"""
+        """Prepare to test."""
         self.cube = Cube([[1.0, 2.0], [3.0, 4.0]], var_name='var')
         self.cube.add_dim_coord(
             DimCoord([1.0, 2.0], standard_name='latitude',
@@ -22,7 +23,7 @@ class TestAllVars(unittest.TestCase):
 
     def test_fix_lat_lon_names(self):
         """
-        Test latitude and longitude var names
+        Test latitude and longitude var names.
 
         They should be lat and lon instead of the original latitude and
         longitude
