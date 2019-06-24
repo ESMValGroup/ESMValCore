b42ccdfd8c4c7f14ad953607094c3f9e50008f69
Björn Brötz <bjoernbroetz@users.noreply.github.com>
2019-06-24 17:33:26 +0200

    Update conf.py

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -31,9 +31,9 @@ from esmvalcore import __version__
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 # This is used for linking and such so we link to the thing we're building
-rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
-if rtd_version not in ["docu_join", "latest"]:
-    rtd_version = "docu_join"
+# rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+# if rtd_version not in ["docu_join", "latest"]:
+rtd_version = "docu_join"
 
 # -- General configuration ------------------------------------------------
 
