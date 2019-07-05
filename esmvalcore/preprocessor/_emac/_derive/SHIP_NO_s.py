7722d498c822cfccd243d07822427f2d38033cef
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 14:26:20 +0200

    Update SHIP_NO_s.py

--- esmvalcore/preprocessor/_emac/_derive/SHIP_NO_s.py
+++ esmvalcore/preprocessor/_emac/_derive/SHIP_NO_s.py
@@ -7,4 +7,4 @@ from . import var_name_constraint
 def derive(cubes):
 	output_cube = cubes.extract_strict(var_name_constraint('SHIP_NO'))
 	output_cube = output_cube.collapsed(['level'],iris.analysis.SUM)
-return output_cube
+	return output_cube
