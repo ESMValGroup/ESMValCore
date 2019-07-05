1df5cb647eca5e0fd0de5b722f7bde8b0cd3554f
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 13:58:38 +0200

    Update np.py

--- esmvalcore/preprocessor/_emac/_derive/np.py
+++ esmvalcore/preprocessor/_emac/_derive/np.py
@@ -9,8 +9,8 @@ def derive(cubes):
 	NOxic_ave_cube = cubes.extract_strict(var_name_constraint('NOxic_ave')
 	Noxcg_ave_sum = NOxcg_ave_cube.collapsed(['longitude','latitude'],iris.analysis.SUM)
 	Noxic_ave_sum = NOxic_ave_cube.collapsed(['longitude','latitude'],iris.analysis.SUM)
-	dt = cubes.attributes['dt']
-        np_cube = (Noxcg_ave_sum+ Noxic_ave_sum)/dt*65*24*3600*1e9
+	dt = cubes.attributes['GCM_timestep']
+    np_cube = (Noxcg_ave_sum+ Noxic_ave_sum)/dt*65*24*3600*1e9
     
     return np_cube
 
