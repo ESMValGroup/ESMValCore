c25dba99cadcb6f91e3acce5324c73b915c2115b
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 17:28:46 +0200

    Update ANTHNT_AER_TC_s.py

--- esmvalcore/preprocessor/_emac/_derive/ANTHNT_AER_TC_s.py
+++ esmvalcore/preprocessor/_emac/_derive/ANTHNT_AER_TC_s.py
@@ -5,6 +5,10 @@ import iris.analysis
 from . import var_name_constraint
 
 def derive(cubes):
-	output_cube = cubes.extract_strict(var_name_constraint('ANTHNT_AER_BC'))+cubes.extract_strict(var_name_constraint('ANTHNT_AER_OC'))
-	output_cube = output_cube.collapsed(['level'],iris.analysis.SUM)
+	cube1= cubes.extract_strict(var_name_constraint('ANTHNT_AER_BC'))
+	cube2 = cubes.extract_strict(var_name_constraint('ANTHNT_AER_OC'))
+	output_cube = cube1+cube2
+	z_coord = cube1.coords(dimensions=1)
+	z_coord_name= z_coord[0].name()
+	output_cube = output_cube.collapsed(z_coord_name,iris.analysis.SUM)
     return output_cube
