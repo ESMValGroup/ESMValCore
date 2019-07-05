7f6e06b0a36c0abbb542922bcc90b4307c019d71
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 17:32:35 +0200

    Update ANTHNT_SO2_s.py

--- esmvalcore/preprocessor/_emac/_derive/ANTHNT_SO2_s.py
+++ esmvalcore/preprocessor/_emac/_derive/ANTHNT_SO2_s.py
@@ -8,5 +8,5 @@ def derive(cubes):
 	cube1 = cubes.extract_strict(var_name_constraint('ANTHNT_SO2'))
 	z_coord = cube1.coords(dimensions=1)
 	z_coord_name= z_coord[0].name()
-	output_cube = output_cube.collapsed(z_coord_name,iris.analysis.SUM)
+	output_cube = cube1.collapsed(z_coord_name,iris.analysis.SUM)
   	return output_cube
