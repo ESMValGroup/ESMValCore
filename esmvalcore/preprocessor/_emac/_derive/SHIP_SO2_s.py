51f917a99f5dd7daa47d745384ac0d7cfec27033
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 14:36:04 +0200

    Update SHIP_SO2_s.py

--- esmvalcore/preprocessor/_emac/_derive/SHIP_SO2_s.py
+++ esmvalcore/preprocessor/_emac/_derive/SHIP_SO2_s.py
@@ -5,6 +5,6 @@ import iris.analysis
 from . import var_name_constraint
 
 def derive(cubes):
-	output_cube = cubes.extract_strict(var_name_constraint('SHIP_SO2_N'))
+	output_cube = cubes.extract_strict(var_name_constraint('SHIP_SO2'))
 	output_cube = output_cube.collapsed(['level'],iris.analysis.SUM)
     return output_cube
