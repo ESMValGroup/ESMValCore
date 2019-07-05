7cbb26b7080dc089e5ba26f6e24f6f69d8b8a5d2
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 13:57:45 +0200

    Update rtmt.py

--- esmvalcore/preprocessor/_emac/_derive/rtmt.py
+++ esmvalcore/preprocessor/_emac/_derive/rtmt.py
@@ -4,7 +4,6 @@
 from . import var_name_constraint
 
 def derive(cubes):
-    rtmt_cube = cubes.extract_strict(var_name_constraint('flxtbot_ave'))[:,0,:]+ cubes.extract_strict(var_name_constraint('flxstop_ave'))[:,0,:]
-    
+    rtmt_cube = cubes.extract_strict(var_name_constraint('flxtbot_ave'))[:,0,:,:]+ cubes.extract_strict(var_name_constraint('flxstop_ave'))[:,0,:,:]
     return rtmt_cube
 
