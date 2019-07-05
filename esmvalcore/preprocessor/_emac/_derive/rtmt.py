e965cedf2065523f6dbc8fc29461985ef8b33de8
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 17:25:09 +0200

    Update rtmt.py

--- esmvalcore/preprocessor/_emac/_derive/rtmt.py
+++ esmvalcore/preprocessor/_emac/_derive/rtmt.py
@@ -1,9 +1,13 @@
 """Derivation of variable `rtmt`."""
 
-
+import iris
 from . import var_name_constraint
 
-def derive(cubes):
-    rtmt_cube = cubes.extract_strict(var_name_constraint('flxtbot_ave'))[:,0,:,:]+ cubes.extract_strict(var_name_constraint('flxstop_ave'))[:,0,:,:]
-    return rtmt_cube
 
+def derive(cubes):
+	flxs_cube = cubes.extract_strict(var_name_constraint('flxt_ave'))
+	flxt_cube = cubes.extract_strict(var_name_constraint('flxs_ave'))
+	z_coord = flxs_cube.coords(dimensions=1)
+	z_coord_name= z_coord[0].name()
+	rtmt_cube = flxs_cube.extract(iris.Constraint(**{z_coord_name : 1.}))+ flxt_cube.extract(iris.Constraint(**{z_coord_name : 1.}))
+	return rtmt_cube
