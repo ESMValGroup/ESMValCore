17237e7f8601adbada468ecae860ec14088ce4f8
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 14:41:50 +0200

    Update pr.py

--- esmvalcore/preprocessor/_emac/_derive/pr.py
+++ esmvalcore/preprocessor/_emac/_derive/pr.py
@@ -3,6 +3,6 @@
 from . import var_name_constraint
 
 def derive(cubes):
-    pr_cube = cubes.extract_strict(var_name_constraint('aprl_ave')) -cubes.extract_strict(var_name_constraint('aprc_ave'))-cubes.extract_strict(var_name_constraint('aprs_ave'))
+    pr_cube = cubes.extract_strict(var_name_constraint('aprl_ave')) + cubes.extract_strict(var_name_constraint('aprc_ave')) + cubes.extract_strict(var_name_constraint('aprs_ave'))
 
     return pr_cube
