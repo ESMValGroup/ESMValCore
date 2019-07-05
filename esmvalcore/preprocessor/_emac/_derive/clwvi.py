8dcd205a161d35379fc3892869cc9ed0ba213bba
debe-kevin <kevin.debeire@dlr.de>
2019-07-05 14:40:28 +0200

    Update clwvi.py

--- esmvalcore/preprocessor/_emac/_derive/clwvi.py
+++ esmvalcore/preprocessor/_emac/_derive/clwvi.py
@@ -5,4 +5,4 @@ from . import var_name_constraint
 
 def derive(cubes):
     clt_cube = cubes.extract_strict(var_name_constraint('xlvi_ave'))+ cubes.extract_strict(var_name_constraint('xivi_ave'))
-      return clt_cube
+    return clt_cube
