73e2511902f5c93e8b1ee15354357741de9d3576
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 13:05:21 +0200

    Update rlds.py

--- esmvalcore/preprocessor/_emac/_derive/rlds.py
+++ esmvalcore/preprocessor/_emac/_derive/rlds.py
@@ -1,10 +1,15 @@
-
 """Derivation of variable `rlds`."""
+
+"""The variable 'rlds' (Surface Downwelling Longwave Radiation) is combined from """
+"""the two EMAC variables: 'flxtbot_ave' and 'tradsu_ave'."""
+"""(following)"""
+
 
 from . import var_name_constraint
 
 def derive(cubes):
-    rlds_cube = cubes.extract_strict(var_name_constraint('flxtbot_ave'))-cubes.extract_strict(var_name_constraint('tradsu_ave'))
+    rlds_cube = cubes.extract_strict(var_name_constraint('flxtbot_ave')) -
+                cubes.extract_strict(var_name_constraint('tradsu_ave'))
     
     return rlds_cube
 
