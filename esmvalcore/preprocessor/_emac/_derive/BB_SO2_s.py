dfb4a87477f86b8e04f4607e4416af265e2ac237
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 14:59:34 +0200

    Update and rename BB_SO_s.py to BB_SO2_s.py

--- esmvalcore/preprocessor/_emac/_derive/BB_SO2_s.py
+++ esmvalcore/preprocessor/_emac/_derive/BB_SO2_s.py
@@ -1,11 +1,11 @@
-"""Derivation of variable `BB_SO_s`."""
+"""Derivation of variable `BB_SO2_s`."""
 
 import iris
 import iris.analysis
 from . import var_name_constraint
 
 def derive(cubes):
-	cube1 = cubes.extract_strict(var_name_constraint('BB_SO'))
+	cube1 = cubes.extract_strict(var_name_constraint('BB_SO2'))
 	z_coord = cube1.coords(dimensions=1)
 	z_coord_name= z_coord[0].name()
 	output_cube = cube1.collapsed(z_coord_name,iris.analysis.SUM)
