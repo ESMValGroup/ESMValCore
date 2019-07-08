6be5942b0423841cce6378b099071177e902a87b
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 12:41:25 +0200

    Update clt.py

--- esmvalcore/preprocessor/_emac/_derive/clt.py
+++ esmvalcore/preprocessor/_emac/_derive/clt.py
@@ -3,6 +3,7 @@
 """The variable 'clt' (total cloud fraction) is stored in the EMAC variable 'aclcov_ave',"""
 """and needs to be multiplied with 100. to represent the CMOR standard."""
 """(following the recipe from the DKRZ CMIP6 Data Request WebGUI)"""
+"""(https://c6dreq.dkrz.de/)"""
 
 from . import var_name_constraint
 
