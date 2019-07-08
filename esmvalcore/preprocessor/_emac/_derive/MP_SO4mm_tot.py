abdf2a31621c26465921cc56d37c542f87ec2eb9
Behrens <behr_gn@lx118.pa.op.dlr.de>
2019-07-08 11:29:36 +0200

    change comments in SO4

--- esmvalcore/preprocessor/_emac/_derive/MP_SO4mm_tot.py
+++ esmvalcore/preprocessor/_emac/_derive/MP_SO4mm_tot.py
@@ -1,10 +1,10 @@
 
-"""Derivation of variable `SO4mm_tot`."""
+"""Derivation of sum over all sulfate aerosol tracers."""
 
 from . import var_name_constraint
 
 def derive(cubes):
         MP_SO4mm_tot_cube = cubes.extract_strict(var_name_constraint('MP_SO4mm_as_ave'))+cubes.extract_strict(var_name_constraint('MP_SO4mm_ks_ave'))+
-                         cubes.extract_strict(var_name_constraint('MP_SO4mm_cs_ave'))+cubes.extract_strict(var_name_constraint('MP_SO4mm_ns_ave'))+
+                         cubes.extract_strict(var_name_constraint('MP_SO4mm_cs_ave'))+cubes.extract_strict(var_name_constraint('MP_SO4mm_ns_ave'))
         return MP_SO4mm_tot_cube
 
