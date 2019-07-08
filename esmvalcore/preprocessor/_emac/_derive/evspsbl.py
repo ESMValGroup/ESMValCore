1c9486bfa3fa3712549086598bdf2109b79871dd
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 11:11:55 +0200

    Update evspsbl.py

--- esmvalcore/preprocessor/_emac/_derive/evspsbl.py
+++ esmvalcore/preprocessor/_emac/_derive/evspsbl.py
@@ -2,6 +2,7 @@
 
 """The variable 'evspsbl' (evaporation from canopy) is stored in the EMAC variable 'evap_ave',"""
 """and needs to be multiplied with -1. to represent the CMOR standard."""
+"""(following the EMAC CMIP6 recipes)"""
 
 from . import var_name_constraint
 
