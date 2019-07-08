0ed5dbfeef87ce7de45969b9787a23eb66d3d41e
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 17:03:46 +0200

    Update SHIP_NO_s.py

--- esmvalcore/preprocessor/_emac/_derive/SHIP_NO_s.py
+++ esmvalcore/preprocessor/_emac/_derive/SHIP_NO_s.py
@@ -1,5 +1,9 @@
 """Derivation of variable `SHIP_NO_s`."""
 
+"""The variable 'SHIP_NO' is an EMAC specific variable that is used """
+"""as monitoring variable. It is vertically resolved, but was summed """
+"""up over all levels for easier plotting. """
+
 import iris
 import iris.analysis
 from . import var_name_constraint
@@ -7,6 +11,6 @@ from . import var_name_constraint
 def derive(cubes):
 	cube1 = cubes.extract_strict(var_name_constraint('SHIP_NO'))
 	z_coord = cube1.coords(dimensions=1)
-	z_coord_name= z_coord[0].name()
+	z_coord_name = z_coord[0].name()
 	output_cube = cube1.collapsed(z_coord_name,iris.analysis.SUM)
 	return output_cube
