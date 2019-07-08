a8ce7878a93dbef1be9ba12083a8855d855d7b68
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 14:54:54 +0200

    Update and rename BB_N_s.py to BB_NO_s.py

--- esmvalcore/preprocessor/_emac/_derive/BB_NO_s.py
+++ esmvalcore/preprocessor/_emac/_derive/BB_NO_s.py
@@ -1,12 +1,12 @@
 
-"""Derivation of variable `BB_N_s`."""
+"""Derivation of variable `BB_NO_s`."""
 
 import iris
 import iris.analysis
 from . import var_name_constraint
 
 def derive(cubes):
-	cube1 = cubes.extract_strict(var_name_constraint('BB_N'))
+	cube1 = cubes.extract_strict(var_name_constraint('BB_NO'))
 	z_coord = cube1.coords(dimensions=1)
 	z_coord_name= z_coord[0].name()
 	output_cube = cube1.collapsed(z_coord_name,iris.analysis.SUM)
