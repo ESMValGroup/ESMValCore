bd9532f6957d161cdf8049dbdd7d8f5b32fc4745
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-08 13:31:08 +0200

    Update rsutcs.py

--- esmvalcore/preprocessor/_emac/_derive/rsutcs.py
+++ esmvalcore/preprocessor/_emac/_derive/rsutcs.py
@@ -1,14 +1,14 @@
 """Derivation of variable `rsutcs`."""
 
-"""The variable 'rsutcs' (TOA Outgoing Clear-Sky Shortwave Radiation) is a""" 
-"""combination of three EMAC variables: 'flxsftop_ave', 'flxstop_ave' and"""
-"""'srad0u_ave'."""
-"""(following the recipe from the DKRZ CMIP6 Data Request WebGUI)"""
-"""(https://c6dreq.dkrz.de/)"""
+"""The variable 'rsutcs' (TOA Outgoing Clear-Sky Shortwave Radiation) is a """ 
+"""combination of three EMAC variables: 'flxsftop_ave', 'flxstop_ave' and """
+"""'srad0u_ave'. """
+"""(following the recipe from the DKRZ CMIP6 Data Request WebGUI) """
+"""(https://c6dreq.dkrz.de/) """
 """(The DKRZ recipe calls for 'viso_flxsftop', 'viso_flxstop' and """
-"""'rad01_srad0u', however, these variables are not available in the CMIP6"""
-"""channels therefore the variables 'flxsftop_ave', 'flxstop_ave' and """
-"""''srad0u_ave' are used.)
+"""'rad01_srad0u', however, these variables are not available in the EMAC """ 
+"""CMIP6 channels therefore the variables 'flxsftop_ave', 'flxstop_ave' """
+"""and 'srad0u_ave' are used.) """
 
 
 from . import var_name_constraint
