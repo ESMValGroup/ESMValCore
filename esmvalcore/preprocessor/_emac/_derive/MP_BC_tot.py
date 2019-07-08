dc965c6553242c0f54fad2b6df9ccb6781503b11
Axel Lauer <axel.lauer@dlr.de>
2019-07-08 14:28:45 +0200

    applied pep8 standard to _emac/_derive/MP_<var>.py scripts

--- esmvalcore/preprocessor/_emac/_derive/MP_BC_tot.py
+++ esmvalcore/preprocessor/_emac/_derive/MP_BC_tot.py
@@ -1,8 +1,13 @@
-"""Derivation of sum over black carbon tracers"""
+"""Calculation of sum of all black carbon (aerosol) tracers over all
+   aerosol modes (ks, ki, as, cs)"""
 
 from . import var_name_constraint
 
+
 def derive(cubes):
-	MP_BC_tot_cube = cubes.extract_strict(var_name_constraint('MP_BC_as_ave'))+cubes.extract_strict(var_name_constraint('MP_BC_ki_ave'))+
-			 cubes.extract_strict(var_name_constraint('MP_BC_ks_ave'))
-	return MP_ BC_tot_cube
+    MP_BC_tot_cube = cubes.extract_strict(
+        var_name_constraint('MP_BC_ki_ave')) + cubes.extract_strict(
+            var_name_constraint('MP_BC_ks_ave')) + cubes.extract_strict(
+                var_name_constraint('MP_BC_as_ave')) + cubes.extract_strict(
+                    var_name_constraint('MP_BC_cs_ave'))
+    return MP_BC_tot_cube
--- esmvalcore/preprocessor/_emac/_derive/MP_SS_tot.py
+++ esmvalcore/preprocessor/_emac/_derive/MP_SS_tot.py
@@ -1,9 +1,12 @@
-"""Derivation of sum over all sea salt tracers"""
+"""Calculation of sum of all sea salt (aerosol) tracers over all
+   aerosol modes (ks, as, cs)"""
 
 from . import var_name_constraint
+
+
 def derive(cubes):
-	MP_SS_tot_cube = cubes.extract_strict(var_name_constraint('MP_SS_as'))+cubes.extract_strict(var_name_constraint('MP_SS_cs'))+
-			 cubes.extract_strict(var_name_constraint('MP_SS_ks'))
-	return MP_SS_tot_cube
-
-
+    MP_SS_tot_cube = cubes.extract_strict(
+        var_name_constraint('MP_SS_ks')) + cubes.extract_strict(
+            var_name_constraint('MP_SS_as')) + cubes.extract_strict(
+                var_name_constraint('MP_SS_cs'))
+    return MP_SS_tot_cube
