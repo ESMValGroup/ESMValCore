1180190e2cb0d93466a8a80d2e67b695199eb282
Birgit Hassler <33543691+hb326@users.noreply.github.com>
2019-07-09 13:58:45 +0200

    Update SHIP_AER_BC_s.py

--- esmvalcore/preprocessor/_emac/_derive/SHIP_AER_BC_s.py
+++ esmvalcore/preprocessor/_emac/_derive/SHIP_AER_BC_s.py
@@ -1,5 +1,11 @@
 """Derivation of variable `SHIP_AER_BC_s`."""
 
+"""The variable 'SHIP_AER_BC_s' is an EMAC variable that is used """
+"""for monitoring EMAC output. It is here summed over all """
+"""available levels. """
+"""The variable is stored in the EMAC CMIP6 channel 'import_grid'. """
+"""SHIP_AER_BC_s: Ship Aerosol Black Carbon, summed """
+
 import iris
 import iris.analysis
 from . import var_name_constraint
@@ -7,6 +13,6 @@ from . import var_name_constraint
 def derive(cubes):
 	cube1 = cubes.extract_strict(var_name_constraint('SHIP_AER_BC'))
 	z_coord = cube1.coords(dimensions=1)
-	z_coord_name= z_coord[0].name()
+	z_coord_name = z_coord[0].name()
 	output_cube = cube1.collapsed(z_coord_name,iris.analysis.SUM)
     return output_cube
