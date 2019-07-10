9ba62801aa44383d72dda65ec339d64b58a467f8
Axel Lauer <axel.lauer@dlr.de>
2019-07-10 10:59:53 +0200

    deleted outdated emac variable derivation scripts

--- esmvalcore/preprocessor/_emac/_derive/pr.py
+++
@@ -1,8 +0,0 @@
-"""Derivation of variable `pr`."""
-
-from . import var_name_constraint
-
-def derive(cubes):
-    pr_cube = cubes.extract_strict(var_name_constraint('aprl_ave')) + cubes.extract_strict(var_name_constraint('aprc_ave')) + cubes.extract_strict(var_name_constraint('aprs_ave'))
-
-    return pr_cube
