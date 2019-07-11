1bba426c1568a6841325f665adc7311d89b9e30d
Manuel Schlund <manuel.schlund@dlr.de>
2019-07-11 14:08:13 +0200

    Reused CESM2 fix for CESM2-WACCM

--- esmvalcore/cmor/_fixes/cmip6/cesm2_waccm.py
+++ esmvalcore/cmor/_fixes/cmip6/cesm2_waccm.py
@@ -1,23 +1,6 @@
 """Fixes for CESM2-WACCM model."""
-from ..fix import Fix
-from ..shared import add_scalar_height_coord
+from .cesm2 import Tas as BaseTas
 
 
-class Tas(Fix):
+class Tas(BaseTas):
     """Fixes for tas."""
-
-    def fix_metadata(self, cubes):
-        """Add height (2m) coordinate.
-
-        Parameters
-        ----------
-        cube : iris.cube.CubeList
-
-        Returns
-        -------
-        iris.cube.Cube
-
-        """
-        cube = self.get_cube_from_list(cubes)
-        add_scalar_height_coord(cube)
-        return cubes
