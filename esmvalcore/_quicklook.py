9be0339dd689fc20e45d28f3892c9cf148dab369
Björn Brötz <bjoern.broetz@dlr.de>
2019-07-16 17:59:59 +0200

    Fix variable naming

--- esmvalcore/_quicklook.py
+++ esmvalcore/_quicklook.py
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 def create_recipe(cfg):
     start = cfg['quicklook'].get('start')
     end = cfg['quicklook'].get('end')
+    # TODO: We should rename "recipes" to "diagnostics" in this context
     recipes = cfg['quicklook'].get('recipes')
     run_id = cfg['quicklook'].get('dataset-id')
     output_dir = os.path.join(cfg['quicklook'].get('output_dir'), run_id)
@@ -28,12 +29,12 @@ def create_recipe(cfg):
 
     logger.debug("Creating directory %s", output_dir)
     os.makedirs(output_dir, exist_ok=True)
-    d = dict()
+    all_diagnostics = dict()
     for recipe in recipes:
         recipe_name = 'diagnostics_{0}.yml'.format(recipe)
         with open(os.path.join(recipe_dir, recipe_name)) as stream:
-            d = {
-                **d,
+            all_diagnostics = {
+                **all_diagnostics,
                 **yaml.load(stream, Loader=yaml.FullLoader).get('diagnostics')
             }
     with open(os.path.join(recipe_dir, 'general.yml')) as stream:
@@ -47,8 +48,8 @@ def create_recipe(cfg):
         'start_year': start,
         'end_year': end
     }]
-    path_to_recipe = os.path.join(output_dir, recipe_name)
-    with open(path_to_recipe, 'w') as f:
+    path_to_recipe = os.path.join(output_dir, 'recipe_quicklook.yml')
+    with open(path_to_recipe, 'w') as stream:
         logger.debug("Writing %s to  %s", recipe_name, output_dir)
-        f.write(yaml.dump(out))
+        stream.write(yaml.dump(out))
     return path_to_recipe
