bd7277f7ef162b42f05f0aed0b423f49b8632a40
Manuel Schlund <manuel.schlund@dlr.de>
2019-07-17 16:22:04 +0200

    Fixed sithick.py

--- esmvalcore/preprocessor/_emac/_derive/sithick.py
+++ esmvalcore/preprocessor/_emac/_derive/sithick.py
@@ -1,15 +1,16 @@
 """Derivation of variable `sithick`.
 
-Change zero to fill_value.
+Change zero to missing value.
 
 """
 
-import numpy.ma as ma
+import numpy as np
 
 from . import var_name_constraint
+
 
 def derive(cubes):
     """Derive `sithick`."""
     sithick_cube = cubes.extract_strict(var_name_constraint('siced_ave'))
-    sithick_cube = ma.masked_equal(sithick_cube, 0.)
+    sithick_cube.data = np.ma.masked_equal(sithick_cube.data, 0.0)
     return sithick_cube
