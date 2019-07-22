d81a4602957f117a881c6a53d342af5af68cdf23
Manuel Schlund <manuel.schlund@dlr.de>
2019-07-22 18:11:46 +0200

    Optimized quicklook recipe creation

--- esmvalcore/_quicklook.py
+++ esmvalcore/_quicklook.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 def create_recipe(cfg):
     """Create recipe for quicklook mode and return its path."""
+    print("Creating quicklook recipe")
     start = cfg['quicklook'].get('start')
     end = cfg['quicklook'].get('end')
     # TODO: We should rename "recipes" to "diagnostics" in this context
@@ -20,16 +21,16 @@ def create_recipe(cfg):
 
     logger.debug("Creating directory %s", preproc_dir)
     os.makedirs(preproc_dir, exist_ok=True)
-    all_diagnostics = dict()
+    all_diagnostics = {}
     for recipe in recipes:
-        recipe_name = 'diagnostics_{0}.yml'.format(recipe)
+        recipe_name = f'diagnostics_{recipe}.yml'
         with open(os.path.join(recipe_dir, recipe_name)) as stream:
-            all_diagnostics = {
-                **all_diagnostics,
-                **yaml.load(stream, Loader=yaml.FullLoader).get('diagnostics')
-            }
+            diagnostics = yaml.load(stream, Loader=yaml.FullLoader)
+            for (diag_name, diag_content) in diagnostics.items():
+                diag_name = diag_name.format(run_id=run_id)
+                all_diagnostics[diag_name] = diag_content
     with open(os.path.join(recipe_dir, 'general.yml')) as stream:
-        out = {**yaml.load(stream, Loader=yaml.FullLoader)}
+        out = yaml.load(stream, Loader=yaml.FullLoader)
 
     out['diagnostics'] = all_diagnostics
     out['datasets'] = [{
