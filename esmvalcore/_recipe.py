6b00da21ad89d3caae83f8187db03b0b812e4cbd
Valeriu Predoi <valeriu.predoi@gmail.com>
2019-07-29 15:49:34 +0100

    removed making a list for products

--- esmvalcore/_recipe.py
+++ esmvalcore/_recipe.py
@@ -694,24 +694,24 @@ def _get_single_preprocessor_task(variables,
     ancestor_products = [p for task in ancestor_tasks for p in task.products]
 
     if variables[0]['frequency'] != 'fx':
-        products = list(_get_preprocessor_products(
+        products = _get_preprocessor_products(
             variables=variables,
             profile=profile,
             order=order,
             ancestor_products=ancestor_products,
             config_user=config_user,
-        ))
+        )
     else:
         fx_profile = _remove_temporal_preprocs(profile)
         # not do time extraction; this is a default preproc
         fx_profile['extract_time'] = False
-        products = list(_get_preprocessor_products(
+        products = _get_preprocessor_products(
             variables=variables,
             profile=fx_profile,
             order=order,
             ancestor_products=None,
             config_user=config_user,
-        ))
+        )
 
     if not products:
         raise RecipeError(
