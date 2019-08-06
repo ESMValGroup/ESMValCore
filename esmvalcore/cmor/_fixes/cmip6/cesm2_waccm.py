96a15d0f78f8a3cde45901e8c45be0cbefdd3da5
Birgit Hassler <birgit.hassler@dlr.de>
2019-08-06 13:47:17 +0200

    added fix for WACCM ua time coordinate

--- esmvalcore/cmor/_fixes/cmip6/cesm2_waccm.py
+++ esmvalcore/cmor/_fixes/cmip6/cesm2_waccm.py
@@ -1,6 +1,39 @@
 """Fixes for CESM2-WACCM model."""
+import iris
+import numpy as np
+
 from .cesm2 import Tas as BaseTas
+from ..fix import Fix
 
 
 class Tas(BaseTas):
     """Fixes for tas."""
+
+
+class Ua(Fix):
+    """Fixes for ua."""
+
+    def fix_metadata(self, cubes):
+        """Fix non-monotonic time axis.
+
+        Parameters
+        ----------
+        cube : iris.cube.CubeList
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        cube = self.get_cube_from_list(cubes)
+        coord = cube.coord('time')
+        idx_sorted = np.argsort(coord.points)
+        coord.points = coord.points[idx_sorted]
+        coord.bounds = None
+        # coord.guess_bounds('time')  # fails for coordinate 'time'
+        time_idx = cube.coord_dims('time')[0]
+        slice_idx = [slice(None)] * cube.ndim
+        slice_idx[time_idx] = idx_sorted
+        cube.data = cube.data[slice_idx]
+        iris.util.promote_aux_coord_to_dim_coord(cube, 'time')
+        return cubes
--- tests/integration/cmor/_fixes/cmip6/test_cesm2_waccm.py
+++ tests/integration/cmor/_fixes/cmip6/test_cesm2_waccm.py
@@ -3,7 +3,7 @@ import iris
 import pytest
 from cf_units import Unit
 
-from esmvalcore.cmor._fixes.cmip6.cesm2_waccm import Tas
+from esmvalcore.cmor._fixes.cmip6.cesm2_waccm import Tas, Ua
 from esmvalcore.cmor.fix import Fix
 
 
@@ -12,6 +12,17 @@ def tas_cubes():
     ta_cube = iris.cube.Cube([1.0], var_name='ta')
     tas_cube = iris.cube.Cube([3.0], var_name='tas')
     return iris.cube.CubeList([ta_cube, tas_cube])
+
+
+@pytest.fixture
+def ua_cubes():
+    ta_cube = iris.cube.Cube([1.0], var_name='ta')
+    time_coord = iris.coords.AuxCoord(
+        [1.0, 3.0, 2.0], standard_name='time', units='days since 1850-01-01',
+        bounds=[[0.5, 1.5], [2.5, 3.5], [1.5, 2.5]])
+    ua_cube = iris.cube.Cube([7.0, 8.0, 9.0], var_name='ua',
+                             aux_coords_and_dims=[(time_coord, 0)])
+    return iris.cube.CubeList([ta_cube, ua_cube])
 
 
 def test_get_tas_fix():
@@ -39,3 +50,22 @@ def test_tas_fix_metadata(tas_cubes):
         else:
             with pytest.raises(iris.exceptions.CoordinateNotFoundError):
                 cube.coord('height')
+
+
+def test_get_ua_fix():
+    fix = Fix.get_fixes('CMIP6', 'CESM2-WACCM', 'ua')
+    assert fix == [Ua()]
+
+
+def test_ua_fix_metadata(ua_cubes):
+    expected_coord = iris.coords.AuxCoord(
+        [1.0, 2.0, 3.0], standard_name='time', units='days since 1850-01-01',
+        bounds=None)
+    expected_cube = iris.cube.Cube([7.0, 9.0, 8.0], var_name='ua',
+                                   aux_coords_and_dims=[(expected_coord, 0)])
+    fix = Ua()
+    out_cubes = fix.fix_metadata(ua_cubes)
+    assert out_cubes is ua_cubes
+    for cube in out_cubes:
+        if cube.var_name == 'ua':
+            assert cube == expected_cube
