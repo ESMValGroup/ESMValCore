ed28733f752d614445a322235d10de52697a66ae
Javier Vegas-Regidor <javier.vegas@bsc.es>
2019-08-23 09:44:18 +0200

    Add anomalies

--- esmvalcore/preprocessor/__init__.py
+++ esmvalcore/preprocessor/__init__.py
@@ -23,7 +23,7 @@ from ._regrid import extract_levels, regrid
 from ._time import (extract_month, extract_season, extract_time, regrid_time,
                     daily_statistics, monthly_statistics, seasonal_statistics,
                     annual_statistics, decadal_statistics,
-                    climate_statistics)
+                    climate_statistics, anomalies)
 
 from ._units import convert_units
 from ._volume import (volume_statistics, depth_integration, extract_trajectory,
@@ -87,6 +87,7 @@ __all__ = [
     'annual_statistics',
     'decadal_statistics',
     'climate_statistics',
+    'anomalies',
     'regrid_time',
     'cmor_check_data',
     'convert_units',
