e243a8cb50e0ef3467ba3e8dbdd9b4dae0903e59
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2019-09-05 09:13:47 +0100

    Added ability to deal with time independent data

--- esmvalcore/preprocessor/_derive/ohc.py
+++ esmvalcore/preprocessor/_derive/ohc.py
@@ -47,21 +47,28 @@ class DerivedVariable(DerivedVariableBase):
         # 2. multiply with each other and with cprho0
         # some juggling with coordinates needed since Iris is very
         # restrictive in this regard
-        t_coord_dims = cube.coord_dims('time')
-        assert len(t_coord_dims) == 1
-        t_coord_dim = t_coord_dims[0]
-        dim_coords = [(coord, cube.coord_dims(coord)[0])
-                      for coord in cube.coords(contains_dimension=t_coord_dim,
-                                               dim_coords=True)]
-        aux_coords = [(coord, cube.coord_dims(coord))
-                      for coord in cube.coords(contains_dimension=t_coord_dim,
-                                               dim_coords=False)]
-        for coord, dims in dim_coords + aux_coords:
-            cube.remove_coord(coord)
+        try:
+            t_coord_dims = cube.coord_dims('time')
+        except iris.exceptions.CoordinateNotFoundError:
+            time_coord_present = False
+        else:
+            time_coord_present = True
+            t_coord_dim = t_coord_dims[0]
+            dim_coords = [(coord, cube.coord_dims(coord)[0])
+                          for coord in cube.coords(
+                                  contains_dimension=t_coord_dim,
+                                  dim_coords=True)]
+            aux_coords = [(coord, cube.coord_dims(coord))
+                          for coord in cube.coords(
+                                  contains_dimension=t_coord_dim,
+                                  dim_coords=False)]
+            for coord, dims in dim_coords + aux_coords:
+                cube.remove_coord(coord)
         new_cube = cube * volume
         new_cube *= RHO_CP
-        for coord, dim in dim_coords:
-            new_cube.add_dim_coord(coord, dim)
-        for coord, dims in aux_coords:
-            new_cube.add_aux_coord(coord, dims)
+        if time_coord_present:
+            for coord, dim in dim_coords:
+                new_cube.add_dim_coord(coord, dim)
+            for coord, dims in aux_coords:
+                new_cube.add_aux_coord(coord, dims)
         return new_cube
