2360d1baf0461d1087f1699b14c1f6cf934bef4f
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-09-24 14:23:45 +0200

    add doc

--- esmvalcore/preprocessor/_area.py
+++ esmvalcore/preprocessor/_area.py
@@ -330,6 +330,9 @@ def extract_shape(cube, shapefile, method='contains', crop=True, decomposed=Fals
     crop: bool, optional
         Crop the resulting cube using `extract_region()`. Note that data on
         irregular grids will not be cropped.
+    decomposed: bool, optional
+        whether or not to retain the sub shapes of the shapefile in the output.
+        If this is set to True, the output cube has a dimension for the sub shapes.
 
     Returns
     -------
