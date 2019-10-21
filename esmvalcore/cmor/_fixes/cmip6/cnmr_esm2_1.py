534f60765487f1c5de3f16d50946ecf39f953a58
Kevin Debeire <kevin.debeire@dlr.de>
2019-10-21 11:06:02 +0200

    added fix files for cnrm-esm2-1 for variable abs550aer  missing dimension lambda550nm

---
+++ esmvalcore/cmor/_fixes/cmip6/cnmr_esm2_1.py
@@ -0,0 +1,30 @@
+"""Fixes for CNRM-ESM2-1 model."""
+from ..fix import Fix
+from ..shared import add_scalar_height_coord
+import iris
+
+
+class Abs550aer(Fix):
+    """Fixes for abs550aer"""
+    def fix_metadata(self, cubes):
+        """
+        Fix missing scalar dimension lambda550nm
+        
+        Parameters
+        ----------
+        cubes: iris CubeList
+            List of cubes to fix
+        
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        lambda550nm = iris.coords.AuxCoord('550.0',
+                                         standard_name='radiation_wavelength',
+                                         long_name='Radiation Wavelength 550 nanometers',
+                                         var_name='wavelength',
+                                         units='nm',
+                                         bounds=None)
+        for cube in cubes:
+            cube.add_aux_coord(lambda550nm)
+        return cubes
