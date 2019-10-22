312f277826a3a80212bfb65846659dfc5bc1c838
bascrezee <bascrezee@gmail.com>
2019-10-22 11:34:00 +0200

    minor change

--- esmvalcore/preprocessor/__init__.py
+++ esmvalcore/preprocessor/__init__.py
@@ -24,7 +24,7 @@ from ._regrid import extract_levels, regrid
 from ._time import (extract_month, extract_season, extract_time, regrid_time,
                     daily_statistics, monthly_statistics, seasonal_statistics,
                     annual_statistics, decadal_statistics, climate_statistics,
-                    anomalies, standardize)
+                    anomalies)
 
 from ._units import convert_units
 from ._volume import (volume_statistics, depth_integration, extract_trajectory,
