2ebe6cfb3007b956071ed8b5fe03c499c2eca151
Kevin Debeire <kevin.debeire@dlr.de>
2019-11-22 13:41:19 +0100

    TAS fix files update

--- esmvalcore/cmor/_fixes/cmip6/gfdl_cm4.py
+++ esmvalcore/cmor/_fixes/cmip6/gfdl_cm4.py
@@ -2,28 +2,6 @@
 from ..fix import Fix
 from ..shared import add_scalar_height_coord
 
-
-class Tas(Fix):
-    """Fixes for tas."""
-
-    def fix_metadata(self, cubes):
-        """
-        Add height (2m) coordinate.
-
-        Parameters
-        ----------
-        cube : iris.cube.CubeList
-
-        Returns
-        -------
-        iris.cube.Cube
-
-        """
-        cube = self.get_cube_from_list(cubes)
-        add_scalar_height_coord(cube, 2.0)
-        return cubes
-
-
 class Uas(Fix):
     """Fixes for uas."""
 
