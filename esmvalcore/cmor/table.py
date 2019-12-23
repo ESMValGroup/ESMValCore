2d12b51eb83f8e3d5bfd5383b630ca089b605735
Bouwe Andela <b.andela@esciencecenter.nl>
2019-12-23 14:25:10 +0100

    Use coordinate names instead of axis

--- esmvalcore/cmor/table.py
+++ esmvalcore/cmor/table.py
@@ -164,11 +164,7 @@ class CMIP6Info(object):
                     )
                     raise
 
-            axis = coord.axis
-            if not axis:
-                axis = 'none'
-
-            var.coordinates[axis] = coord
+            var.coordinates[dimension] = coord
 
     def _load_coordinates(self):
         self.coords = {}
