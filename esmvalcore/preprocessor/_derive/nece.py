f924276a10a8005066bc965ee6c14dcfef3b0e36
Lee de Mora <ledm@pml.ac.uk>
2020-01-20 16:04:14 +0000

    Required now a function, not a dict.

--- esmvalcore/preprocessor/_derive/nece.py
+++ esmvalcore/preprocessor/_derive/nece.py
@@ -9,10 +9,14 @@ class DerivedVariable(DerivedVariableBase):
     """Derivation of variable `nece` (Net Ecosystem Carbon Exchange)."""
 
     # Required variables
-    required = [{'short_name': 'rh', },
-                {'short_name': 'npp', }]
-    # ra = Autotrophic respiration
-    # npp = net primary production
+    .
+    def required(project):
+        """Declare the variables needed for derivation."""
+        # ra = Autotrophic respiration
+        # npp = net primary production
+        required = [{'short_name': 'rh', },
+                    {'short_name': 'npp', }]
+        return required
 
     @staticmethod
     def calculate(cubes):
