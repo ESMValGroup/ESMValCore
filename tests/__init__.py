a3326304a6bef2d5b5fce5cfcb6b8efa9c3f2cda
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-01-20 12:57:14 +0000

    removed obsolete function

--- tests/__init__.py
+++ tests/__init__.py
@@ -71,9 +71,3 @@ class Test(unittest.TestCase):
         np.testing.assert_array_equal(a, b)
         if np.ma.isMaskedArray(a) or np.ma.isMaskedArray(b):
             np.testing.assert_array_equal(a.mask, b.mask)
-
-    @wraps(np.testing.assert_array_almost_equal)
-    def assertArrayAlmostEqual(self, a, b, decimal=6,  # noqa: N802
-                               err_msg='', verbose=True):
-        np.testing.assert_array_almost_equal(
-            a, b, decimal=decimal, err_msg=err_msg, verbose=verbose)
