2b575e1247ea94103e88c79d13d07639ee00b92c
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-01-21 12:27:40 +0000

    fixed handling of tasks

--- esmvalcore/_recipe.py
+++ esmvalcore/_recipe.py
@@ -924,16 +924,17 @@ def _get_preprocessor_task(variables, profiles, config_user, task_name):
                     _get_correct_fx_file(var, fx_var, config_user)[1][0]
                     for fx_var in fx_vars
                 ]
-                fx_name = task_name.split(
-                    TASKSEP)[0] + TASKSEP + 'area-vol_stat_' + \
-                    fx_variables[0]['variable_group']
-                task = _get_single_preprocessor_task(
-                    fx_variables,
-                    before,
-                    config_user,
-                    name=fx_name,
-                )
-                fx_preproc_tasks.append(task)
+                for fx_variable in fx_variables:
+                    fx_name = task_name.split(
+                        TASKSEP)[0] + TASKSEP + 'fx_area-volume_stats_' + \
+                        fx_variable['variable_group']
+                    task = _get_single_preprocessor_task(
+                        [fx_variable],
+                        before,
+                        config_user,
+                        name=fx_name,
+                    )
+                    fx_preproc_tasks.append(task)
 
     derive_tasks.extend(fx_preproc_tasks)
 
