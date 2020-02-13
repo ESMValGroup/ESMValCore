e7252f337dcd18c12b42c0dee48b69e37af1ef2f
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-02-13 17:56:40 +0000

    added test for simple mask combination for a small threshold

--- tests/integration/preprocessor/_mask/test_mask.py
+++ tests/integration/preprocessor/_mask/test_mask.py
@@ -156,6 +156,46 @@ class Test(tests.Test):
         self.assert_array_equal(result_2.data.mask, data_2.mask)
         self.assert_array_equal(result_1.data, data_1)
 
+    def test_mask_fillvalues_zero_threshold(self):
+        """Test the fillvalues mask: func mask_fillvalues for 0-threshold"""
+        data_1 = data_2 = self.mock_data
+        data_1.mask = np.ones((4, 3, 3), bool)
+        data_1.mask[0, 0, 0] = False
+        data_1.mask[2, 0, 1] = False
+        data_2.mask = np.ones((4, 3, 3), bool)
+        data_2.mask[0, 0, 0] = False
+        data_2.mask[1, 0, 1] = False
+        coords_spec = [(self.times, 0), (self.lats, 1), (self.lons, 2)]
+        cube_1 = iris.cube.Cube(data_1, dim_coords_and_dims=coords_spec)
+        cube_2 = iris.cube.Cube(data_2, dim_coords_and_dims=coords_spec)
+        filename_1 = tempfile.NamedTemporaryFile().name + '.nc'
+        filename_2 = tempfile.NamedTemporaryFile().name + '.nc'
+        product_1 = PreprocessorFile(attributes={'filename': filename_1},
+                                     settings={})
+        product_1.cubes = [cube_1]
+        product_2 = PreprocessorFile(attributes={'filename': filename_2},
+                                     settings={})
+        product_2.cubes = [cube_2]
+        results = mask_fillvalues({product_1, product_2},
+                                  0.5,
+                                  min_value=-1.e20)
+        result_1, result_2 = None, None
+        for product in results:
+            if product.filename == filename_1:
+                result_1 = product.cubes[0]
+            if product.filename == filename_2:
+                result_2 = product.cubes[0]
+        self.assert_array_equal(result_2.data.mask, data_2.mask)
+        self.assert_array_equal(result_1.data, data_1)
+        # identical masks per time point
+        self.assert_array_equal(result_2[0].data.mask, result_1[0].data.mask)
+        self.assert_array_equal(result_2[1].data.mask, result_1[1].data.mask)
+        self.assert_array_equal(result_2[2].data.mask, result_1[2].data.mask)
+        # identical masks with cumluative
+        cumulative_mask = cube_1[0].data.mask | cube_2[0].data.mask
+        self.assert_array_equal(result_1[0].data.mask, cumulative_mask)
+        self.assert_array_equal(result_2[0].data.mask, cumulative_mask)
+
 
 if __name__ == '__main__':
     unittest.main()
