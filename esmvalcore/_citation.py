299bd841551804040f7351b752ad685784384115
SarahAlidoost <f.alidoost@esciencecenter.nl>
2020-02-21 17:54:54 +0100

    refactor

--- esmvalcore/_citation.py
+++ esmvalcore/_citation.py
@@ -12,35 +12,43 @@ CMIP6_URL_STEM = 'https://cera-www.dkrz.de/WDCC/ui/cerasearch'
 
 def _write_citation_file(product):
     """
-        Write citation information provided by the recorded provenance.
-        Recipe and cmip6 data references are saved into one bibtex file.
-        cmip6 data references are provided by CMIP6 data citation service.
-        each cmip6 data reference has a json link. In the case of internet
-        connection, cmip6 data references are saved into a bibtex file.
-        Otherwise, cmip6 data reference links are saved into a text file.
+    Write citation information provided by the recorded provenance.
+
+    Recipe and cmip6 data references are saved into one bibtex file.
+    cmip6 data references are provided by CMIP6 data citation service.
+    each cmip6 data reference has a json link. In the case of internet
+    connection, cmip6 data references are saved into a bibtex file.
+    Otherwise, cmip6 data reference links are saved into a text file.
     """
     # collect info from provenance
     product_name = os.path.splitext(product.filename)[0]
     products_tags = []
     product_entries = ''
     product_urls = ''
+    product_info_urls = []
+    product_json_urls = []
+
     for item in product.provenance.records:
         for key, value in item.attributes:
             if key.namespace.prefix == 'attribute':
                 if key.localpart in {'reference', 'references'}:
                     products_tags.append(value)
                 elif key.localpart == 'mip_era' and value == 'CMIP6':
-                    json_url, info_url = _make_url(item.attributes)
-                    cmip_entry = _collect_cmip_citation(json_url, info_url)
-                    if cmip_entry == info_url:
-                        product_urls += '{}\n'.format(cmip_entry)
-                    else:
-                        product_entries += '{}\n'.format(cmip_entry)
+                    url_prefix = _make_url_prefix(item.attributes)
+                    product_info_urls.append(_make_info_url(url_prefix))
+                    product_json_urls.append(_make_json_url(url_prefix))
 
     # save CMIP6 url_info, if any
-    if product_urls:
+    if product_info_urls:
+        for info_url in product_info_urls:
+            product_urls += '{}\n'.format(info_url)
         with open(f'{product_name}_data_citation_url.txt', 'w') as file:
             file.write(product_urls)
+
+    # convert json_urls to bibtex entries
+    if product_json_urls:
+        for json_url in product_json_urls:
+            product_entries += '{}\n'.format(_collect_cmip_citation(json_url))
 
     # convert tags to bibtex entries
     if products_tags:
@@ -56,7 +64,7 @@ def _write_citation_file(product):
 
 
 def _clean_tags(tags):
-    """some tages are combined in one string variable in provenance."""
+    """Clean the tages that are recorded as str by provenance."""
     pattern = re.compile(r'\w+')
     return pattern.findall(str(tags))
 
@@ -79,7 +87,9 @@ def _get_response(url):
 def _json_to_bibtex(data):
     """Make a bibtex entry from CMIP6 Data Citation json data."""
     if data.get('creators', False):
-        author_list = [item.get('creatorName', '') for item in data['creators']]
+        author_list = [
+            item.get('creatorName', '') for item in data['creators']
+        ]
     if author_list:
         if author_list[0] == author_list[-1]:
             authors = author_list[0]
@@ -105,7 +115,7 @@ def _json_to_bibtex(data):
         f'author = {{{authors}}},\n\t'
         f'doi = {{{doi}}},\n'
         f'{"}"}\n'
-        )
+    )
     return bibtex_entry
 
 
@@ -117,26 +127,28 @@ def _collect_bibtex_citation(tag):
             with open(bibtex_file, 'r') as file:
                 entry = '{}'.format(file.read())
         else:
-            raise ValueError('The reference file {} does not exist.'.format(bibtex_file))
+            raise ValueError(
+                'The reference file {} does not exist.'.format(bibtex_file)
+            )
     else:
         logger.info('The reference folder does not exist.')
         entry = ''
     return entry
 
 
-def _collect_cmip_citation(json_url, info_url):
+def _collect_cmip_citation(json_url):
     """Collect information from CMIP6 Data Citation Service."""
-    bibtex_entry = info_url
     json_data = _get_response(json_url)
     if json_data:
         bibtex_entry = _json_to_bibtex(json_data)
     else:
         logger.info('Invalid json link %s', json_url)
+        bibtex_entry = 'Invalid json link'
     return bibtex_entry
 
 
-def _make_url(attribute):
-    """make json and info urls based on CMIP6 Data Citation Service."""
+def _make_url_prefix(attribute):
+    """Make url prefix based on CMIP6 Data Citation Service."""
     # the order of keys is important
     localpart = {
         'mip_era': '',
@@ -149,6 +161,16 @@ def _make_url(attribute):
         if key.localpart in localpart:
             localpart[key.localpart] = value
     url_prefix = '.'.join(localpart.values())
+    return url_prefix
+
+
+def _make_json_url(url_prefix):
+    """Make json url based on CMIP6 Data Citation Service."""
     json_url = f'{CMIP6_URL_STEM}/cerarest/exportcmip6?input={url_prefix}'
+    return json_url
+
+
+def _make_info_url(url_prefix):
+    """Make info url based on CMIP6 Data Citation Service."""
     info_url = f'{CMIP6_URL_STEM}/cmip6?input=CMIP6.{url_prefix}'
-    return json_url, info_url
+    return info_url
