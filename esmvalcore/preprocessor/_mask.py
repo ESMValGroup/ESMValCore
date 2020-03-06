0799ab44820a119c4fe6cc702c965a3d60566171
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-06 11:24:23 +0000

    implement fx files as dicts everywhere

--- esmvalcore/preprocessor/_mask.py
+++ esmvalcore/preprocessor/_mask.py
@@ -12,10 +12,10 @@ import os
 
 import cartopy.io.shapereader as shpreader
 import iris
-from iris.analysis import Aggregator
-from iris.util import rolling_window
 import numpy as np
 import shapely.vectorized as shp_vect
+from iris.analysis import Aggregator
+from iris.util import rolling_window
 
 logger = logging.getLogger(__name__)
 
@@ -100,8 +100,8 @@ def mask_landsea(cube, fx_files, mask_out, always_use_ne_mask=False):
     cube: iris.cube.Cube
         data cube to be masked.
 
-    fx_files: list
-        list holding the full paths to fx files.
+    fx_files: dict
+        dict: keys: fx variables, values: full paths to fx files.
 
     mask_out: str
         either "land" to mask out land mass or "sea" to mask out seas.
@@ -131,7 +131,8 @@ def mask_landsea(cube, fx_files, mask_out, always_use_ne_mask=False):
         'sea': os.path.join(cwd, 'ne_masks/ne_50m_ocean.shp')
     }
 
-    if fx_files and not always_use_ne_mask:
+    fx_files = fx_files.values()
+    if any(fx_files) and not always_use_ne_mask:
         fx_cubes = {}
         for fx_file in fx_files:
             fxfile_members = os.path.basename(fx_file).split('_')
@@ -192,8 +193,8 @@ def mask_landseaice(cube, fx_files, mask_out):
     cube: iris.cube.Cube
         data cube to be masked.
 
-    fx_files: list
-        list holding the full paths to fx files.
+    fx_files: dict
+        dict: keys: fx variables, values: full paths to fx files.
 
     mask_out: str
         either "landsea" to mask out landsea or "ice" to mask out ice.
@@ -211,8 +212,9 @@ def mask_landseaice(cube, fx_files, mask_out):
         Error raised if fx_files list is empty.
 
     """
-    # sftgif is the only one so far
-    if fx_files:
+    # sftgif is the only one so far but users can set others
+    fx_files = fx_files.values()
+    if any(fx_files):
         for fx_file in fx_files:
             fx_cube = iris.load_cube(fx_file)
 
