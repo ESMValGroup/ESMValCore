1a402a09f72e5ca80cdcc80dab8852c8cf8dd291
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-03-10 14:30:27 +0000

    Bouwe suggestion
    Co-Authored-By: Bouwe Andela <bouweandela@users.noreply.github.com>

--- esmvalcore/_recipe.py
+++ esmvalcore/_recipe.py
@@ -389,9 +389,8 @@ def _get_correct_fx_file(variable, fx_variable, config_user):
     else:
         # Get all fx-related mips (original var mip,
         # 'fx' and extend from cmor tables)
-        fx_mips = [variable['mip'], 'fx']
-        fx_mips.extend(
-            [key for key in cmor_table.tables if 'fx' in key and key != 'fx'])
+        fx_mips = [variable['mip']]
+        fx_mips.extend(mip for mip in cmor_table.tables if 'fx' in mip)
 
     # Search all mips for available variables
     # priority goes to user specified mip if available
