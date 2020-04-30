2ccf2f909804378c09ef359cb8d2c8b15e3e5a56
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-04-30 13:58:31 +0100

    added checks and default values for cropped coords

--- esmvalcore/preprocessor/_area.py
+++ esmvalcore/preprocessor/_area.py
@@ -331,14 +331,23 @@ def _crop_cube(cube, start_longitude, start_latitude, end_longitude,
         lon_bound = lon_coord.core_bounds()[0]
         lon_step = lon_bound[1] - lon_bound[0]
         start_longitude -= lon_step
+        if start_longitude < -180:
+            start_longitude = -180.
         end_longitude += lon_step
+        if end_longitude > 360:
+            end_longitude = 360.
+        if end_longitude > 180 and start_longitude == -180.:
+            end_longitude = 180.
         lat_bound = lat_coord.core_bounds()[0]
         lat_step = lat_bound[1] - lat_bound[0]
         start_latitude -= lat_step
+        if start_latitude < -90:
+            start_latitude = -90.
         end_latitude += lat_step
+        if end_latitude > 90.:
+            end_latitude = 90.
         cube = extract_region(cube, start_longitude, end_longitude,
                               start_latitude, end_latitude)
-
     return cube
 
 
