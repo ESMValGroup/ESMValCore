d80d57f95bc030d6b6d55ee8920a87cedbe72267
Martin Jury <martin.w.jury@gmail.com>
2020-06-17 12:48:53 +0200

    Add linear_trend

--- esmvalcore/preprocessor/_detrend.py
+++ esmvalcore/preprocessor/_detrend.py
@@ -36,3 +36,38 @@ def detrend(cube, dimension='time', method='linear'):
         shape=(cube.shape[axis],)
     )
     return cube.copy(detrended)
+
+
+def linear_trend(cube, dimension='time'):
+    """
+    Give the trend along a given dimension by detrending first and returning
+    the difference
+
+    Parameters
+    ----------
+    cube: iris.cube.Cube
+        input cube.
+    dimension: str
+        Dimension to detrend
+    method: str
+        Method to detrend. Available: linear, constant. See documentation of
+        'scipy.signal.detrend' for details
+
+    Returns
+    -------
+    iris.cube.Cube
+        Detrended cube
+    """
+    # from cf_units import Unit
+
+    cube_detrend = detrend(cube, dimension=dimension, method='linear')
+    cube.data -= cube_detrend.data
+
+    # # if frequency not in ['yr', 'mon', 'day']
+    # if frequency == 'mon':
+    #     frequency = 'month'
+    # cube.units = Unit(str(cube.units) + f' {frequency}-1')
+
+    cube.long_name = cube.long_name + " trend"
+
+    return cube
