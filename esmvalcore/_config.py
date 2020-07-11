10f4d37999cc4adceac80fcdc03bc6fbf0a9b9d7
Javier Vegas-Regidor <javier.vegas@bsc.es>
2020-07-11 11:20:01 +0200

    Options arg in read_config_user_file now optional

--- esmvalcore/_config.py
+++ esmvalcore/_config.py
@@ -31,7 +31,7 @@ def find_diagnostics():
 DIAGNOSTICS_PATH = find_diagnostics()
 
 
-def read_config_user_file(config_file, folder_name, options):
+def read_config_user_file(config_file, folder_name, options=dict()):
     """Read config user file and store settings in a dictionary."""
     config_file = os.path.abspath(
         os.path.expandvars(os.path.expanduser(config_file)))
