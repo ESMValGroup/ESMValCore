b463b4e664f7b6ecdc18e95e47f075ef709af3cc
Manuel Schlund <manuel.schlund@dlr.de>
2020-07-14 16:21:25 +0200

    Added add_plev_from_altitude and add_sigma_factory to public API

--- esmvalcore/cmor/__init__.py
+++ esmvalcore/cmor/__init__.py
@@ -1,1 +1,10 @@
-"""CMOR module"""
+"""CMOR module."""
+
+from esmvalcore.cmor._fixes.shared import (add_plev_from_altitude,
+                                           add_sigma_factory)
+
+
+__all__ = [
+    'add_plev_from_altitude',
+    'add_sigma_factory',
+]
