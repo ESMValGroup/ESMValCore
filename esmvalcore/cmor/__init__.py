cbaf9cf26decb7718218fc617aa8fae298c1cef2
Bouwe Andela <b.andela@esciencecenter.nl>
2020-07-20 12:25:02 +0200

    Move public fix functions to separate module

--- esmvalcore/cmor/__init__.py
+++ esmvalcore/cmor/__init__.py
@@ -1,10 +1,1 @@
 """CMOR module."""
-
-from esmvalcore.cmor._fixes.shared import (add_plev_from_altitude,
-                                           add_sigma_factory)
-
-
-__all__ = [
-    'add_plev_from_altitude',
-    'add_sigma_factory',
-]
