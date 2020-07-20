046d34d8baf4415c6d7702dd3edd897b0abe1dac
Javier Vegas-Regidor <javier.vegas@bsc.es>
2020-07-20 16:27:49 +0200

    Fix diagnostic filter (#713)
    Make syntax to select some diagnostics more intuitive.

--- esmvalcore/_main.py
+++ esmvalcore/_main.py
@@ -382,6 +382,8 @@ class ESMValTool():
         logger.info("Writing program log files to:\n%s", "\n".join(log_files))
 
         cfg['skip-nonexistent'] = skip_nonexistent
+        if isinstance(diagnostics, str):
+            diagnostics = diagnostics.split(' ')
         cfg['diagnostics'] = {
             pattern if TASKSEP in pattern else pattern + TASKSEP + '*'
             for pattern in diagnostics or ()
