55a5c67efe86d44ee2335a1bead2e27693f9f9bb
Nikolay Koldunov <koldunovn@gmail.com>
2020-08-18 17:14:37 +0200

    fix thetao and so in BCC models

--- esmvalcore/cmor/_fixes/cmip6/bcc_esm1.py
+++ esmvalcore/cmor/_fixes/cmip6/bcc_esm1.py
@@ -1,15 +1,16 @@
 """Fixes for BCC-ESM1 model."""
 from ..common import ClFixHybridPressureCoord
 from .bcc_csm2_mr import Tos as BaseTos
-
+from .bcc_csm2_mr import Thetao as BaseThetao
 
 Cl = ClFixHybridPressureCoord
 
-
 Cli = ClFixHybridPressureCoord
-
 
 Clw = ClFixHybridPressureCoord
 
-
 Tos = BaseTos
+
+Thetao = BaseThetao
+
+So = BaseThetao
