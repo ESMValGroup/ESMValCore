df90a4ed75673f27c48d8fd0532e097cf394f015
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-09-11 13:46:12 +0100

    bummer - the test cube didnt have lazy data

--- esmvalcore/preprocessor/_regrid.py
+++ esmvalcore/preprocessor/_regrid.py
@@ -435,7 +435,10 @@ def _vertical_interpolate(cube, src_levels, levels, interpolation,
 
     # force mask onto data as nan's
     if da.ma.getmaskarray(cube.core_data()).any():
-        cube.core_data()[da.ma.getmaskarray(cube.core_data())] = np.nan
+        if cube.has_lazy_data():
+            cube.core_data()[da.ma.getmaskarray(cube.core_data())] = np.nan
+        else:
+            cube.data[cube.data.mask] = np.nan
 
     # Now perform the actual vertical interpolation.
     new_data = stratify.interpolate(
