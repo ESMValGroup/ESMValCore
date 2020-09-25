34361095c9248840188ff241c1ae5adcb31846e1
Lee de Mora <ledm@pml.ac.uk>
2020-09-25 16:29:37 +0100

    Update sam0_unicon.py

--- esmvalcore/cmor/_fixes/cmip6/sam0_unicon.py
+++ esmvalcore/cmor/_fixes/cmip6/sam0_unicon.py
@@ -1,5 +1,6 @@
 """Fixes for SAM0-UNICON model."""
 from ..common import ClFixHybridPressureCoord
+from ..fix import Fix
 
 
 Cl = ClFixHybridPressureCoord
@@ -9,3 +10,22 @@ Cli = ClFixHybridPressureCoord
 
 
 Clw = ClFixHybridPressureCoord
+
+
+class msftmz(Fix):
+    """Fix msftmz."""
+
+    def fix_metadata(self, cubes):
+        """
+        Fix standard and long name.
+        Parameters
+        ----------
+        cube: iris.cube.CubeList
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        for cube in cubes:
+            basin = cube.coord('region')
+            basin.var_name = 'basin'
+        return cubes
