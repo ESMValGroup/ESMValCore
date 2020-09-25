7246176f017aad475fc36ff061be661e37c22a47
Lee de Mora <ledm@pml.ac.uk>
2020-09-25 16:25:20 +0100

    Added amoc fixes for hadgem3 and ukesm

--- esmvalcore/cmor/_fixes/cmip6/hadgem3_gc31_ll.py
+++ esmvalcore/cmor/_fixes/cmip6/hadgem3_gc31_ll.py
@@ -1,6 +1,7 @@
 """Fixes for CMIP6 HadGEM-GC31-LL."""
 from ..common import ClFixHybridHeightCoord
 from .ukesm1_0_ll import AllVars as BaseAllVars
+from ..fix import Fix
 
 
 AllVars = BaseAllVars
@@ -13,3 +14,24 @@ Cli = ClFixHybridHeightCoord
 
 
 Clw = ClFixHybridHeightCoord
+
+
+class msftyz(Fix):
+    """Fix msftyz."""
+
+    def fix_metadata(self, cubes):
+        """
+        Fix standard and long name.
+        Parameters
+        ----------
+        cube: iris.cube.CubeList
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        for cube in cubes:
+            basin = cube.coord('region')
+            basin.var_name = 'basin'
+            print('\nbasin:', basin)
+
+        return cubes
--- esmvalcore/cmor/_fixes/cmip6/ukesm1_0_ll.py
+++ esmvalcore/cmor/_fixes/cmip6/ukesm1_0_ll.py
@@ -30,6 +30,25 @@ class AllVars(Fix):
         return cubes
 
 
+class msftyz(Fix):
+    """Fix msftyz."""
+
+    def fix_metadata(self, cubes):
+        """
+        Fix standard and long name.
+        Parameters
+        ----------
+        cube: iris.cube.CubeList
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        for cube in cubes:
+            basin = cube.coord('region')
+            basin.var_name = 'basin'
+
+        return cubes
+
 Cl = ClFixHybridHeightCoord
 
 
