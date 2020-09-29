b7a3608a55cecbf028712592e99e68ddb23d7aa7
Stef <s.smeets@esciencecenter.nl>
2020-09-29 14:30:10 +0200

    Add documentation

--- esmvalcore/_projects.py
+++ esmvalcore/_projects.py
@@ -11,6 +11,24 @@ logger = logging.getLogger(__name__)
 
 
 class SearchLocation(object):
+    """This class specifies a search location.
+
+    It will construct something like:
+        search_path = `rootpath` / `input_dir` / `input_file`
+
+    This path will be used to search for data. Note that `input_dir` and
+    `input_file` can tags that are replaced by attributes in the variable.
+
+    Parameters
+    ----------
+    rootpath: path-like
+        The root path to look for data
+    input_dir: str
+        Specification of the directory structure inside `rootpath`.
+        Can contain tags.
+    input_file: str
+        Specification of the filename. Can contain tags.
+    """
     def __init__(self, rootpath: str, input_dir: str, input_file: str):
         self.input_dir = input_dir
         self.input_file = input_file
@@ -21,6 +39,7 @@ class SearchLocation(object):
         return f'{self.__class__.__name__}({pformat(dct)})'
 
     def to_dict(self):
+        """Return attributes as a dictionary."""
         return {
             'rootpath': str(self.rootpath),
             'input_dir': self.input_dir,
@@ -28,7 +47,7 @@ class SearchLocation(object):
         }
 
     def _find_input_dirs(self, variable):
-        """Return a the full paths to input directories."""
+        """Return a list of the full paths to input directories."""
         dirnames = []
 
         base_path = self.rootpath
@@ -40,6 +59,7 @@ class SearchLocation(object):
             dirname = os.path.join(base_path, dirname)
             matches = glob.glob(dirname)
             matches = [match for match in matches if os.path.isdir(match)]
+
             if matches:
                 for match in matches:
                     logger.debug("Found %s", match)
@@ -56,6 +76,8 @@ class SearchLocation(object):
         return filenames_glob
 
     def find_input_files(self, variable):
+        """Find all data files according to the defined spec for the given
+        variable."""
         input_dirs = self._find_input_dirs(variable)
         filenames_glob = self._get_filenames_glob(variable)
         files = _data_finder.find_files(input_dirs, filenames_glob)
@@ -68,7 +90,24 @@ class SearchLocation(object):
 
 
 class ProjectData(object):
-    def __init__(self, name, output_file, drs_list):
+    """Organization class to hold information about the project data.
+
+    It contains a list of search locations, which will be used to look for
+    data. It also defines how the output file should be called for a
+    project.
+
+    Parameters
+    ----------
+    name : str
+        Name of the project.
+    output_file : str
+        Definition of the output file. Can include tags.
+    drs_list : list of dicts
+        List of dictionaries containing data reference specifications (i.e.
+        search locations). These will be passed to initialize `SearchLocation`
+        objects.
+    """
+    def __init__(self, name: str, output_file: str, drs_list: list):
         self.name = name
 
         search_locations = []
@@ -96,10 +135,12 @@ class ProjectData(object):
         return self._output_file
 
     def get_cmor_table(self):
+        """Get the CMOR table for this project."""
         from .cmor.table import CMOR_TABLES
         return CMOR_TABLES[self.name]
 
     def get_input_filelist(self, variable):
+        """Look for input files for the given variable."""
         filelist = []
         for search_location in self.search_locations:
             result = search_location.find_input_files(variable)
