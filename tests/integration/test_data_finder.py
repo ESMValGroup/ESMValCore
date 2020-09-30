2b6477ccaa668467ee9bdcce17aecfd40fa940ac
Stef <s.smeets@esciencecenter.nl>
2020-09-30 10:28:03 +0200

    Get 'test_get_output_file' to run

--- tests/integration/test_data_finder.py
+++ tests/integration/test_data_finder.py
@@ -56,7 +56,10 @@ def create_tree(path, filenames=None, symlinks=None):
 @pytest.mark.parametrize('cfg', CONFIG['get_output_file'])
 def test_get_output_file(cfg):
     """Test getting output name for preprocessed files."""
-    output_file = get_output_file(cfg['variable'], cfg['preproc_dir'])
+    from esmvalcore import session
+
+    output_file = session.preproc_dir / get_output_file(cfg['variable'])
+
     assert output_file == cfg['output_file']
 
 
