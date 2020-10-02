71d4893316b855340b73e9788c9ba67187681584
Evgenia Galytska <egalytska@iup.physik.uni-bremen.de>
2020-10-02 09:36:33 +0200

    Fix siconca variable in EC-Earth3 and EC-Earth3-Veg models in amip simulation (#702)
    Co-authored-by: Valeriu Predoi <valeriu.predoi@gmail.com>

---
+++ esmvalcore/cmor/_fixes/cmip6/ec_earth3.py
@@ -0,0 +1,24 @@
+"""Fixes for EC-Earth3 model."""
+from ..fix import Fix
+
+
+class Siconca(Fix):
+    """Fixes for siconca."""
+
+    def fix_data(self, cube):
+        """Fix data.
+
+        Fixes discrepancy between declared units and real units
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+            Input cube which needs to be fixed.
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        cube.data = cube.core_data() * 100.
+        return cube
