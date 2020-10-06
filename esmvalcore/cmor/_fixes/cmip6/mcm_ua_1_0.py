e0134abdc2a5ad12d667ddb69bc4be4cb56b5957
mwjury <martin.w.jury@gmail.com>
2020-10-06 13:58:27 +0200

    Fix MCM-UA-1-0

--- esmvalcore/cmor/_fixes/cmip6/mcm_ua_1_0.py
+++ esmvalcore/cmor/_fixes/cmip6/mcm_ua_1_0.py
@@ -10,6 +10,8 @@ class AllVars(Fix):
 
     def fix_metadata(self, cubes):
         """Rename ``var_name`` of latitude and longitude.
+        Longitude boundary description may be wrong (lons=[0, ..., 356.25];
+        lon_bnds=[[-1.875, 1.875], ..., [354.375, 360]]).
 
         Parameters
         ----------
@@ -36,6 +38,16 @@ class AllVars(Fix):
             if time_units is not None:
                 cube.attributes['parent_time_units'] = time_units.replace(
                     ' (noleap)', '')
+
+        for cube in cubes:
+            if cube.coord('longitude').ndim == 1:
+                lon_bnds = cube.coord('longitude').bounds.copy()
+                if cube.coord('longitude').points[0] == 0. and \
+                    cube.coord('longitude').points[-1] == 356.25 and \
+                    lon_bnds[-1][-1] == 360.:
+                    lon_bnds[-1][-1] = 358.125
+                cube.coord('longitude').bounds = lon_bnds
+
         return cubes
 
 
