a8bfd028a0a8b3f0f51af5ed8c38358d2a0423ce
Manuel Schlund <manuel.schlund@dlr.de>
2020-10-08 10:49:43 +0200

    Fixed derivation of vegfrac

--- esmvalcore/preprocessor/_derive/vegfrac.py
+++ esmvalcore/preprocessor/_derive/vegfrac.py
@@ -1,6 +1,7 @@
 """Derivation of variable `vegFrac`."""
 
-import iris
+from esmvalcore.iris_helpers import var_name_constraint
+
 from ._baseclass import DerivedVariableBase
 
 
@@ -10,16 +11,20 @@ class DerivedVariable(DerivedVariableBase):
     @staticmethod
     def required(project):
         """Declare the variables needed for derivation."""
-        required = [{
-            'short_name': 'baresoilFrac',
-        }]
+        required = [
+            {'short_name': 'baresoilFrac'},
+            {'short_name': 'residualFrac'},
+        ]
         return required
 
     @staticmethod
     def calculate(cubes):
         """Compute vegetation fraction from bare soil fraction."""
-        baresoilfrac_cube = cubes.extract_strict(
-            iris.Constraint(name='area_fraction'))
+        baresoilfrac_cube = cubes.extract_strict(var_name_constraint(
+            'baresoilFrac'))
+        residualfrac = cubes.extract_strict(var_name_constraint(
+            'residualFrac'))
 
-        baresoilfrac_cube.data = 100.0 - baresoilfrac_cube.core_data()
+        baresoilfrac_cube.data = (100.0 - baresoilfrac_cube.core_data() -
+                                  residualfrac.core_data())
         return baresoilfrac_cube
