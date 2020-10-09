a6a7e3f7427f5f636d766c067f8eef7f2fd66b98
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-10-09 14:03:54 +0100

    pep8 bugger off

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from setuptools import Command, setup
 
 sys.path.insert(0, os.path.dirname(__file__))
-from esmvalcore._version import __version__
+from esmvalcore._version import __version__  # noqa: E402
 
 PACKAGES = [
     'esmvalcore',
