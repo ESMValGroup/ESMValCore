43f70826d2035587bcac4a7475934a431464da96
Stef Smeets <s.smeets@esciencecenter.nl>
2020-11-26 16:59:15 +0100

    Add warning for unstable API

--- esmvalcore/future/__init__.py
+++ esmvalcore/future/__init__.py
@@ -1,5 +1,12 @@
 """ESMValCore future API module."""
 
+import warnings
+
+warnings.warn(
+    '\n  Thank you for trying out the new ESMValCore API.'
+    '\n  Note that this API is experimental and may be subject to change.'
+    '\n  More info: https://github.com/ESMValGroup/ESMValCore/issues/498', )
+
 from .config import CFG
 
 __all__ = [
