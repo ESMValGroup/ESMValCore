added test for thetao fix olev units

tests/integration/cmor/_fixes/cmip6/test_cesm2.py
@@ -9,7 +9,7 @@ import pytest
 from cf_units import Unit
 from netCDF4 import Dataset
 
-from esmvalcore.cmor._fixes.cmip6.cesm2 import Cl, Cli, Clw, Tas, Tos
+from esmvalcore.cmor._fixes.cmip6.cesm2 import Cl, Cli, Clw, Tas, Thetao, Tos
 from esmvalcore.cmor.fix import Fix
 from esmvalcore.cmor.table import get_var_info
 
@@ -296,6 +296,36 @@ def tos_cubes():
     return iris.cube.CubeList([tos_cube])
 
 
+@pytest.fixture
+def thetao_cubes():
+    """Cubes to test fixes for ``tos``."""
+    time_coord = iris.coords.DimCoord(
+        [0.0004, 1.09776], var_name='time', standard_name='time',
+        units='days since 1850-01-01 00:00:00')
+    lat_coord = iris.coords.DimCoord(
+        [0.0, 1.0], var_name='lat', standard_name='latitude', units='degrees')
+    lon_coord = iris.coords.DimCoord(
+        [0.0, 1.0], var_name='lon', standard_name='longitude', units='degrees')
+    lev_coord = iris.coords.DimCoord(
+        [500.0, 1000.0], var_name='lev', standard_name=None,
+        units='centimeters')
+    coord_specs = [
+        (time_coord, 0),
+        (lat_coord, 2),
+        (lon_coord, 3),
+        (lev_coord, 1),
+    ]
+    thetao_cube = iris.cube.Cube(
+        np.ones((2, 2, 2, 2)),
+        var_name='thetao',
+        dim_coords_and_dims=coord_specs,
+    )
+    thetao_cube.attributes = {}
+    thetao_cube.attributes['mipTable'] = 'Omon'
+
+    return iris.cube.CubeList([thetao_cube])
+
+
 def test_get_tas_fix():
     """Test getting of fix."""
     fix = Fix.get_fixes('CMIP6', 'CESM2', 'Amon', 'tas')
@@ -306,6 +336,12 @@ def test_get_tos_fix():
     """Test getting of fix."""
     fix = Fix.get_fixes('CMIP6', 'CESM2', 'Amon', 'tos')
     assert fix == [Tos(None)]
+
+
+def test_get_thetao_fix():
+    """Test getting of fix."""
+    fix = Fix.get_fixes('CMIP6', 'CESM2', 'Omon', 'thetao')
+    assert fix == [Thetao(None)]
 
 
 def test_tas_fix_metadata(tas_cubes):
@@ -342,3 +378,14 @@ def test_tos_fix_metadata(tos_cubes):
     assert out_cubes is tos_cubes
     for cube in out_cubes:
         np.testing.assert_equal(cube.coord("time").points, [0.,  1.1])
+
+
+def test_thetao_fix_metadata(thetao_cubes):
+    """Test ``fix_metadata`` for ``thetao``."""
+    vardef = get_var_info('CMIP6', 'Omon', 'thetao')
+    fix = Thetao(vardef)
+    out_cubes = fix.fix_metadata(thetao_cubes)
+    assert out_cubes is thetao_cubes
+    expected_levs = [5., 10.]
+    for cube in out_cubes:
+        np.testing.assert_equal(cube.coord("lev").points, expected_levs)