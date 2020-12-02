3d915cf08a6515f6dbf3a9a54e96f1bfb4555d0e
Manuel Schlund <manuel.schlund@dlr.de>
2020-12-02 17:05:09 +0100

    Added add_altitude_from_plev to public API

--- tests/unit/cmor/test_fixes.py
+++ tests/unit/cmor/test_fixes.py
@@ -1,12 +1,17 @@
 """Test individual fix functions."""
 import pytest
 
-import esmvalcore.cmor.fixes
+import esmvalcore.cmor._fixes.shared as shared
+import esmvalcore.cmor.fixes as fixes
 
 
 @pytest.mark.parametrize('func', [
+    'add_altitude_from_plev',
     'add_plev_from_altitude',
     'add_sigma_factory',
 ])
 def test_imports(func):
-    assert func in esmvalcore.cmor.fixes.__all__
+    assert func in fixes.__all__
+    fn_in_shared = getattr(shared, func)
+    fn_in_fixes = getattr(fixes, func)
+    assert fn_in_shared is fn_in_fixes
