f0d91fe7a837fae0992c16c6ab00257df73ffc03
Stef Smeets <s.smeets@esciencecenter.nl>
2020-12-09 12:15:09 +0100

    Expose functions to experimental API

--- esmvalcore/experimental/__init__.py
+++ esmvalcore/experimental/__init__.py
@@ -8,7 +8,13 @@ warnings.warn(
     '\n  More info: https://github.com/ESMValGroup/ESMValCore/issues/498', )
 
 from .config import CFG  # noqa: E402
+from .recipe_info import RecipeInfo  # noqa: E402
+from .utils import find_recipes, get_recipes_list  # noqa: E402
 
 __all__ = [
     'CFG',
+    'find_recipes',
+    'get_recipes_list',
+    'find_recipes',
+    'RecipeInfo',
 ]
