80c08a57f5f7eb5c4f63355de68b6bdbd8fd05cc
Stef Smeets <s.smeets@esciencecenter.nl>
2020-12-11 11:50:22 +0100

    Make `find_recipes` a method on RecipeList
    RecipeList is a container for recipes.

--- esmvalcore/experimental/__init__.py
+++ esmvalcore/experimental/__init__.py
@@ -9,11 +9,11 @@ warnings.warn(
 
 from .config import CFG  # noqa: E402
 from .recipe_info import RecipeInfo  # noqa: E402
-from .utils import find_recipes, get_recipes  # noqa: E402
+from .utils import RecipeList, get_recipes  # noqa: E402
 
 __all__ = [
     'CFG',
     'get_recipes',
-    'find_recipes',
     'RecipeInfo',
+    'RecipeList',
 ]
--- esmvalcore/experimental/utils.py
+++ esmvalcore/experimental/utils.py
@@ -8,39 +8,39 @@ from esmvalcore._config import DIAGNOSTICS_PATH
 from .recipe_info import RecipeInfo
 
 
+class RecipeList(list):
+    """Container for recipes."""
+    def find(self, query: str):
+        """Search for recipes matching the search query or pattern.
+
+        This function will search the recipe description, author list, and
+        project information.
+
+        Parameters
+        ----------
+        query : str
+            String to search for, e.g. `find_recipes('righi')` will return all
+            matching that author. Can be a `regex` pattern.
+
+        Returns
+        -------
+        recipes : RecipeList
+            Returns a list of recipes matching the search query.
+        """
+        query = re.compile(query, flags=re.IGNORECASE)
+
+        matches = RecipeList()
+
+        for recipe in self:
+            match = re.search(query, str(recipe))
+            if match:
+                matches.append(recipe)
+
+        return matches
+
+
 def get_recipes() -> list:
     """Return a list of all available recipes."""
     rootdir = Path(DIAGNOSTICS_PATH, 'recipes')
     files = rootdir.glob('**/*.yml')
-    return [RecipeInfo(file) for file in files]
-
-
-def find_recipes(query: str) -> list:
-    """Search for recipes matching the search query or pattern.
-
-    This function will search the recipe description, author list, and
-    project information.
-
-    Parameters
-    ----------
-    query : str
-        String to search for, e.g. `find_recipes('righi')` will return all
-        matching that author. Can be a `regex` pattern.
-
-    Returns
-    -------
-    recipes : list
-        Returns a list of recipes matching the search query.
-    """
-    recipes = get_recipes()
-
-    query = re.compile(query, flags=re.IGNORECASE)
-
-    result = []
-
-    for recipe in recipes:
-        match = re.search(query, str(recipe))
-        if match:
-            result.append(recipe)
-
-    return result
+    return RecipeList(RecipeInfo(file) for file in files)
