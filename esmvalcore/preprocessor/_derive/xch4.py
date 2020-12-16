60ebfbfa024a09afd1ee9fd9450ccc50007ed34a
Manuel Schlund <manuel.schlund@dlr.de>
2020-12-16 17:28:07 +0100

    Added units tests for derivation of xch4

--- esmvalcore/preprocessor/_derive/xch4.py
+++ esmvalcore/preprocessor/_derive/xch4.py
@@ -1,17 +1,9 @@
 """Derivation of variable `xch4`."""
 
-import iris
 from iris import Constraint
-import numpy as np
-from scipy import constants
 
 from ._baseclass import DerivedVariableBase
-from ._shared import pressure_level_widths
-
-# Constants
-FAIR_COR = 3.0825958e-6
-MW_AIR = 28.9644e-3
-AVOGADRO_CONST = constants.value('Avogadro constant')
+from ._shared import column_average
 
 
 class DerivedVariable(DerivedVariableBase):
@@ -30,57 +22,15 @@ class DerivedVariable(DerivedVariableBase):
 
     @staticmethod
     def calculate(cubes):
-        """Calculate the average-column atmospheric CH4 [1e-9].
-
-        The calculation follows the method described in the obs4MIPs
-        technical note "Merged SCIMACHY/ENVISAT and TANSO-FTS/GOSAT
-        atmospheric column-average dry-air mole fraction of CH4 (XCH4)"
-        by M. Buchwitz and M. Reuter, available at:
-        http://esgf-data1.ceda.ac.uk/thredds/fileServer/esg_obs4mips/
-              esacci/ghg/data/obs4mips/crdp_3/CH4/v100/
-              TechNote_SCIAGOSAT_L3_CRDP3_001_XCH4_FINAL.pdf
-        """
+        """Calculate the column-averaged atmospheric CH4 [1e-9]."""
         ch4_cube = cubes.extract_strict(
             Constraint(name='mole_fraction_of_methane_in_air'))
         hus_cube = cubes.extract_strict(Constraint(name='specific_humidity'))
         zg_cube = cubes.extract_strict(Constraint(name='geopotential_height'))
         ps_cube = cubes.extract_strict(Constraint(name='surface_air_pressure'))
 
-        # level thickness (note: Buchwitz & Reuter use hPa but we use Pa;
-        # in fact, this does not matter as units cancel out when calculating
-        # xch4)
-        p_layer_widths = pressure_level_widths(ch4_cube, ps_cube,
-                                               top_limit=0.0)
-
-        # latitudes (1-dim array)
-        lat = ch4_cube.coord('latitude').points
-
-        # gravitational acceleration g_0 on the geoid approximated by the
-        # international gravity formula depending only on the latitude
-        g_0 = np.array(lat)
-        g_0 = 9.780327 * (1. + 0.0053024 * (np.sin(lat / 180. * np.pi))**2
-                          - 0.0000058 * (np.sin(2. * lat / 180. * np.pi))**2)
-
-        # approximation of the gravitational acceleration including the
-        # free air correction
-        # note: the formula for g given in Buchwitz & Reuter contains a typo
-        #       and should read: g_0**2 - 2*f*zg (i.e. minus instead of +)
-        g_4d_array = iris.util.broadcast_to_shape(g_0**2, zg_cube.shape, [2])
-        g_4d_array = np.sqrt(g_4d_array.data - 2. * FAIR_COR * zg_cube.data)
-
-        # number of dry air particles (air molecules excluding water vapor)
-        # within each layer
-        n_dry = ((hus_cube * -1.0 + 1.0) * AVOGADRO_CONST *
-                 p_layer_widths.data / (MW_AIR * g_4d_array))
-
-        # number of CH4 molecules per layer
-        ch4_cube = ch4_cube * n_dry
-
-        # column-average CH4
-        xch4_cube = (
-            ch4_cube.collapsed('air_pressure', iris.analysis.SUM) /
-            n_dry.collapsed('air_pressure', iris.analysis.SUM))
-        xch4_cube.units = ch4_cube.units
-        xch4_cube.convert_units("1")
+        # Column-averaged CH4
+        xch4_cube = column_average(ch4_cube, hus_cube, zg_cube, ps_cube)
+        xch4_cube.convert_units('1')
 
         return xch4_cube
--- tests/unit/preprocessor/_derive/test_shared.py
+++ tests/unit/preprocessor/_derive/test_shared.py
@@ -1,9 +1,60 @@
 """Unit tests for shared variable derivation functions."""
 
+import iris
 import numpy as np
 import pytest
 
-from esmvalcore.preprocessor._derive._shared import _get_pressure_level_widths
+from esmvalcore.preprocessor._derive._shared import (
+    _get_pressure_level_widths,
+    column_average,
+)
+
+
+def get_cube(data, air_pressure_coord=True, **kwargs):
+    """Get sample cube."""
+    time_coord = iris.coords.DimCoord([0.0], standard_name='time',
+                                      var_name='time',
+                                      units='days since 1950-01-01 00:00:00')
+    plev_coord = iris.coords.DimCoord([90000.0, 80000.0],
+                                      standard_name='air_pressure',
+                                      var_name='plev', units='Pa')
+    lat_coord = iris.coords.DimCoord([45.0], standard_name='latitude',
+                                     var_name='lat', units='degrees')
+    lon_coord = iris.coords.DimCoord([10.0], standard_name='longitude',
+                                     var_name='lon', units='degrees')
+    if air_pressure_coord:
+        coord_specs = [(time_coord, 0), (plev_coord, 1), (lat_coord, 2),
+                       (lon_coord, 3)]
+    else:
+        coord_specs = [(time_coord, 0), (lat_coord, 1), (lon_coord, 2)]
+    cube = iris.cube.Cube(data, dim_coords_and_dims=coord_specs, **kwargs)
+    return cube
+
+
+def test_column_average():
+    """Test calculation of column-average."""
+    cube = get_cube([[[[1.0]], [[2.0]]]], air_pressure_coord=True,
+                    var_name='ch4', units='1')
+    hus_cube = get_cube([[[[20.0]], [[20.0]]]], air_pressure_coord=True,
+                        var_name='hus', units='%')
+    zg_cube = get_cube([[[100.0]]], air_pressure_coord=False, var_name='zg',
+                       units='m')
+    ps_cube = get_cube([[[100000.0]]], air_pressure_coord=False, var_name='ps',
+                       units='Pa')
+    x_cube = column_average(cube, hus_cube, zg_cube, ps_cube)
+    assert x_cube.shape == (1, 1, 1)
+    assert x_cube.units == '1'
+    assert x_cube.coords('time')
+    assert x_cube.coords('air_pressure')
+    assert x_cube.coords('latitude')
+    assert x_cube.coords('longitude')
+    np.testing.assert_allclose(x_cube.data, [[[1.85]]])
+    np.testing.assert_allclose(x_cube.coord('time').points, [0.0])
+    np.testing.assert_allclose(x_cube.coord('air_pressure').points, 85000.0)
+    np.testing.assert_allclose(x_cube.coord('air_pressure').bounds,
+                               [[80000.0, 90000.0]])
+    np.testing.assert_allclose(x_cube.coord('latitude').points, [45.0])
+    np.testing.assert_allclose(x_cube.coord('longitude').points, [10.0])
 
 
 def test_col_is_not_monotonic():
