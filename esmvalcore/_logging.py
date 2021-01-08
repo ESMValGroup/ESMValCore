64f931f0cde7894bb27e41966a861b82a6ac9d47
Stef Smeets <s.smeets@esciencecenter.nl>
2021-01-08 11:08:47 +0100

    Refactor logging configuration

--- esmvalcore/_logging.py
+++ esmvalcore/_logging.py
@@ -3,41 +3,87 @@
 import logging
 import os
 import time
+from pathlib import Path
 
 import yaml
 
 
-def configure_logging(cfg_file=None, output_dir=None, console_log_level=None):
-    """Set up logging."""
-    if cfg_file is None:
-        cfg_file = os.path.join(os.path.dirname(__file__),
-                                'config-logging.yml')
+def _purge_handlers_with_filename(cfg: dict) -> None:
+    """Removes handlers with filename set.
 
-    cfg_file = os.path.abspath(cfg_file)
+    This is used to remove file handlers which require an output
+    directory to be set.
+    """
+    cfg['handlers'] = {
+        name: handler
+        for name, handler in cfg['handlers'].items()
+        if 'filename' not in handler
+    }
+    prev_root = cfg['root']['handlers']
+    cfg['root']['handlers'] = [
+        name for name in prev_root if name in cfg['handlers']
+    ]
+
+
+def _get_log_files(cfg: dict, output_dir: str = None) -> list:
+    """Initialize log files for the file handlers."""
+    log_files = []
+
+    handlers = cfg['handlers']
+
+    for handler in handlers.values():
+        filename = handler.get('filename', None)
+
+        if filename:
+            if not os.path.isabs(filename):
+                handler['filename'] = os.path.join(output_dir, filename)
+            log_files.append(handler['filename'])
+
+    return log_files
+
+
+def _update_stream_level(cfg: dict, level=None):
+    """Update the stream level for the stream handlers."""
+    handlers = cfg['handlers']
+
+    for handler in handlers.values():
+        if level is not None and 'stream' in handler:
+            if handler['stream'] in ('ext://sys.stdout', 'ext://sys.stderr'):
+                handler['level'] = level.upper()
+
+
+def configure_logging(cfg_file: str = None,
+                      output_dir: str = None,
+                      console_log_level: str = None) -> list:
+    """Configure logging.
+
+    Parameters
+    ----------
+    cfg_file : str, optional
+        Logging config file. If `None`, defaults to `configure-logging.yml`
+    output_dir : str, optional
+        Output directory for the log files. If `None`, log only to the console.
+    console_log_level : str, optional
+        If `None`, use the default (INFO).
+
+    Returns
+    -------
+    log_files : list
+        Filenames that will be logged to.
+    """
+    if cfg_file is None:
+        cfg_file = Path(__file__).parent / 'config-logging.yml'
+
+    cfg_file = Path(cfg_file).absolute()
+
     with open(cfg_file) as file_handler:
         cfg = yaml.safe_load(file_handler)
 
     if output_dir is None:
-        cfg['handlers'] = {
-            name: handler
-            for name, handler in cfg['handlers'].items()
-            if 'filename' not in handler
-        }
-        prev_root = cfg['root']['handlers']
-        cfg['root']['handlers'] = [
-            name for name in prev_root if name in cfg['handlers']
-        ]
+        _purge_handlers_with_filename(cfg)
 
-    log_files = []
-    for handler in cfg['handlers'].values():
-        if 'filename' in handler:
-            if not os.path.isabs(handler['filename']):
-                handler['filename'] = os.path.join(output_dir,
-                                                   handler['filename'])
-            log_files.append(handler['filename'])
-        if console_log_level is not None and 'stream' in handler:
-            if handler['stream'] in ('ext://sys.stdout', 'ext://sys.stderr'):
-                handler['level'] = console_log_level.upper()
+    log_files = _get_log_files(cfg, output_dir=output_dir)
+    _update_stream_level(cfg, level=console_log_level)
 
     logging.config.dictConfig(cfg)
     logging.Formatter.converter = time.gmtime
