01e9a0a6fe853edf0b1413b10fbe37f4cd149ec5
Bettina Gier <gier@uni-bremen.de>
2021-01-15 12:01:46 +0100

    add fixes

--- esmvalcore/cmor/_fixes/cmip6/access_esm1_5.py
+++ esmvalcore/cmor/_fixes/cmip6/access_esm1_5.py
@@ -1,7 +1,45 @@
 """Fixes for ACCESS-ESM1-5."""
 import iris
+import numpy as np
 
 from ..common import ClFixHybridHeightCoord
+from ..fix import Fix
+
+class Zg(Fix):
+    """Fixes for zg."""
+    def fix_metadata(self, cubes):
+        """Correctly round air pressure coordinate.
+        Parameters
+        ----------
+        cube : iris.cube.CubeList
+        Returns
+        -------
+        iris.cube.Cube
+        """
+        cube = self.get_cube_from_list(cubes)
+        cube.coord('air_pressure').points = \
+            np.round(cube.coord('air_pressure').points, 0)
+        cube.coord('air_pressure').bounds = \
+            np.round(cube.coord('air_pressure').bounds, 0)
+        return cubes
+
+class Hus(Fix):
+    """Fixes for hus."""
+    def fix_metadata(self, cubes):
+        """Correctly round air pressure coordinate.
+        Parameters
+        ----------
+        cube : iris.cube.CubeList
+        Returns
+        -------
+        iris.cube.Cube
+        """
+        cube = self.get_cube_from_list(cubes)
+        cube.coord('air_pressure').points = \
+            np.round(cube.coord('air_pressure').points, 0)
+        cube.coord('air_pressure').bounds = \
+            np.round(cube.coord('air_pressure').bounds, 0)
+        return cubes
 
 
 class Cl(ClFixHybridHeightCoord):
---
+++ esmvalcore/cmor/_fixes/cmip6/canesm5.py
@@ -0,0 +1,43 @@
+"""Fixes for CanESM5 model."""
+from ..fix import Fix
+import numpy as np
+
+
+class Co2(Fix):
+    """Fixes for co2."""
+    def fix_metadata(self, cubes):
+        """Converts units from ppmv to 1.
+
+        Parameters
+        ----------
+        cube : iris.cube.CubeList
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        cube = self.get_cube_from_list(cubes)
+        metadata = cube.metadata
+        cube *= 1.e-6
+        cube.metadata = metadata
+        return cubes
+
+class Gpp(Fix):
+    """Fixes for fgco2, land values set to 0 instead of masked."""
+
+    def fix_data(self, cube):
+        """
+        Fix data.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        cube.data = np.ma.masked_where(cube.data==0, cube.data)
+        return cube
