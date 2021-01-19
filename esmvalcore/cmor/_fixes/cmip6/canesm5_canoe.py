715842e4bd3b54ed4cefa0a73134bd53bff1159b
Bettina Gier <gier@uni-bremen.de>
2021-01-19 11:25:49 +0100

    import canesm5 fix

--- esmvalcore/cmor/_fixes/cmip6/canesm5_canoe.py
+++ esmvalcore/cmor/_fixes/cmip6/canesm5_canoe.py
@@ -1,44 +1,12 @@
 """Fixes for CanESM5-CanOE model."""
-import numpy as np
-from ..fix import Fix
+
+from .canesm5 import Co2 as BaseCO2
+from .canesm5 import Gpp as BaseGpp
 
 
-class Co2(Fix):
+class Co2(BaseCO2):
     """Fixes for co2."""
 
-    def fix_metadata(self, cubes):
-        """Correct units from ppmv to 1.
 
-        Parameters
-        ----------
-        cubes : iris.cube.CubeList
-
-        Returns
-        -------
-        iris.cube.Cube
-
-        """
-        cube = self.get_cube_from_list(cubes)
-        metadata = cube.metadata
-        cube *= 1.e-6
-        cube.metadata = metadata
-        return cubes
-
-
-class Gpp(Fix):
-    """Fixes for gpp, ocean values set to 0 instead of masked."""
-
-    def fix_data(self, cube):
-        """Fix masked values.
-
-        Parameters
-        ----------
-        cube: iris.cube.Cube
-
-        Returns
-        -------
-        iris.cube.Cube
-
-        """
-        cube.data = np.ma.masked_where(cube.data == 0, cube.data)
-        return cube
+class Gpp(BaseGpp):
+    """Fixes for gpp."""
