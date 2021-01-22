86d170ba74e74a05ef482958af9ed87430af71bc
Breixo Soliño <Breixo.SolinoFernandez@dlr.de>
2021-01-22 07:21:18 +0100

    Emac fix runs successfully

--- esmvalcore/cmor/_fixes/native6/emac.py
+++ esmvalcore/cmor/_fixes/native6/emac.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 
 class AllVars(Fix):
 
-  var_mappings = { #CMOR : EMAC
-    "awhea" : "awhea_ave"
+  var_mappings = { # EMAC : CMOR
+    "awhea_ave" : "awhea"
   }
 
 
@@ -21,7 +21,12 @@ class AllVars(Fix):
 
 
   def fix_metadata(self, cubes):
-    logger.info(f"\nFix metadata\n==================\n{cubes}\n")
+#    logger.info(f"\nFix metadata\n==================\n{cubes}\n")
+    for cube in cubes:
+      logger.info(f"{cube.var_name}")
+      if cube.var_name in self.var_mappings.keys():
+        logger.info(f"{cube.var_name} = {self.var_mappings[cube.var_name]}")
+        cube.var_name = self.var_mappings[cube.var_name]
     return cubes
 
 
