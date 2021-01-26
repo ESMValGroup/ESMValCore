a1a540f7efb294c6a3ec680f0c80b7ee4fa6fe20
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-01-26 14:08:34 +0000

    relax to defaults

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ REQUIREMENTS = {
         'pybtex',
         'pyyaml',
         'requests',
-        'scitools-iris>=3',
+        'scitools-iris>=2.2.1',
         'requests',
         'shapely[vectorized]',
         'stratify',
