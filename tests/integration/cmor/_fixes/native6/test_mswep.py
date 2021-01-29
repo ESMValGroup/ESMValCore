44128b1e4ad1f6eb1c3c9e9c6374606abc15b35d
Stef Smeets <s.smeets@esciencecenter.nl>
2021-01-29 12:14:58 +0100

    Add tests for MSWEP data [WIP]

---
+++ tests/integration/cmor/_fixes/native6/test_mswep.py
@@ -0,0 +1,89 @@
+"""Tests for the fixes of ERA5."""
+from pathlib import Path
+
+import iris
+import pytest
+
+from esmvalcore.cmor._fixes.native6.mswep import (
+    Pr,
+    fix_time_day,
+    fix_time_month,
+)
+from esmvalcore.cmor.fix import Fix
+from esmvalcore.cmor.table import CMOR_TABLES
+
+
+@pytest.mark.parametrize('mip_table', ('Amon', 'day'))
+def test_get_pr_fix(mip_table):
+    """Test whether the right fix gets found."""
+    fix = Fix.get_fixes('native6', 'MSWEP', mip_table, 'pr')
+    assert fix == [Pr(None)]
+
+
+@pytest.fixture
+def cube_month():
+    """Return extract from mswep monthly data (shape 3x5x5)."""
+    # out = cube[0:3, 0:360:72, 0:720:144]
+    # iris.save(out, 'mswep_month.nc')
+    path = Path(__file__).with_name('mswep_month.nc')
+    return iris.load_cube(str(path))
+
+
+@pytest.fixture
+def cube_day():
+    """Return extract from mswep daily data (shape 3x5x5)."""
+    # out = cube[0:3, 0:360:72, 0:720:144]
+    # iris.save(out, 'mswep_day.nc')
+    path = Path(__file__).with_name('mswep_day.nc')
+    return iris.load_cube(str(path))
+
+
+@pytest.fixture
+def fix_month():
+    """Return fix for monthly pr data."""
+    cmor_table = CMOR_TABLES['native6']
+    vardef = cmor_table.get_variable('Amon', 'pr')
+    return Pr(vardef)
+
+
+@pytest.fixture
+def fix_day():
+    """Return fix for daily pr data."""
+    cmor_table = CMOR_TABLES['native6']
+    vardef = cmor_table.get_variable('day', 'pr')
+    return Pr(vardef)
+
+
+def test_fix_time_month(cube_month):
+    """Test `fix_time_month`."""
+    fix_time_month(cube_month)
+
+
+def test_fix_time_day(cube_day):
+    """Test `fix_time_day`."""
+    fix_time_day(cube_day)
+
+
+def test_fix_units_month(fix_month, cube_month):
+    """Test `Pr._fix_units_month`."""
+    fix_month._fix_units_month(cube_month)
+
+
+def test_fix_units_day(fix_day, cube_day):
+    """Test `Pr._fix_units_day`."""
+    fix_day._fix_units_day(cube_day)
+
+
+def test_fix_longitude(fix_month, cube_month):
+    """Test `Pr._fix_longitude`."""
+    fix_month._fix_longitude(cube_month)
+
+
+def test_fix_bounds(fix_month, cube_month):
+    """Test `Pr._fix_bounds`."""
+    fix_month._fix_bounds(cube_month)
+
+
+def test_fix_names(fix_month, cube_month):
+    """Test `Pr._fix_names`."""
+    fix_month._fix_names(cube_month)
