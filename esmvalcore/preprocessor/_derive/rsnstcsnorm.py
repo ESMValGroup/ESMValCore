c26427c9ca2adfa5f0e553dd39bb648372c64f88
Stef Smeets <stefsmeets@users.noreply.github.com>
2021-01-31 18:40:40 +0000

    Switch to iris3 (#819)
    * Fix AttributeError: 'DimCoord' object has no attribute '_as_defn', use new attribute .metadata instead
    
    * Replace .extract_strict -> .extract_cube
    
    * Pin iris version to 3.0.1, because the changes are not backward compatible
    
    * Fix tests failing with 'ValueError: Invalid units: sigma must be dimensionless.'
    
    * Rename .coord_comparison -> ._dimensional_metadata_comparison
    
    * Solve CoordinateNotFoundError in test
    
    * Added automatic fix for invalid sigma units in AtmosphereSigmaFactory
    
    * Try to avoid outdated pytest cache
    
    * Fixed custom AtmosphereSigmaFactory
    
    * unpin cftime
    
    Co-authored-by: Valeriu Predoi <valeriu.predoi@gmail.com>
    Co-authored-by: Javier Vegas-Regidor <javier.vegas@bsc.es>
    Co-authored-by: Manuel Schlund <manuel.schlund@dlr.de>
    Co-authored-by: Bouwe Andela <b.andela@esciencecenter.nl>

--- esmvalcore/preprocessor/_derive/rsnstcsnorm.py
+++ esmvalcore/preprocessor/_derive/rsnstcsnorm.py
@@ -4,8 +4,9 @@ authors:
     - weig_ka
 
 """
-from iris import Constraint
 from cf_units import Unit
+from iris import Constraint
+
 from ._baseclass import DerivedVariableBase
 
 
@@ -40,19 +41,20 @@ class DerivedVariable(DerivedVariableBase):
         assuming clear sky normalized by
         the incoming shortwave flux at the top of the atmosphere.
         """
-        rsdscs_cube = cubes.extract_strict(
+        rsdscs_cube = cubes.extract_cube(
             Constraint(name='surface_downwelling_shortwave_flux_in_air_' +
                        'assuming_clear_sky'))
-        rsdt_cube = cubes.extract_strict(
+        rsdt_cube = cubes.extract_cube(
             Constraint(name='toa_incoming_shortwave_flux'))
-        rsuscs_cube = cubes.extract_strict(
+        rsuscs_cube = cubes.extract_cube(
             Constraint(name='surface_upwelling_shortwave_flux_in_air_' +
                        'assuming_clear_sky'))
-        rsutcs_cube = cubes.extract_strict(
+        rsutcs_cube = cubes.extract_cube(
             Constraint(name='toa_outgoing_shortwave_flux_assuming_clear_sky'))
 
         rsnstcsnorm_cube = (((rsdt_cube - rsutcs_cube) -
                              (rsdscs_cube - rsuscs_cube)) / rsdt_cube) * 100.0
         rsnstcsnorm_cube.units = Unit('percent')
+        rsnstcsnorm_cube.attributes.pop('positive', None)
 
         return rsnstcsnorm_cube
