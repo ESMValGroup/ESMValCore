28d7bb12c4418aa6150dec66a88932cc3dfd5808
sloosvel <saskia.loosveldt@bsc.es>
2021-02-09 16:44:24 +0100

    Fix flake again

--- tests/integration/preprocessor/_other/test_add_cell_measure.py
+++ tests/integration/preprocessor/_other/test_add_cell_measure.py
@@ -105,7 +105,7 @@ class Test:
             cube = add_cell_measure(
                 cube, {fx_var: fx_file}, 'CMIP6',
                 'EC-Earth3', CheckLevels.IGNORE)
-            assert cube.cell_measure(self.fx_volume.standard_name) is not None 
+            assert cube.cell_measure(self.fx_volume.standard_name) is not None
 
     def test_no_cell_measure(self):
         cube = iris.cube.Cube(self.new_cube_3D_data,
