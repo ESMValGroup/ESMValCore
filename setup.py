7e8302a4ff07d84eb24c43cd9d9a93de1270f609
Javier Vegas-Regidor <javier.vegas@bsc.es>
2021-02-10 12:16:14 +0100

    Apply suggestions from code review
    Co-authored-by: Bouwe Andela <b.andela@esciencecenter.nl>

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ REQUIREMENTS = {
         'pytest-metadata>=1.5.1',
         'pytest-mock',
         'pytest-xdist',
-        'ESMValTool_sample_data>=v0.0.3',
+        'ESMValTool_sample_data==0.0.3',
     ],
     # Development dependencies
     # Use pip install -e .[develop] to install in development mode
