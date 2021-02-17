e7de86b7ed3d63e236f79b105a0af83ea106a9a8
Martin Jury <martin.w.jury@gmail.com>
2021-02-17 14:29:24 +0100

    Add CMIP5 SMILEs (MMLEA) fixes

---
+++ esmvalcore/cmor/_fixes/cmip5lens/__init__.py
@@ -0,0 +1,4 @@
+"""Fixes for CMIP5 LENS data from the MMLEA inventory.
+
+https://www.cesm.ucar.edu/projects/community-projects/MMLEA/
+"""
---
+++ esmvalcore/cmor/_fixes/cmip5lens/gfdl_cm3.py
@@ -0,0 +1,27 @@
+"""Fixes for the ACCESS1-0 model."""
+from esmvalcore.preprocessor._shared import guess_bounds
+
+from ..fix import Fix
+
+class AllVars(Fix):
+    """Common fixes to all vars."""
+
+    def fix_metadata(self, cubes):
+        """
+        Fix metadata.
+
+        Fixes wrong calendar 'gregorian' instead of 'proleptic_gregorian'.
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes which need to be fixed.
+
+        Returns
+        -------
+        iris.cube.CubeList
+
+        """
+        for cube in cubes:
+            cube = guess_bounds(cube, ['time'])
+        return cubes
