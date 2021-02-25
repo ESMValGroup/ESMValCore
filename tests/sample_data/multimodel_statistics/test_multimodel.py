159759a83ed3ab192ca49f22052be8e650c1dc50
Stef Smeets <s.smeets@esciencecenter.nl>
2021-02-25 11:19:03 +0100

    Use nearest scheme to avoid interpolation errors with masked data

--- tests/sample_data/multimodel_statistics/test_multimodel.py
+++ tests/sample_data/multimodel_statistics/test_multimodel.py
@@ -49,7 +49,7 @@ def preprocess_data(cubes, time_slice: dict = None):
     # regrid to first cube
     regrid_kwargs = {
         'grid': first_cube,
-        'scheme': iris.analysis.Linear(),
+        'scheme': iris.analysis.Nearest(),
     }
 
     cubes = [cube.regrid(**regrid_kwargs) for cube in cubes]
@@ -61,7 +61,8 @@ def get_cache_key(value):
     """Get a cache key that is hopefully unique enough for unpickling.
 
     If this doesn't avoid problems with unpickling the cached data,
-    manually clean the pytest cache with the command `pytest --cache-clear`.
+    manually clean the pytest cache with the command `pytest --cache-
+    clear`.
     """
     return ' '.join([
         str(value),
