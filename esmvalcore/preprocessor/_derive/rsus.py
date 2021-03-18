241c59bbb44026f694a2fce09c7ff017f03939f7
Lukas Brunner <lukas.brunner@env.ethz.ch>
2021-03-18 09:18:19 +0100

    Add derived variable rsus

---
+++ esmvalcore/preprocessor/_derive/rsus.py
@@ -0,0 +1,38 @@
+"""Derivation of variable `rsus`.
+
+authors:
+    - lukas_brunner
+
+"""
+from iris import Constraint
+
+from ._baseclass import DerivedVariableBase
+
+
+class DerivedVariable(DerivedVariableBase):
+    """Derivation of variable `rsus`."""
+
+    @staticmethod
+    def required(project):
+        """Declare the variables needed for derivation."""
+        required = [
+            {
+                'short_name': 'rsds'
+            },
+            {
+                'short_name': 'rsns'
+            },
+        ]
+        return required
+
+    @staticmethod
+    def calculate(cubes):
+        """Compute upwelling shortwave flux from downwelling and net."""
+        rsds_cube = cubes.extract(
+            Constraint(name='surface_downwelling_shortwave_flux_in_air'))
+        rsns_cube = cubes.extract(
+            Constraint(name='surface_net_downward_shortwave_flux'))
+
+        rsus_cube = rsds_cube - rsns_cube
+
+        return rsus_cube
