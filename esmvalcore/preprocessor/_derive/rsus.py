ee02e2bd111a2f6c1a4ba7f202e5246aa5eeecc6
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-03-22 14:48:40 +0000

    fix cube extraction and convert var_names

--- esmvalcore/preprocessor/_derive/rsus.py
+++ esmvalcore/preprocessor/_derive/rsus.py
@@ -28,10 +28,13 @@ class DerivedVariable(DerivedVariableBase):
     @staticmethod
     def calculate(cubes):
         """Compute upwelling shortwave flux from downwelling and net."""
-        rsds_cube = cubes.extract(
+        rsds_cube = cubes.extract_cube(
             Constraint(name='surface_downwelling_shortwave_flux_in_air'))
-        rsns_cube = cubes.extract(
+        rsns_cube = cubes.extract_cube(
             Constraint(name='surface_net_downward_shortwave_flux'))
+        # fix latitude and longitude var_name
+        rsns_cube.coord("longitude").var_name = "Longitude"
+        rsns_cube.coord("latitude").var_name = "Latitude"
 
         rsus_cube = rsds_cube - rsns_cube
 
