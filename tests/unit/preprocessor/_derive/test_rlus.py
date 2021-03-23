e22bf7829493f9e7fa23204ef329e6052c07d51a
Lukas Brunner <lukas.brunner@env.ethz.ch>
2021-03-23 17:16:47 +0100

    Adjust variable names

--- tests/unit/preprocessor/_derive/test_rlus.py
+++ tests/unit/preprocessor/_derive/test_rlus.py
@@ -10,25 +10,25 @@ from .test_shared import get_cube
 
 @pytest.fixture
 def cubes():
-    rsds_name = 'surface_downwelling_longwave_flux_in_air'
-    rsns_name = 'surface_net_downward_longwave_flux'
-    rsds_cube = get_cube([[[100.]]],
+    rlds_name = 'surface_downwelling_longwave_flux_in_air'
+    rlns_name = 'surface_net_downward_longwave_flux'
+    rlds_cube = get_cube([[[100.]]],
                          air_pressure_coord=False,
-                         standard_name=rsds_name)
-    rsds_cube.attributes["positive"] = "down"
-    rsns_cube = get_cube([[[50.0]]],
+                         standard_name=rlds_name)
+    rlds_cube.attributes["positive"] = "down"
+    rlns_cube = get_cube([[[50.0]]],
                          air_pressure_coord=False,
-                         standard_name=rsns_name)
-    rsns_cube.attributes["positive"] = "down"
+                         standard_name=rlns_name)
+    rlns_cube.attributes["positive"] = "down"
 
-    rsns_cube.coord("longitude").var_name = "lon"
-    rsns_cube.coord("longitude").var_name = "lat"
+    rlns_cube.coord("longitude").var_name = "lon"
+    rlns_cube.coord("longitude").var_name = "lat"
 
-    return iris.cube.CubeList([rsds_cube, rsns_cube])
+    return iris.cube.CubeList([rlds_cube, rlns_cube])
 
 
-def test_rsntcs_calculation(cubes):
-    derived_var = rsus.DerivedVariable()
+def test_rlntcs_calculation(cubes):
+    derived_var = rlus.DerivedVariable()
     out_cube = derived_var.calculate(cubes)
     np.testing.assert_allclose(out_cube.data,
                                np.array([[[50.0]]]))
