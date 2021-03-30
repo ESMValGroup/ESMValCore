40d618462344659693f7765dd57d64c8c60ac431
sloosvel <saskia.loosveldt@bsc.es>
2021-03-30 15:19:02 +0200

    Update test

--- tests/integration/test_recipe.py
+++ tests/integration/test_recipe.py
@@ -2165,8 +2165,7 @@ def test_fx_list_mip_change_cmip6(tmp_path, patched_datafinder, config_user):
     assert '_Ofx_' in fx_variables['areacello']['filename']
     assert '_Efx_' in fx_variables['clayfrac']['filename']
     assert '_fx_' in fx_variables['sftlf']['filename']
-    assert len(fx_variables['sftgif']['filename']) == 1
-    assert 'sftgif_' in fx_variables['sftgif']['filename'][0]
+    assert '_fx_' in fx_variables['sftgif']['filename']
     assert '_Ofx_' in fx_variables['sftof']['filename']
 
     # Check mask_landsea
