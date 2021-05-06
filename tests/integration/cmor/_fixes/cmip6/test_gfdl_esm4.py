968c3a731a8a9c590a2d131943f8873e0ba97d0d
Manuel Schlund <manuel.schlund@dlr.de>
2021-05-06 16:00:56 +0200

    Added missing tests for fgco2

--- tests/integration/cmor/_fixes/cmip6/test_gfdl_esm4.py
+++ tests/integration/cmor/_fixes/cmip6/test_gfdl_esm4.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 from cf_units import Unit
 
-from esmvalcore.cmor._fixes.cmip6.gfdl_esm4 import Siconc, Omon
+from esmvalcore.cmor._fixes.cmip6.gfdl_esm4 import Fgco2, Omon, Siconc
 from esmvalcore.cmor.fix import Fix
 from esmvalcore.cmor.table import get_var_info
 
@@ -109,3 +109,34 @@ def test_thetao_fix_metadata(thetao_cubes):
     assert depth_coord.long_name == 'ocean depth coordinate'
     assert depth_coord.units == 'm'
     assert depth_coord.attributes == {'positive': 'down'}
+
+
+def test_get_fgco2_fix():
+    """Test getting of fix."""
+    fix = Fix.get_fixes('CMIP6', 'GFDL-ESM4', 'Omon', 'fgco2')
+    assert fix == [Fgco2(None), Omon(None)]
+
+
+def test_fgco2_fix_metadata():
+    """Test ``fix_metadata`` for ``fgco2``."""
+    vardef = get_var_info('CMIP6', 'Omon', 'fgco2')
+    cubes = iris.cube.CubeList([
+        iris.cube.Cube(0.0, var_name='fgco2'),
+    ])
+    fix = Fgco2(vardef)
+    out_cubes = fix.fix_metadata(cubes)
+    assert out_cubes is cubes
+    assert len(out_cubes) == 1
+    out_cube = out_cubes[0]
+
+    # Check depth coordinate
+    depth_coord = out_cube.coord('depth')
+    assert depth_coord.standard_name == 'depth'
+    assert depth_coord.var_name == 'depth'
+    assert depth_coord.long_name == 'depth'
+    assert depth_coord.units == 'm'
+    assert depth_coord.attributes == {'positive': 'down'}
+
+    # Check values of depth coordinate
+    np.testing.assert_allclose(depth_coord.points, 0.0)
+    assert depth_coord.bounds is None
