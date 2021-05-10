4df0235659fb785d1e257bd6f7de2ad3e24af6de
Lisa Bock <Lisa.Bock@dlr.de>
2021-05-10 14:50:35 +0200

    add fix

---
+++ esmvalcore/cmor/_fixes/cmip6/norcpm1.py
@@ -0,0 +1,20 @@
+"""Fixes for NorCPM1."""
+from ..fix import Fix
+
+class Nbp(Fix):
+    """Fixes for nbp."""
+
+    def fix_data(self, cube):
+        """
+        Fix data.
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+        Returns
+        -------
+        iris.cube.Cube
+        """
+        metadata = cube.metadata
+        cube *= 29./44.
+        cube.metadata = metadata
+        return cube
