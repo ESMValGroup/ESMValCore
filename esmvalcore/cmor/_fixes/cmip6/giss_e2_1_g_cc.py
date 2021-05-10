9b78d9fd9673400b943dea5bfe6362cd51b0203b
Lisa Bock <Lisa.Bock@dlr.de>
2021-05-10 12:24:17 +0200

    add fixes for nbp

---
+++ esmvalcore/cmor/_fixes/cmip6/giss_e2_1_g_cc.py
@@ -0,0 +1,6 @@
+"""Fixes for GISS-E2-1-G-CC model."""
+from ..fix import Fix
+from giss-e2-1-g import Nbp as BaseNbp
+
+
+Nbp = BaseNbp
--- esmvalcore/cmor/_fixes/cmip6/sam0_unicon.py
+++ esmvalcore/cmor/_fixes/cmip6/sam0_unicon.py
@@ -9,3 +9,23 @@ Cli = ClFixHybridPressureCoord
 
 
 Clw = ClFixHybridPressureCoord
+
+
+class Nbp(Fix):
+    """Fixes for nbp."""
+
+    def fix_data(self, cube):
+        """
+        Fix data.
+        Fixes wrong sign for land surface flux.
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+        Returns
+        -------
+        iris.cube.Cube
+        """
+        metadata = cube.metadata
+        cube *= -1
+        cube.metadata = metadata
+        return cube
