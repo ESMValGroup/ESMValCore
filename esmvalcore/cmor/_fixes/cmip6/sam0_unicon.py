80d26ea7cd628035891e4f5b66f494402e697c0b
Lisa Bock <Lisa.Bock@dlr.de>
2021-05-10 15:05:25 +0200

    fix bug

--- esmvalcore/cmor/_fixes/cmip6/sam0_unicon.py
+++ esmvalcore/cmor/_fixes/cmip6/sam0_unicon.py
@@ -1,6 +1,7 @@
 """Fixes for SAM0-UNICON model."""
 from ..common import ClFixHybridPressureCoord
-from ..giss_e2_1_g import Nbp as BaseNbp
+from .giss_e2_1_g import Nbp as BaseNbp
+
 
 Cl = ClFixHybridPressureCoord
 
