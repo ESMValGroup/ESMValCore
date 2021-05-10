c6be6074a9bf75201a816ec9fcc97ce053dabd18
Lisa Bock <Lisa.Bock@dlr.de>
2021-05-10 15:20:32 +0200

    fix style

--- esmvalcore/cmor/_fixes/cmip6/norcpm1.py
+++ esmvalcore/cmor/_fixes/cmip6/norcpm1.py
@@ -8,14 +8,16 @@ class Nbp(Fix):
     def fix_data(self, cube):
         """
         Fix data.
+
         Parameters
         ----------
         cube: iris.cube.Cube
+
         Returns
         -------
         iris.cube.Cube
         """
         metadata = cube.metadata
-        cube *= 29./44.
+        cube *= 29. / 44.
         cube.metadata = metadata
         return cube
