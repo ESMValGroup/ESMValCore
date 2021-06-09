79915cdcc8cfacde1c2638ab2ab40c67526420c4
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2021-06-09 10:14:43 +0200

    Improve formatting

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -8,11 +8,14 @@ from esmvalcore._config._config import (
     importlib_files,
 )
 
-TEST_DEEP_UPDATE = [([{}], {}), ([dict(a=1, b=2), dict(a=3)], dict(a=3, b=2)),
-                    ([
-                        dict(a=dict(b=1, c=dict(d=2)), e=dict(f=4, g=5)),
-                        dict(a=dict(b=2, c=3))
-                    ], dict(a=dict(b=2, c=3), e=dict(f=4, g=5)))]
+TEST_DEEP_UPDATE = [
+    ([{}], {}),
+    ([dict(a=1, b=2), dict(a=3)], dict(a=3, b=2)),
+    ([
+        dict(a=dict(b=1, c=dict(d=2)), e=dict(f=4, g=5)),
+        dict(a=dict(b=2, c=3)),
+    ], dict(a=dict(b=2, c=3), e=dict(f=4, g=5))),
+]
 
 
 @pytest.mark.parametrize('dictionaries, expected_merged', TEST_DEEP_UPDATE)
