3572709fd34a636f9e686bca6c192f73a154fa0d
sloosvel <saskia.loosveldt@bsc.es>
2021-06-14 15:17:26 +0200

    Set circular value directly and add test

--- esmvalcore/cmor/_fixes/cmip6/mcm_ua_1_0.py
+++ esmvalcore/cmor/_fixes/cmip6/mcm_ua_1_0.py
@@ -2,7 +2,6 @@
 import iris
 import numpy as np
 from dask import array as da
-from iris.util import _is_circular
 
 from ..fix import Fix
 from ..shared import add_scalar_height_coord, fix_ocean_depth_coord
@@ -71,9 +70,7 @@ class AllVars(Fix):
                             lon_bnds[-1][-1] == 360.:
                         lon_bnds[-1][-1] = 358.125
                         lon_coord.bounds = lon_bnds
-                        lon_coord.circular = _is_circular(lon_coord.points,
-                                                          360,
-                                                          lon_coord.bounds)
+                        lon_coord.circular = True
                     # ocean & seaice
                     if lon_coord.points[0] == -0.9375:
                         lon_dim = cube.coord_dims('longitude')[0]
