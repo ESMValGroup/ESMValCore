faa44495151e0a4af02d08a22dbfe1e817a6e2f4
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-08-23 15:10:06 +0100

    added handy tool to run to parse pytest-monitor pymon db

---
+++ parse_pymon.py
@@ -0,0 +1,22 @@
+"""
+Parse and display test memory.
+
+Uses pytest-monitor plugin from https://github.com/CFMTech/pytest-monitor
+Lots of other metrics can be read from the file via sqlite parsing.,
+currently just MEM_USAGE (RES memory, in MB).
+"""
+import sqlite3
+
+# Create a SQL connection to our SQLite database
+con = sqlite3.connect(".pymon")
+
+cur = con.cursor()
+
+# The result of a "cursor.execute" can be iterated over by row
+for row in cur.execute('select ITEM, MEM_USAGE from TEST_METRICS;'):
+    test_name, memory_used = row[0], row[1]
+    if memory_used > 1000.:  # test result in RES mem in MB
+        print(test_name, memory_used)
+
+# Be sure to close the connection
+con.close()
