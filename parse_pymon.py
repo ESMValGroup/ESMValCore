98f9baf6f024ef0512d882c80a9918e1b3963ef6
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-08-23 15:50:14 +0100

    added some stdout to parser

--- parse_pymon.py
+++ parse_pymon.py
@@ -13,10 +13,14 @@ con = sqlite3.connect(".pymon")
 cur = con.cursor()
 
 # The result of a "cursor.execute" can be iterated over by row
+print("Looking for tests that exceed 1GB resident memory.")
 for row in cur.execute('select ITEM, MEM_USAGE from TEST_METRICS;'):
     test_name, memory_used = row[0], row[1]
     if memory_used > 1000.:  # test result in RES mem in MB
+        print("Test name / memory (MB)")
         print(test_name, memory_used)
+    else:
+        print("All our tests need less than 1GB resident memory, phew.")
 
 # Be sure to close the connection
 con.close()
