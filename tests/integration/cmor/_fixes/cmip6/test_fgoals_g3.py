26a7445e4cd74ba23c8ea640cfb611f1d7fa745f
Thomas Crocker <thomas.crocker@metoffice.gov.uk>
2021-08-24 18:01:44 +0100

    Adding test for Mrsos fix

--- tests/integration/cmor/_fixes/cmip6/test_fgoals_g3.py
+++ tests/integration/cmor/_fixes/cmip6/test_fgoals_g3.py
@@ -5,7 +5,9 @@ import iris
 import numpy as np
 
 from esmvalcore.cmor._fixes.cmip5.fgoals_g2 import Cl as BaseCl
-from esmvalcore.cmor._fixes.cmip6.fgoals_g3 import Cl, Cli, Clw, Siconc, Tos
+from esmvalcore.cmor._fixes.cmip6.fgoals_g3 import Cl, Cli, Clw, Siconc
+from esmvalcore.cmor._fixes.cmip6.fgoals_g3 import Tos, Mrsos
+from esmvalcore.cmor._fixes.cmip6.fgoals_g3 import _check_bounds_monotonicity
 from esmvalcore.cmor._fixes.common import OceanFixGrid
 from esmvalcore.cmor.fix import Fix
 from esmvalcore.cmor.table import get_var_info
@@ -68,7 +70,8 @@ def test_tos_fix_metadata(mock_base_fix_metadata):
     lon_coord = iris.coords.AuxCoord([1.0, 2.0, 1e30],
                                      var_name='lon',
                                      standard_name='longitude')
-    cube = iris.cube.Cube([1.0, 2.0, 3.0], var_name='tos',
+    cube = iris.cube.Cube([1.0, 2.0, 3.0],
+                          var_name='tos',
                           standard_name='sea_surface_temperature',
                           aux_coords_and_dims=[(lat_coord, 0), (lon_coord, 0)])
     cubes = iris.cube.CubeList([cube])
@@ -79,10 +82,10 @@ def test_tos_fix_metadata(mock_base_fix_metadata):
     fixed_cubes = fix.fix_metadata(cubes)
     assert len(fixed_cubes) == 1
     fixed_cube = fixed_cubes[0]
-    np.testing.assert_allclose(fixed_cube.coord('latitude').points,
-                               [3.14, 0.0, 6.28])
-    np.testing.assert_allclose(fixed_cube.coord('longitude').points,
-                               [1.0, 2.0, 0.0])
+    np.testing.assert_allclose(
+        fixed_cube.coord('latitude').points, [3.14, 0.0, 6.28])
+    np.testing.assert_allclose(
+        fixed_cube.coord('longitude').points, [1.0, 2.0, 0.0])
     mock_base_fix_metadata.assert_called_once_with(fix, cubes)
 
 
@@ -95,3 +98,46 @@ def test_get_siconc_fix():
 def test_siconc_fix():
     """Test fix for ``siconc``."""
     assert Siconc is Tos
+
+
+def test_get_mrsos_fix():
+    """Test getting of fix."""
+    fix = Fix.get_fixes('CMIP6', 'FGOALS-g3', 'Lmon', 'mrsos')
+    assert fix == [Mrsos(None)]
+
+
+def test_mrsos_fix():
+    """Test fix for ``mrsos``."""
+    assert issubclass(Mrsos, Fix)
+
+
+@mock.patch('esmvalcore.cmor._fixes.cmip6.fgoals_g3.Fix.fix_metadata',
+            autospec=True)
+def test_mrsos_fix_metadata(mock_base_fix_metadata):
+    """Test ``fix_metadata`` for ``mrsos``."""
+    mock_base_fix_metadata.side_effect = lambda x, y: y
+
+    # Create test cube
+    lat_coord = iris.coords.AuxCoord([1.0, 2.0, 3.0],
+                                     var_name='lat',
+                                     standard_name='latitude')
+    lat_coord.bounds = [[0.5, 1.5], [-0.5, 0.5], [2.5, 3.5]]
+    lon_coord = iris.coords.AuxCoord([1.0, 2.0, 3.0],
+                                     var_name='lon',
+                                     standard_name='longitude')
+    lon_coord.bounds = [[0.5, 1.5], [-0.5, 0.5], [2.5, 3.5]]
+    cube = iris.cube.Cube([1.0, 2.0, 3.0],
+                          var_name='mrsos',
+                          standard_name='mass_content_of_water_in_soil_layer',
+                          aux_coords_and_dims=[(lat_coord, 0), (lon_coord, 0)])
+    cubes = iris.cube.CubeList([cube])
+
+    # Apply fix
+    vardef = get_var_info('CMIP6', 'Lmon', 'mrsos')
+    fix = Mrsos(vardef)
+    fixed_cubes = fix.fix_metadata(cubes)
+    assert len(fixed_cubes) == 1
+    fixed_cube = fixed_cubes[0]
+    assert _check_bounds_monotonicity(fixed_cube.coord('latitude'))
+    assert _check_bounds_monotonicity(fixed_cube.coord('longitude'))
+    mock_base_fix_metadata.assert_called_once_with(fix, cubes)
