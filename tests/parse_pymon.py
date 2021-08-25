392da3e9113ff8886b24b669e6ee65c6187bb7f7
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-08-25 13:04:43 +0100

    add printing of the 100 slowest tests

--- tests/parse_pymon.py
+++ tests/parse_pymon.py
@@ -7,6 +7,7 @@ currently just MEM_USAGE (RES memory, in MB).
 """
 import sqlite3
 import sys
+from operator import itemgetter
 
 
 def _parse_pymon_database():
@@ -15,6 +16,7 @@ def _parse_pymon_database():
     cur = con.cursor()
 
     # The result of a "cursor.execute" can be iterated over by row
+    # first look at memory
     print("Looking for tests that exceed 1GB resident memory.")
     big_mem_tests = []
     for row in cur.execute('select ITEM, MEM_USAGE from TEST_METRICS;'):
@@ -24,6 +26,23 @@ def _parse_pymon_database():
             print(test_name, memory_used)
         elif memory_used > 4000.:
             big_mem_tests.append((test_name, memory_used))
+
+    # then look at total time (in seconds)
+    # (user time is availbale too via USER_TIME, kernel time via KERNEL_TIME)
+    timed_tests = []
+    sq_command = 'select ITEM, ITEM_VARIANT, TOTAL_TIME from TEST_METRICS;'
+    for row in cur.execute(sq_command):
+        test_name, test_var, time_used = row[0], row[1], row[2]
+        timed_tests.append((test_name, test_var, time_used))
+
+    timed_tests = sorted(timed_tests, reverse=True, key=itemgetter(2))
+    hundred_slowest_tests = timed_tests[0:100]
+    print("List of 100 slowest tests (test name, test variant: duration [s])")
+    if hundred_slowest_tests:
+        for test_name, test_var, test_duration in hundred_slowest_tests:
+            print(test_name + " " + test_var + ": " + "%.2f" % test_duration)
+    else:
+        print("Could not retrieve test timing data.")
 
     # Be sure to close the connection
     con.close()
