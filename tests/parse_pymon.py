651167d8bff99539f4b56249318d8bd9f6ecac9e
Valeriu Predoi <valeriu.predoi@gmail.com>
2021-08-25 16:05:12 +0100

    restructured and reformatted timing output according to Bouwe suggestion

--- tests/parse_pymon.py
+++ tests/parse_pymon.py
@@ -10,14 +10,8 @@ import sys
 from operator import itemgetter
 
 
-def _parse_pymon_database():
-    # Create a SQL connection to our SQLite database
-    con = sqlite3.connect(".pymon")
-    cur = con.cursor()
-
-    # The result of a "cursor.execute" can be iterated over by row
-    # first look at memory
-    print("Looking for tests that exceed 1GB resident memory.")
+def _get_big_mem_tests(cur):
+    """Find out which tests are heavy on memory."""
     big_mem_tests = []
     for row in cur.execute('select ITEM, MEM_USAGE from TEST_METRICS;'):
         test_name, memory_used = row[0], row[1]
@@ -27,8 +21,11 @@ def _parse_pymon_database():
         elif memory_used > 4000.:
             big_mem_tests.append((test_name, memory_used))
 
-    # then look at total time (in seconds)
-    # (user time is availbale too via USER_TIME, kernel time via KERNEL_TIME)
+    return big_mem_tests
+
+
+def _get_slow_tests(cur):
+    """Find out which tests are slow."""
     timed_tests = []
     sq_command = \
         'select ITEM, ITEM_VARIANT, ITEM_PATH, TOTAL_TIME from TEST_METRICS;'
@@ -39,13 +36,32 @@ def _parse_pymon_database():
 
     timed_tests = sorted(timed_tests, reverse=True, key=itemgetter(3))
     hundred_slowest_tests = timed_tests[0:100]
-    print("List of 100 slowest tests (name, variant: duration [s] and path)")
+    print("List of 100 slowest tests (duration, path, name")
     if hundred_slowest_tests:
-        for test_name, test_var, pth, test_duration in hundred_slowest_tests:
-            print(test_name + " " + test_var + ": " + "%.2f" % test_duration +
-                  "s\n" + pth)
+        for _, test_var, pth, test_duration in hundred_slowest_tests:
+            pth = pth.replace(".", "/") + ".py"
+            executable_test = pth + "::" + test_var
+            if ", " in executable_test:
+                executable_test = executable_test.replace(", ", "-")
+            mssg = "%.2f" % test_duration + "s " + executable_test
+            print(mssg)
     else:
         print("Could not retrieve test timing data.")
+
+
+def _parse_pymon_database():
+    # Create a SQL connection to our SQLite database
+    con = sqlite3.connect(".pymon")
+    cur = con.cursor()
+
+    # The result of a "cursor.execute" can be iterated over by row
+    # first look at memory
+    print("Looking for tests that exceed 1GB resident memory.")
+    big_mem_tests = _get_big_mem_tests(cur)
+
+    # then look at total time (in seconds)
+    # (user time is availbale too via USER_TIME, kernel time via KERNEL_TIME)
+    _get_slow_tests(cur)
 
     # Be sure to close the connection
     con.close()
