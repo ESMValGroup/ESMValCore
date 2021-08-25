eed121ead43f1e041b774342a5caa7cbdde57c3b
Bouwe Andela <b.andela@esciencecenter.nl>
2021-08-25 11:06:58 +0200

    Add fixes for EC-Earth3

--- esmvalcore/cmor/_fixes/cmip6/ec_earth3.py
+++ esmvalcore/cmor/_fixes/cmip6/ec_earth3.py
@@ -1,7 +1,28 @@
 """Fixes for EC-Earth3 model."""
+import cf_units
 import numpy as np
 
 from ..fix import Fix
+from ..shared import round_coordinates
+
+
+class AllVars(Fix):
+    """Fixes for all variables."""
+    def fix_metadata(self, cubes):
+        """Fix metadata."""
+        for cube in cubes:
+            if cube.attributes.get('variant_label', '') == 'r3i1p1f1':
+                round_coordinates(
+                    [cube],
+                    decimals=3,
+                    coord_names=['latitude'],
+                )
+                if (cube.attributes.get('experiment_id', '') == 'historical'
+                        and cube.coords('time')):
+                    time_coord = cube.coord('time')
+                    time_coord.units = cf_units.Unit(time_coord.units.origin,
+                                                     'proleptic_gregorian')
+        return cubes
 
 
 class Siconca(Fix):
@@ -26,7 +47,6 @@ class Siconca(Fix):
 
 class Tas(Fix):
     """Fixes for tas."""
-
     def fix_metadata(self, cubes):
         """Fix latitude points and bounds.
 
