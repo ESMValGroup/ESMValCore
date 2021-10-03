ESMValGroup/ESMValCore

commit d83edb131318b312b0b4f805f461122f8f981415

Add support for re-using preprocessor output from previous runs (#1321)

esmvalcore/_config/_config.py
@@ -102,21 +102,22 @@ def read_config_user_file(config_file, folder_name, options=None):
 
     # set defaults
     defaults = {
+        'auxiliary_data_dir': 'auxiliary_data',
         'compress_netcdf': False,
+        'config_developer_file': None,
+        'drs': {},
+        'download_dir': '~/climate_data',
         'exit_on_warning': False,
+        'extra_facets_dir': tuple(),
+        'max_parallel_tasks': None,
         'offline': True,
         'output_file_type': 'png',
         'output_dir': 'esmvaltool_output',
-        'download_dir': '~/climate_data',
-        'auxiliary_data_dir': 'auxiliary_data',
-        'extra_facets_dir': tuple(),
-        'save_intermediary_cubes': False,
-        'remove_preproc_dir': True,
-        'max_parallel_tasks': None,
-        'run_diagnostic': True,
         'profile_diagnostic': False,
-        'config_developer_file': None,
-        'drs': {},
+        'remove_preproc_dir': True,
+        'resume_from': [],
+        'run_diagnostic': True,
+        'save_intermediary_cubes': False,
     }
 
     for key in defaults:

commit 30da130dfe59c9cf736d4fb2289bfc7fc9714e39

Log fewer messages to screen and hide stack trace for known recipe errors (#1296)
* Log fewer messages to stdout and hide stack trace for known errors
* Change KeyError to RecipeError for missing key in recipe
* Turn warning about spherical earth radius into a debug log message
* Fix problem with logging of missing data introduced in d83edb131318b312b0b4f805f461122f8f981415

esmvalcore/_config/_config.py
@@ -12,6 +12,7 @@ from pathlib import Path
 import yaml
 
 from esmvalcore.cmor.table import CMOR_TABLES, read_cmor_tables
+from esmvalcore.exceptions import RecipeError
 
 logger = logging.getLogger(__name__)
 
@@ -230,17 +231,15 @@ def load_config_developer(cfg_file=None):
 
 def get_project_config(project):
     """Get developer-configuration for project."""
-    logger.debug("Retrieving %s configuration", project)
     if project in CFG:
         return CFG[project]
-    raise ValueError(f"Project '{project}' not in config-developer.yml")
+    raise RecipeError(f"Project '{project}' not in config-developer.yml")
 
 
 def get_institutes(variable):
     """Return the institutes given the dataset name in CMIP6."""
     dataset = variable['dataset']
     project = variable['project']
-    logger.debug("Retrieving institutes for dataset %s", dataset)
     try:
         return CMOR_TABLES[project].institutes[dataset]
     except (KeyError, AttributeError):
@@ -252,7 +251,6 @@ def get_activity(variable):
     project = variable['project']
     try:
         exp = variable['exp']
-        logger.debug("Retrieving activity_id for experiment %s", exp)
         if isinstance(exp, list):
             return [CMOR_TABLES[project].activities[value][0] for value in exp]
         return CMOR_TABLES[project].activities[exp][0]

esmvalcore/_data_finder.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import iris
 
 from ._config import get_project_config
+from .exceptions import RecipeError
 
 logger = logging.getLogger(__name__)
 
@@ -104,7 +105,6 @@ def get_start_end_year(filename):
         raise ValueError(f'File {filename} dates do not match a recognized'
                          'pattern and time can not be read from the file')
 
-    logger.debug("Found start_year %s and end_year %s", start_year, end_year)
     return int(start_year), int(end_year)
 
 
@@ -139,7 +139,6 @@ def _replace_tags(paths, variable):
             ))
             tlist.add('sub_experiment')
         paths = new_paths
-    logger.debug(tlist)
 
     for tag in tlist:
         original_tag = tag
@@ -150,8 +149,8 @@ def _replace_tags(paths, variable):
         if tag in variable:
             replacewith = variable[tag]
         else:
-            raise KeyError("Dataset key {} must be specified for {}, check "
-                           "your recipe entry".format(tag, variable))
+            raise RecipeError(f"Dataset key '{tag}' must be specified for "
+                              f"{variable}, check your recipe entry")
         paths = _replace_tag(paths, original_tag, replacewith)
     return paths
 
@@ -209,7 +208,7 @@ def _resolve_latestversion(dirname_template):
             if os.path.isdir(dirname):
                 return dirname
 
-    return dirname_template
+    return None
 
 
 def _select_drs(input_type, drs, project):
@@ -258,11 +257,12 @@ def _find_input_dirs(variable, rootpath, drs):
         for base_path in root:
             dirname = os.path.join(base_path, dirname_template)
             dirname = _resolve_latestversion(dirname)
+            if dirname is None:
+                continue
             matches = glob.glob(dirname)
             matches = [match for match in matches if os.path.isdir(match)]
             if matches:
                 for match in matches:
-                    logger.debug("Found %s", match)
                     dirnames.append(match)
             else:
                 logger.debug("Skipping non-existent %s", dirname)

esmvalcore/_main.py
@@ -443,7 +443,9 @@ def run():
     """Run the `esmvaltool` program, logging any exceptions."""
     import sys
 
-    # Workaroud to avoid using more for the output
+    from .exceptions import RecipeError
+
+    # Workaround to avoid using more for the output
 
     def display(lines, out):
         text = "\n".join(lines) + "\n"
@@ -455,6 +457,11 @@ def run():
         fire.Fire(ESMValTool())
     except fire.core.FireExit:
         raise
+    except RecipeError as exc:
+        # Hide the stack trace for RecipeErrors
+        logger.error("%s", exc)
+        logger.debug("Stack trace for debugging:", exc_info=True)
+        sys.exit(1)
     except Exception:  # noqa
         if not logger.handlers:
             # Add a logging handler if main failed to do so.

esmvalcore/_recipe.py
@@ -27,10 +27,10 @@ from ._data_finder import (
     get_statistic_output_file,
 )
 from ._provenance import TrackedFile, get_recipe_provenance
-from ._recipe_checks import InputFilesNotFound, RecipeError
 from ._task import DiagnosticTask, ResumeTask, TaskSet
 from .cmor.check import CheckLevels
 from .cmor.table import CMOR_TABLES
+from .exceptions import InputFilesNotFound, RecipeError
 from .preprocessor import (
     DEFAULT_ORDER,
     FINAL_STEPS,
@@ -70,7 +70,6 @@ def read_recipe_file(filename, config_user, initialize_tasks=True):
 
 def _add_cmor_info(variable, override=False):
     """Add information from CMOR tables to variable."""
-    logger.debug("If not present: adding keys from CMOR table to %s", variable)
     # Copy the following keys from CMOR table
     cmor_keys = [
         'standard_name', 'long_name', 'units', 'modeling_realm', 'frequency'
@@ -369,8 +368,8 @@ def _search_fx_mip(tables, variable, fx_info, config_user):
                      fx_info['short_name'], mip)
         fx_files = _get_input_files(fx_info, config_user)[0]
         if fx_files:
-            logger.debug("Found fx variables '%s':\n%s",
-                         fx_info['short_name'], pformat(fx_files))
+            logger.debug("Found fx variables '%s':\n%s", fx_info['short_name'],
+                         pformat(fx_files))
             fx_files_for_mips[mip] = fx_files
 
     # Dict contains more than one element -> ambiguity
@@ -430,8 +429,9 @@ def _get_fx_files(variable, fx_info, config_user):
 
     # Flag a warning if no files are found
     if not fx_files:
-        logger.warning("Missing data for fx variable %s of dataset %s",
-                       fx_info['short_name'], fx_info['dataset'])
+        logger.warning("Missing data for fx variable '%s' of dataset %s",
+                       fx_info['short_name'],
+                       fx_info['alias'].replace('_', ' '))
 
     # If frequency = fx, only allow a single file
     if fx_files:
@@ -474,12 +474,10 @@ def _update_fx_files(step_name, settings, variable, config_user, fx_vars):
         fx_files, fx_info = _get_fx_files(variable, fx_info, config_user)
         if fx_files:
             fx_info['filename'] = fx_files
-            settings['add_fx_variables']['fx_variables'].update({
-                fx_var: fx_info
-            })
-            logger.info(
-                'Using fx files for variable %s during step %s: %s',
-                variable['short_name'], step_name, pformat(fx_files))
+            settings['add_fx_variables']['fx_variables'].update(
+                {fx_var: fx_info})
+            logger.debug('Using fx files for variable %s during step %s: %s',
+                         variable['short_name'], step_name, pformat(fx_files))
 
 
 def _fx_list_to_dict(fx_vars):
@@ -499,6 +497,7 @@ def _fx_list_to_dict(fx_vars):
 
 def _update_fx_settings(settings, variable, config_user):
     """Update fx settings depending on the needed method."""
+
     # get fx variables either from user defined attribute or fixed
     def _get_fx_vars_from_attribute(step_settings, step_name):
         user_fx_vars = step_settings.get('fx_variables')
@@ -596,12 +595,17 @@ def _get_ancestors(variable, config_user):
     (input_files, dirnames,
      filenames) = _get_input_files(variable, config_user)
 
-    logger.info(
+    logger.debug(
         "Using input files for variable %s of dataset %s:\n%s",
-        variable['short_name'], variable['dataset'], '\n'.join(
+        variable['short_name'],
+        variable['alias'].replace('_', ' '),
+        '\n'.join(
             f'{f} (will be downloaded)' if not os.path.exists(f) else str(f)
-            for f in input_files))
+            for f in input_files),
+    )
     check.data_availability(input_files, variable, dirnames, filenames)
+    logger.info("Found input files for %s",
+                variable['alias'].replace('_', ' '))
 
     # Set up provenance tracking
     for i, filename in enumerate(input_files):
@@ -873,8 +877,9 @@ def _get_single_preprocessor_task(variables,
         write_ncl_interface=config_user['write_ncl_interface'],
     )
 
-    logger.info("PreprocessingTask %s created. It will create the files:\n%s",
-                task.name, '\n'.join(p.filename for p in task.products))
+    logger.info("PreprocessingTask %s created.", task.name)
+    logger.debug("PreprocessingTask %s will create the files:\n%s", task.name,
+                 '\n'.join(p.filename for p in task.products))
 
     return task
 
@@ -1494,16 +1499,16 @@ class Recipe:
                     tasks.add(task)
                     priority += 1
 
-        # Resolve diagnostic ancestors
-        if self._cfg.get('run_diagnostic', True):
-            self._resolve_diagnostic_ancestors(tasks)
-
         if failed_tasks:
             recipe_error = RecipeError('Could not create all tasks')
             recipe_error.failed_tasks.extend(failed_tasks)
             raise recipe_error
 
         check.tasks_valid(tasks)
+
+        # Resolve diagnostic ancestors
+        if self._cfg.get('run_diagnostic', True):
+            self._resolve_diagnostic_ancestors(tasks)
 
         return tasks
 

esmvalcore/_recipe_checks.py
@@ -9,26 +9,11 @@ from shutil import which
 import yamale
 
 from ._data_finder import get_start_end_year
+from .exceptions import InputFilesNotFound, RecipeError
 from .preprocessor import TIME_PREPROCESSORS, PreprocessingTask
 from .preprocessor._multimodel import STATISTIC_MAPPING
 
 logger = logging.getLogger(__name__)
-
-
-class RecipeError(Exception):
-    """Recipe contains an error."""
-    def __init__(self, msg):
-        super().__init__(self)
-        self.message = msg
-        self.failed_tasks = []
-
-    def __str__(self):
-        """Return message string."""
-        return self.message
-
-
-class InputFilesNotFound(RecipeError):
-    """Files that are required to run the recipe have not been found."""
 
 
 def ncl_version():

esmvalcore/cmor/check.py
@@ -43,13 +43,13 @@ def _get_time_bounds(time, freq):
             max_bound = time.units.date2num(
                 datetime.datetime(next_year, next_month, 1, 0, 0))
         elif freq == 'yr':
-            min_bound = time.units.date2num(
-                datetime.datetime(year, 1, 1, 0, 0))
+            min_bound = time.units.date2num(datetime.datetime(
+                year, 1, 1, 0, 0))
             max_bound = time.units.date2num(
                 datetime.datetime(year + 1, 1, 1, 0, 0))
         elif freq == 'dec':
-            min_bound = time.units.date2num(
-                datetime.datetime(year, 1, 1, 0, 0))
+            min_bound = time.units.date2num(datetime.datetime(
+                year, 1, 1, 0, 0))
             max_bound = time.units.date2num(
                 datetime.datetime(year + 10, 1, 1, 0, 0))
         else:
@@ -137,8 +137,8 @@ class CMORCheck():
             except iris.exceptions.CoordinateNotFoundError:
                 pass
             else:
-                if lat.ndim == 1 and (self._cube.coord_dims(lat) ==
-                                      self._cube.coord_dims(lon)):
+                if lat.ndim == 1 and (self._cube.coord_dims(lat)
+                                      == self._cube.coord_dims(lon)):
                     self._unstructured = True
         return self._unstructured
 
@@ -229,9 +229,14 @@ class CMORCheck():
             If any errors were reported before calling this method.
         """
         if self.has_errors():
-            msg = 'There were errors in variable {}:\n{}\n in cube:\n{}'
-            msg = msg.format(self._cube.var_name, '\n '.join(self._errors),
-                             self._cube)
+            msg = '\n'.join([
+                f'There were errors in variable {self._cube.var_name}:'
+                '\n '.join(self._errors),
+                'in cube:',
+                f'{self._cube}',
+                'loaded from file ' +
+                self._cube.attributes.get('source_file', ''),
+            ])
             raise CMORCheckError(msg)
 
     def report_warnings(self):
@@ -243,8 +248,12 @@ class CMORCheck():
             Given logger
         """
         if self.has_warnings():
-            msg = 'There were warnings in variable {}:\n{}\n'.format(
-                self._cube.var_name, '\n '.join(self._warnings))
+            msg = '\n'.join([
+                f'There were warnings in variable {self._cube.var_name}:',
+                '\n '.join(self._warnings),
+                'loaded from file ' +
+                self._cube.attributes.get('source_file', ''),
+            ])
             self._logger.warning(msg)
 
     def report_debug_messages(self):
@@ -256,8 +265,13 @@ class CMORCheck():
             Given logger.
         """
         if self.has_debug_messages():
-            msg = 'There were metadata changes in variable {}:\n{}\n'.format(
-                self._cube.var_name, '\n '.join(self._debug_messages))
+            msg = '\n'.join([
+                'There were metadata changes in variable' +
+                self._cube.var_name,
+                '\n '.join(self._debug_messages),
+                'loaded from file ' +
+                self._cube.attributes.get('source_file', ''),
+            ])
             self._logger.debug(msg)
 
     def _check_fill_value(self):
@@ -511,7 +525,6 @@ class CMORCheck():
         search for the correct coordinate version and then check against this.
         For ``cmor_strict=False`` project (like OBS) the check for requested
         values might be disabled.
-
         """
         table_type = self._cmor_var.table_type
         alternative_coord = None
@@ -653,8 +666,7 @@ class CMORCheck():
             self.report_debug_message(
                 f'Coordinate {coord.standard_name} appears to belong to '
                 'an unstructured grid. Skipping monotonicity and '
-                'direction tests.'
-            )
+                'direction tests.')
             return
 
         if not coord.is_monotonic():

esmvalcore/exceptions.py
@@ -1,0 +1,18 @@
+"""Exceptions that may be raised by ESMValCore."""
+
+
+class RecipeError(Exception):
+    """Recipe contains an error."""
+
+    def __init__(self, msg):
+        super().__init__(self)
+        self.message = msg
+        self.failed_tasks = []
+
+    def __str__(self):
+        """Return message string."""
+        return self.message
+
+
+class InputFilesNotFound(RecipeError):
+    """Files that are required to run the recipe have not been found."""

esmvalcore/preprocessor/_area.py
@@ -4,6 +4,7 @@ Allows for selecting data subsets using certain latitude and longitude
 bounds; selecting geographical regions; constructing area averages; etc.
 """
 import logging
+import warnings
 
 import fiona
 import iris
@@ -179,6 +180,23 @@ def meridional_statistics(cube, operator):
     raise ValueError(msg)
 
 
+def compute_area_weights(cube):
+    """Compute area weights."""
+    with warnings.catch_warnings(record=True) as caught_warnings:
+        warnings.filterwarnings(
+            'always',
+            message="Using DEFAULT_SPHERICAL_EARTH_RADIUS.",
+            category=UserWarning,
+            module='iris.analysis.cartography',
+        )
+        weights = iris.analysis.cartography.area_weights(cube)
+        for warning in caught_warnings:
+            logger.debug(
+                "%s while computing area weights of the following cube:\n%s",
+                warning.message, cube)
+    return weights
+
+
 def area_statistics(cube, operator):
     """Apply a statistical operator in the horizontal direction.
 
@@ -234,11 +252,11 @@ def area_statistics(cube, operator):
     try:
         grid_areas = cube.cell_measure('cell_area').core_data()
     except iris.exceptions.CellMeasureNotFoundError:
-        logger.info(
+        logger.debug(
             'Cell measure "cell_area" not found in cube %s. '
             'Check fx_file availability.', cube.summary(shorten=True)
         )
-        logger.info('Attempting to calculate grid cell area...')
+        logger.debug('Attempting to calculate grid cell area...')
 
     if grid_areas is None and cube.coord('latitude').points.ndim == 2:
         coord_names = [coord.standard_name for coord in cube.coords()]
@@ -249,8 +267,8 @@ def area_statistics(cube, operator):
             cube_tmp.coord('grid_latitude').rename('latitude')
             cube_tmp.remove_coord('longitude')
             cube_tmp.coord('grid_longitude').rename('longitude')
-            grid_areas = iris.analysis.cartography.area_weights(cube_tmp)
-            logger.info('Calculated grid area shape: %s', grid_areas.shape)
+            grid_areas = compute_area_weights(cube_tmp)
+            logger.debug('Calculated grid area shape: %s', grid_areas.shape)
         else:
             logger.error(
                 'fx_file needed to calculate grid cell area for irregular '
@@ -261,8 +279,8 @@ def area_statistics(cube, operator):
     coord_names = ['longitude', 'latitude']
     if grid_areas is None:
         cube = guess_bounds(cube, coord_names)
-        grid_areas = iris.analysis.cartography.area_weights(cube)
-        logger.info('Calculated grid area shape: %s', grid_areas.shape)
+        grid_areas = compute_area_weights(cube)
+        logger.debug('Calculated grid area shape: %s', grid_areas.shape)
 
     if cube.shape != grid_areas.shape:
         raise ValueError('Cube shape ({}) doesn`t match grid area shape '
@@ -281,7 +299,7 @@ def area_statistics(cube, operator):
 
     new_dtype = result.dtype
     if original_dtype != new_dtype:
-        logger.warning(
+        logger.debug(
             "area_statistics changed dtype from "
             "%s to %s, changing back", original_dtype, new_dtype)
         result.data = result.core_data().astype(original_dtype)

esmvalcore/preprocessor/_time.py
@@ -575,7 +575,7 @@ def climate_statistics(cube,
 
     new_dtype = clim_cube.dtype
     if original_dtype != new_dtype:
-        logger.warning(
+        logger.debug(
             "climate_statistics changed dtype from "
             "%s to %s, changing back", original_dtype, new_dtype)
         clim_cube.data = clim_cube.core_data().astype(original_dtype)

esmvalcore/preprocessor/_volume.py
@@ -1,12 +1,10 @@
-"""
-Volume and z coordinate operations on data cubes.
+"""Volume and z coordinate operations on data cubes.
 
-Allows for selecting data subsets using certain volume bounds;
-selecting depth or height regions; constructing volumetric averages;
+Allows for selecting data subsets using certain volume bounds; selecting
+depth or height regions; constructing volumetric averages;
 """
-from copy import deepcopy
-
 import logging
+from copy import deepcopy
 
 import iris
 import numpy as np
@@ -15,8 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 def extract_volume(cube, z_min, z_max):
-    """
-    Subset a cube based on a range of values in the z-coordinate.
+    """Subset a cube based on a range of values in the z-coordinate.
 
     Function that subsets a cube on a box (z_min, z_max)
     This function is a restriction of masked_cube_lonlat();
@@ -54,8 +51,7 @@ def extract_volume(cube, z_min, z_max):
 
 
 def _create_cube_time(src_cube, data, times):
-    """
-    Generate a new cube with the volume averaged data.
+    """Generate a new cube with the volume averaged data.
 
     The resultant cube is seeded with `src_cube` metadata and coordinates,
     excluding any source coordinates that span the associated vertical
@@ -139,8 +135,7 @@ def _create_cube_time(src_cube, data, times):
 
 
 def calculate_volume(cube):
-    """
-    Calculate volume from a cube.
+    """Calculate volume from a cube.
 
     This function is used when the volume netcdf fx_variables can't be found.
 
@@ -175,8 +170,7 @@ def calculate_volume(cube):
 
 
 def volume_statistics(cube, operator):
-    """
-    Apply a statistical operation over a volume.
+    """Apply a statistical operation over a volume.
 
     The volume average is weighted according to the cell volume. Cell volume
     is calculated from iris's cartography tool multiplied by the cell
@@ -209,11 +203,11 @@ def volume_statistics(cube, operator):
     try:
         grid_volume = cube.cell_measure('ocean_volume').core_data()
     except iris.exceptions.CellMeasureNotFoundError:
-        logger.info(
+        logger.debug(
             'Cell measure "ocean_volume" not found in cube. '
             'Check fx_file availability.'
         )
-        logger.info('Attempting to calculate grid cell volume...')
+        logger.debug('Attempting to calculate grid cell volume...')
         grid_volume = calculate_volume(cube)
 
     if cube.data.shape != grid_volume.shape:
@@ -280,8 +274,7 @@ def volume_statistics(cube, operator):
 
 
 def depth_integration(cube):
-    """
-    Determine the total sum over the vertical component.
+    """Determine the total sum over the vertical component.
 
     Requires a 3D cube. The z-coordinate
     integration is calculated by taking the sum in the z direction of the
@@ -322,8 +315,7 @@ def depth_integration(cube):
 
 
 def extract_transect(cube, latitude=None, longitude=None):
-    """
-    Extract data along a line of constant latitude or longitude.
+    """Extract data along a line of constant latitude or longitude.
 
     Both arguments, latitude and longitude, are treated identically.
     Either argument can be a single float, or a pair of floats, or can be
@@ -413,8 +405,7 @@ def extract_transect(cube, latitude=None, longitude=None):
 
 
 def extract_trajectory(cube, latitudes, longitudes, number_points=2):
-    """
-    Extract data along a trajectory.
+    """Extract data along a trajectory.
 
     latitudes and longitudes are the pairs of coordinates for two points.
     number_points is the number of points between the two points.

tests/integration/test_main.py
@@ -13,8 +13,8 @@ from unittest.mock import patch
 import pytest
 from fire.core import FireExit
 
-import esmvalcore._recipe_checks as check
 from esmvalcore._main import Config, ESMValTool, Recipes, run
+from esmvalcore.exceptions import RecipeError
 
 
 def wrapper(f):
@@ -77,7 +77,7 @@ def test_empty_run(tmp_path):
     """)
     recipe_file.write_text(content)
     Config.get_config_user(path=tmp_path)
-    with pytest.raises(check.RecipeError) as exc:
+    with pytest.raises(RecipeError) as exc:
         ESMValTool.run(recipe_file, config_file=f"{tmp_path}/config-user.yml")
     assert str(exc.value) == 'The given recipe does not have any diagnostic.'
     log_dir = './esmvaltool_output'

tests/integration/test_recipe.py
@@ -12,9 +12,9 @@ from PIL import Image
 import esmvalcore
 from esmvalcore._config import TAGS
 from esmvalcore._recipe import TASKSEP, read_recipe_file
-from esmvalcore._recipe_checks import RecipeError
 from esmvalcore._task import DiagnosticTask
 from esmvalcore.cmor.check import CheckLevels
+from esmvalcore.exceptions import RecipeError
 from esmvalcore.preprocessor import DEFAULT_ORDER, PreprocessingTask
 from esmvalcore.preprocessor._io import concatenate_callback
 

tests/integration/test_recipe_checks.py
@@ -7,6 +7,7 @@ import pytest
 
 import esmvalcore._recipe_checks as check
 import esmvalcore.esgf
+from esmvalcore.exceptions import RecipeError
 
 ERR_ALL = 'Looked for files matching%s'
 ERR_D = ('Looked for files in %s, but did not find any file pattern to match '
@@ -58,7 +59,7 @@ def test_data_availability_data(mock_logger, input_files, var, error):
         check.data_availability(input_files, var, None, None)
         mock_logger.error.assert_not_called()
     else:
-        with pytest.raises(check.RecipeError) as rec_err:
+        with pytest.raises(RecipeError) as rec_err:
             check.data_availability(input_files, var, None, None)
         assert str(rec_err.value) == error
     assert var == saved_var
@@ -98,7 +99,7 @@ def test_data_availability_no_data(mock_logger, dirnames, filenames, error):
     }
     error_first = ('No input files found for variable %s', var_no_filename)
     error_last = ("Set 'log_level' to 'debug' to get more information", )
-    with pytest.raises(check.RecipeError) as rec_err:
+    with pytest.raises(RecipeError) as rec_err:
         check.data_availability([], var, dirnames, filenames)
     assert str(rec_err.value) == 'Missing data for alias: tas'
     if error is None:

tests/sample_data/experimental/test_run_recipe.py
@@ -9,7 +9,7 @@ import iris
 import pytest
 
 from esmvalcore._config import TAGS
-from esmvalcore._recipe import RecipeError
+from esmvalcore.exceptions import RecipeError
 from esmvalcore.experimental import CFG, Recipe, get_recipe
 from esmvalcore.experimental.recipe_output import (
     DataFile,

tests/unit/config/test_config.py
@@ -10,6 +10,7 @@ from esmvalcore._config._config import (
     get_extra_facets,
     importlib_files,
 )
+from esmvalcore.exceptions import RecipeError
 
 TEST_DEEP_UPDATE = [
     ([{}], {}),
@@ -88,6 +89,19 @@ def test_get_extra_facets_cmip5():
     extra_facets = get_extra_facets(**variable, extra_facets_dir=tuple())
 
     assert extra_facets == {'institute': ['CSIRO-BOM'], 'product': 'output1'}
+
+
+def test_get_project_config(mocker):
+    mock_result = mocker.Mock()
+    mocker.patch.object(_config, 'CFG', {'CMIP6': mock_result})
+
+    # Check valid result
+    result = _config.get_project_config('CMIP6')
+    assert result == mock_result
+
+    # Check error
+    with pytest.raises(RecipeError):
+        _config.get_project_config('non-existent-project')
 
 
 def test_load_default_config(monkeypatch):

tests/unit/data_finder/test_replace_tags.py
@@ -1,5 +1,8 @@
 """Tests for _replace_tags in _data_finder.py."""
+import pytest
+
 from esmvalcore._data_finder import _replace_tags
+from esmvalcore.exceptions import RecipeError
 
 VARIABLE = {
     'project': 'CMIP6',
@@ -29,6 +32,16 @@ def test_replace_tags():
     ]
     assert input_file == ['tas_Amon_ACCURATE-MODEL_experiment_r1i1p1f1_gr*.nc']
     assert output_file == ['CMIP6_ACCURATE-MODEL_Amon_experiment_r1i1p1f1_tas']
+
+
+def test_replace_tags_missing_facet():
+    """Check that a RecipeError is raised if a required facet is missing."""
+    paths = ['{short_name}_{missing}_*.nc']
+    variable = {'short_name': 'tas'}
+    with pytest.raises(RecipeError) as exc:
+        _replace_tags(paths, variable)
+
+    assert "Dataset key 'missing' must be specified" in exc.value.message
 
 
 def test_replace_tags_list_of_str():

tests/unit/test_main.py
@@ -1,0 +1,35 @@
+import sys
+
+import fire
+
+from esmvalcore import _main
+from esmvalcore.exceptions import RecipeError
+
+
+def test_run_recipe_error(mocker, caplog):
+    """Test a run of the tool with a mistake in the recipe."""
+    program = mocker.patch.object(
+        fire,
+        'Fire',
+        create_autospec=True,
+        instance=True,
+    )
+    msg = "A mistake in the recipe"
+    program.side_effect = RecipeError(msg)
+
+    exit_ = mocker.patch.object(sys, 'exit', create_autspec=True)
+
+    caplog.set_level('DEBUG')
+    _main.run()
+    print(caplog.text)
+
+    # Check that the exit status is 1
+    assert exit_.called_once_with(1)
+
+    # Check that only the RecipeError is logged above DEBUG level
+    errors = [r for r in caplog.records if r.levelname != 'DEBUG']
+    assert len(errors) == 1
+    assert errors[0].message == msg
+
+    # Check that the stack trace is logged
+    assert 'Traceback' in caplog.text

tests/unit/test_recipe.py
@@ -3,8 +3,8 @@ import pytest
 
 import esmvalcore.experimental.recipe_output
 from esmvalcore import _recipe
-from esmvalcore._recipe_checks import RecipeError
 from esmvalcore.esgf._download import ESGFFile
+from esmvalcore.exceptions import RecipeError
 
 
 class TestRecipe: