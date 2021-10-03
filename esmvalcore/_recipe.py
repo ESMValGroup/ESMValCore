ESMValGroup/ESMValCore

commit d83edb131318b312b0b4f805f461122f8f981415

Add support for re-using preprocessor output from previous runs (#1321)

esmvalcore/_recipe.py
@@ -28,7 +28,7 @@ from ._data_finder import (
 )
 from ._provenance import TrackedFile, get_recipe_provenance
 from ._recipe_checks import InputFilesNotFound, RecipeError
-from ._task import DiagnosticTask, TaskSet
+from ._task import DiagnosticTask, ResumeTask, TaskSet
 from .cmor.check import CheckLevels
 from .cmor.table import CMOR_TABLES
 from .preprocessor import (
@@ -1131,10 +1131,10 @@ class Recipe:
 
     @staticmethod
     def _expand_tag(variables, input_tag):
-        """Expand tags such as ensemble members or stardates to multiple
-        datasets.
+        """Expand tags such as ensemble members or startdates.
 
         Expansion only supports ensembles defined as strings, not lists.
+        Returns the expanded datasets.
         """
         expanded = []
         regex = re.compile(r'\(\d+:\d+\)')
@@ -1416,22 +1416,33 @@ class Recipe:
                         ancestors.extend(tasks[a] for a in ancestor_ids)
                     tasks[task_id].ancestors = ancestors
 
-    def initialize_tasks(self):
-        """Define tasks in recipe."""
-        logger.info("Creating tasks from recipe")
-        tasks = TaskSet()
-
-        run_diagnostic = self._cfg.get('run_diagnostic', True)
-        tasknames_to_run = self._cfg.get('diagnostics')
-
-        priority = 0
+    def _create_preprocessor_tasks(self, diagnostic_name, diagnostic):
+        """Create preprocessor tasks."""
+        tasks = []
         failed_tasks = []
-        for diagnostic_name, diagnostic in self.diagnostics.items():
-            logger.info("Creating tasks for diagnostic %s", diagnostic_name)
-
-            # Create preprocessor tasks
-            for variable_group in diagnostic['preprocessor_output']:
-                task_name = diagnostic_name + TASKSEP + variable_group
+        for variable_group in diagnostic['preprocessor_output']:
+            task_name = diagnostic_name + TASKSEP + variable_group
+            # Resume previous runs if requested, else create a new task
+            for resume_dir in self._cfg['resume_from']:
+                prev_preproc_dir = Path(
+                    resume_dir,
+                    'preproc',
+                    diagnostic_name,
+                    variable_group,
+                )
+                if prev_preproc_dir.exists():
+                    logger.info("Re-using preprocessed files from %s for %s",
+                                prev_preproc_dir, task_name)
+                    preproc_dir = Path(
+                        self._cfg['preproc_dir'],
+                        'preproc',
+                        diagnostic_name,
+                        variable_group,
+                    )
+                    task = ResumeTask(prev_preproc_dir, preproc_dir, task_name)
+                    tasks.append(task)
+                    break
+            else:
                 logger.info("Creating preprocessor task %s", task_name)
                 try:
                     task = _get_preprocessor_task(
@@ -1444,39 +1455,67 @@ class Recipe:
                 except RecipeError as ex:
                     failed_tasks.append(ex)
                 else:
-                    for task0 in task.flatten():
-                        task0.priority = priority
+                    tasks.append(task)
+
+        return tasks, failed_tasks
+
+    def _create_tasks(self):
+        """Create tasks from the recipe."""
+        logger.info("Creating tasks from recipe")
+        tasks = TaskSet()
+
+        priority = 0
+        failed_tasks = []
+        for diagnostic_name, diagnostic in self.diagnostics.items():
+            logger.info("Creating tasks for diagnostic %s", diagnostic_name)
+
+            # Create preprocessor tasks
+            new_tasks, failed = self._create_preprocessor_tasks(
+                diagnostic_name, diagnostic)
+            failed_tasks.extend(failed)
+            for task in new_tasks:
+                for task0 in task.flatten():
+                    task0.priority = priority
+                tasks.add(task)
+                priority += 1
+
+            # Create diagnostic tasks
+            if self._cfg.get('run_diagnostic', True):
+                for script_name, script_cfg in diagnostic['scripts'].items():
+                    task_name = diagnostic_name + TASKSEP + script_name
+                    logger.info("Creating diagnostic task %s", task_name)
+                    task = DiagnosticTask(
+                        script=script_cfg['script'],
+                        output_dir=script_cfg['output_dir'],
+                        settings=script_cfg['settings'],
+                        name=task_name,
+                    )
+                    task.priority = priority
                     tasks.add(task)
                     priority += 1
 
-            # Create diagnostic tasks
-            for script_name, script_cfg in diagnostic['scripts'].items():
-                task_name = diagnostic_name + TASKSEP + script_name
-                logger.info("Creating diagnostic task %s", task_name)
-                task = DiagnosticTask(
-                    script=script_cfg['script'],
-                    output_dir=script_cfg['output_dir'],
-                    settings=script_cfg['settings'],
-                    name=task_name,
-                )
-                task.priority = priority
-                tasks.add(task)
-                priority += 1
+        # Resolve diagnostic ancestors
+        if self._cfg.get('run_diagnostic', True):
+            self._resolve_diagnostic_ancestors(tasks)
+
         if failed_tasks:
             recipe_error = RecipeError('Could not create all tasks')
             recipe_error.failed_tasks.extend(failed_tasks)
             raise recipe_error
+
         check.tasks_valid(tasks)
 
-        # Resolve diagnostic ancestors
-        self._resolve_diagnostic_ancestors(tasks)
+        return tasks
+
+    def initialize_tasks(self):
+        """Define tasks in recipe."""
+        tasks = self._create_tasks()
 
         # Select only requested tasks
-        tasks = tasks.flatten()
-        if not run_diagnostic:
-            tasks = TaskSet(t for t in tasks
-                            if isinstance(t, PreprocessingTask))
+        tasknames_to_run = self._cfg.get('diagnostics')
+
         if tasknames_to_run:
+            tasks = tasks.flatten()
             names = {t.name for t in tasks}
             selection = set()
             for pattern in tasknames_to_run:

esmvalcore/_config/_config.py
@@ -102,21 +102,22 @@ def read_config_user_file(config_file, folder_name, options=None):
 
     # set defaults
     defaults = {
+        'auxiliary_data_dir': 'auxiliary_data',
         'compress_netcdf': False,
+        'config_developer_file': None,
+        'drs': {},
+        'download_dir': '~/climate_data',
         'exit_on_warning': False,
+        'extra_facets_dir': tuple(),
+        'max_parallel_tasks': None,
         'offline': True,
         'output_file_type': 'png',
         'output_dir': 'esmvaltool_output',
-        'download_dir': '~/climate_data',
-        'auxiliary_data_dir': 'auxiliary_data',
-        'extra_facets_dir': tuple(),
-        'save_intermediary_cubes': False,
-        'remove_preproc_dir': True,
-        'max_parallel_tasks': None,
-        'run_diagnostic': True,
         'profile_diagnostic': False,
-        'config_developer_file': None,
-        'drs': {},
+        'remove_preproc_dir': True,
+        'resume_from': [],
+        'run_diagnostic': True,
+        'save_intermediary_cubes': False,
     }
 
     for key in defaults:

esmvalcore/_main.py
@@ -28,6 +28,8 @@ http://docs.esmvaltool.org. Have fun!
 """  # noqa: line-too-long pylint: disable=line-too-long
 # pylint: disable=import-outside-toplevel
 import logging
+import os.path
+from pathlib import Path
 
 import fire
 from pkg_resources import iter_entry_points
@@ -47,10 +49,28 @@ ______________________________________________________________________
 """ + __doc__
 
 
+def parse_resume(resume, recipe):
+    """Set `resume` to a correct value and sanity check."""
+    if not resume:
+        return []
+    if isinstance(resume, str):
+        resume = resume.split(' ')
+    for i, resume_dir in enumerate(resume):
+        resume[i] = Path(os.path.expandvars(resume_dir)).expanduser()
+
+    # Sanity check resume directories:
+    current_recipe = recipe.read_text()
+    for resume_dir in resume:
+        resume_recipe = resume_dir / 'run' / recipe.name
+        if current_recipe != resume_recipe.read_text():
+            raise ValueError(f'Only identical recipes can be resumed, but '
+                             f'{resume_recipe} is different from {recipe}')
+    return resume
+
+
 def process_recipe(recipe_file, config_user):
     """Process recipe."""
     import datetime
-    import os
     import shutil
 
     from . import __version__
@@ -122,7 +142,6 @@ class Config():
 
     @staticmethod
     def _copy_config_file(filename, overwrite, path):
-        import os
         import shutil
 
         from ._config import configure_logging
@@ -197,8 +216,6 @@ class Recipes():
 
         Show all installed recipes, grouped by folder.
         """
-        import os
-
         from ._config import DIAGNOSTICS, configure_logging
         configure_logging(console_log_level='info')
         recipes_folder = DIAGNOSTICS.recipes
@@ -226,7 +243,6 @@ class Recipes():
             Name of the recipe to get, including any subdirectories.
         """
         import shutil
-        from pathlib import Path
 
         from ._config import DIAGNOSTICS, configure_logging
         configure_logging(console_log_level='info')
@@ -307,6 +323,7 @@ class ESMValTool():
     @staticmethod
     def run(recipe,
             config_file=None,
+            resume_from=None,
             max_datasets=None,
             max_years=None,
             skip_nonexistent=False,
@@ -328,6 +345,9 @@ class ESMValTool():
         config_file: str, optional
             Configuration file to use. If not provided the file
             ${HOME}/.esmvaltool/config-user.yml will be used.
+        resume_from: list(str), optional
+            Resume one or more previous runs by using preprocessor output files
+            from these output directories.
         max_datasets: int, optional
             Maximum number of datasets to use.
         max_years: int, optional
@@ -347,7 +367,6 @@ class ESMValTool():
             default (fail if there are any errors),
             strict (fail if there are any warnings).
         """
-        import os
         import shutil
 
         from ._config import (
@@ -363,12 +382,9 @@ class ESMValTool():
             installed_recipe = str(DIAGNOSTICS.recipes / recipe)
             if os.path.exists(installed_recipe):
                 recipe = installed_recipe
-        recipe = os.path.abspath(os.path.expandvars(
-            os.path.expanduser(recipe)))
+        recipe = Path(os.path.expandvars(recipe)).expanduser().absolute()
 
-        recipe_name = os.path.splitext(os.path.basename(recipe))[0]
-
-        cfg = read_config_user_file(config_file, recipe_name, kwargs)
+        cfg = read_config_user_file(config_file, recipe.stem, kwargs)
 
         # Create run dir
         if os.path.exists(cfg['run_dir']):
@@ -385,6 +401,7 @@ class ESMValTool():
         logger.info("Using config file %s", cfg['config_file'])
         logger.info("Writing program log files to:\n%s", "\n".join(log_files))
 
+        cfg['resume_from'] = parse_resume(resume_from, recipe)
         cfg['skip-nonexistent'] = skip_nonexistent
         if isinstance(diagnostics, str):
             diagnostics = diagnostics.split(' ')

esmvalcore/_provenance.py
@@ -6,7 +6,7 @@ import os
 from netCDF4 import Dataset
 from PIL import Image
 from PIL.PngImagePlugin import PngInfo
-from prov.model import ProvDocument
+from prov.model import ProvDerivation, ProvDocument
 
 from ._version import __version__
 
@@ -102,9 +102,31 @@ def get_task_provenance(task, recipe_entity):
 class TrackedFile:
     """File with provenance tracking."""
 
-    def __init__(self, filename, attributes, ancestors=None):
-        """Create an instance of a file with provenance tracking."""
+    def __init__(self,
+                 filename,
+                 attributes,
+                 ancestors=None,
+                 prov_filename=None):
+        """Create an instance of a file with provenance tracking.
+
+        Arguments
+        ---------
+        filename: str
+            Path to the file on disk.
+        attributes: dict
+            Dictionary with facets describing the file.
+        ancestors: :obj:`list` of :obj:`TrackedFile`
+            Ancestor files.
+        prov_filename: str
+            The path this file has in the provenance record. This can
+            differ from `filename` if the file was moved before resuming
+            processing.
+        """
         self._filename = filename
+        if prov_filename is None:
+            self.prov_filename = filename
+        else:
+            self.prov_filename = prov_filename
         self.attributes = copy.deepcopy(attributes)
 
         self.provenance = None
@@ -116,18 +138,11 @@ class TrackedFile:
         """Return summary string."""
         return "{}: {}".format(self.__class__.__name__, self.filename)
 
-    def copy_provenance(self, target=None):
+    def copy_provenance(self):
         """Create a copy with identical provenance information."""
         if self.provenance is None:
             raise ValueError("Provenance of {} not initialized".format(self))
-        if target is None:
-            new = TrackedFile(self.filename, self.attributes)
-        else:
-            if target.filename != self.filename:
-                raise ValueError(
-                    "Attempt to copy provenance to incompatible file.")
-            new = target
-            new.attributes = copy.deepcopy(self.attributes)
+        new = TrackedFile(self.filename, self.attributes)
         new.provenance = copy.deepcopy(self.provenance)
         new.entity = new.provenance.get_record(self.entity.identifier)[0]
         new.activity = new.provenance.get_record(self.activity.identifier)[0]
@@ -137,6 +152,11 @@ class TrackedFile:
     def filename(self):
         """Filename."""
         return self._filename
+
+    @property
+    def provenance_file(self):
+        """Filename of provenance."""
+        return os.path.splitext(self.filename)[0] + '_provenance.xml'
 
     def initialize_provenance(self, activity):
         """Initialize the provenance document.
@@ -173,6 +193,7 @@ class TrackedFile:
         }
         self.entity = self.provenance.entity('file:' + self.filename,
                                              attributes)
+
         attribute_to_authors(self.entity, self.attributes.get('authors', []))
         attribute_to_projects(self.entity, self.attributes.get('projects', []))
 
@@ -180,7 +201,10 @@ class TrackedFile:
         """Register ancestor files for provenance tracking."""
         for ancestor in self._ancestors:
             if ancestor.provenance is None:
-                ancestor.initialize_provenance(activity)
+                if os.path.exists(ancestor.provenance_file):
+                    ancestor.restore_provenance()
+                else:
+                    ancestor.initialize_provenance(activity)
             self.provenance.update(ancestor.provenance)
             self.wasderivedfrom(ancestor)
 
@@ -238,5 +262,21 @@ class TrackedFile:
             namespaces=self.provenance.namespaces,
         )
         self._include_provenance()
-        filename = os.path.splitext(self.filename)[0] + '_provenance'
-        self.provenance.serialize(filename + '.xml', format='xml')
+        self.provenance.serialize(self.provenance_file, format='xml')
+        self.activity = None
+        self.entity = None
+        self.provenance = None
+
+    def restore_provenance(self):
+        """Import provenance information from a previously saved file."""
+        self.provenance = ProvDocument.deserialize(self.provenance_file,
+                                                   format='xml')
+        entity_uri = f"{ESMVALTOOL_URI_PREFIX}file{self.prov_filename}"
+        self.entity = self.provenance.get_record(entity_uri)[0]
+        # Find the associated activity
+        for rec in self.provenance.records:
+            if isinstance(rec, ProvDerivation):
+                if rec.args[0] == self.entity.identifier:
+                    activity_id = rec.args[2]
+                    self.activity = self.provenance.get_record(activity_id)[0]
+                    break

esmvalcore/_task.py
@@ -215,6 +215,7 @@ def write_ncl_settings(settings, filename, mode='wt'):
 
 class BaseTask:
     """Base class for defining task classes."""
+
     def __init__(self, ancestors=None, name='', products=None):
         """Initialize task."""
         self.ancestors = [] if ancestors is None else ancestors
@@ -279,12 +280,51 @@ class BaseTask:
         return f"{self.__class__.__name__}({repr(self.name)})"
 
 
+class ResumeTask(BaseTask):
+    """Task for re-using preprocessor output files from a previous run."""
+
+    def __init__(self, prev_preproc_dir, preproc_dir, name):
+        """Create a resume task."""
+        # Set the path to the file resulting from running this task
+        self._metadata_file = preproc_dir / 'metadata.yml'
+
+        # Reconstruct output
+        prev_metadata_file = prev_preproc_dir / 'metadata.yml'
+        with prev_metadata_file.open('rb') as file:
+            prev_metadata = yaml.safe_load(file)
+
+        products = set()
+        for prov_filename, attributes in prev_metadata.items():
+            # Update the filename in case the output directory was moved
+            # since the original run
+            filename = str(prev_preproc_dir / Path(prov_filename).name)
+            attributes['filename'] = filename
+            product = TrackedFile(filename,
+                                  attributes,
+                                  prov_filename=prov_filename)
+            products.add(product)
+
+        super().__init__(ancestors=None, name=name, products=products)
+
+    def _run(self, _):
+        """Return the result of a previous run."""
+        metadata = self.get_product_attributes()
+
+        # Write metadata to file
+        self._metadata_file.parent.mkdir(parents=True)
+        with self._metadata_file.open('w') as file:
+            yaml.safe_dump(metadata, file)
+
+        return [str(self._metadata_file)]
+
+
 class DiagnosticError(Exception):
     """Error in diagnostic."""
 
 
 class DiagnosticTask(BaseTask):
     """Task for running a diagnostic."""
+
     def __init__(self, script, settings, output_dir, ancestors=None, name=''):
         """Create a diagnostic task."""
         super().__init__(ancestors=ancestors, name=name)
@@ -616,8 +656,8 @@ class DiagnosticTask(BaseTask):
 
             product = TrackedFile(filename, attributes, ancestors)
             product.initialize_provenance(self.activity)
-            product.save_provenance()
             _write_citation_files(product.filename, product.provenance)
+            product.save_provenance()
             self.products.add(product)
 
         if not valid:
@@ -732,14 +772,7 @@ class TaskSet(set):
 
 def _copy_results(task, future):
     """Update task with the results from the remote process."""
-    task.output_files, updated_products = future.get()
-    for updated in updated_products:
-        for original in task.products:
-            if original.filename == updated.filename:
-                updated.copy_provenance(target=original)
-                break
-        else:
-            task.products.add(updated)
+    task.output_files, task.products = future.get()
 
 
 def _run_task(task):

esmvalcore/experimental/config/_config_object.py
@@ -72,6 +72,7 @@ class Config(ValidatedConfig):
         mapping = _read_config_file(filename)
         # Add defaults that are not available in esmvalcore/config-user.yml
         mapping['extra_facets_dir'] = tuple()
+        mapping['resume_from'] = []
 
         new.update(mapping)
 

esmvalcore/experimental/config/_config_validators.py
@@ -269,6 +269,7 @@ _validators = {
     'output_file_type': validate_string,
 
     # From CLI
+    "resume_from": validate_pathlist,
     "skip-nonexistent": validate_bool,
     "diagnostics": validate_diagnostics,
     "check_level": validate_check_level,

esmvalcore/preprocessor/__init__.py
@@ -383,16 +383,17 @@ class PreprocessorFile(TrackedFile):
 
     def save(self):
         """Save cubes to disk."""
-        if self._cubes is not None:
-            self.files = preprocess(self._cubes, 'save',
-                                    **self.settings['save'])
-            self.files = preprocess(self.files, 'cleanup',
-                                    **self.settings.get('cleanup', {}))
+        self.files = preprocess(self._cubes, 'save',
+                                **self.settings['save'])
+        self.files = preprocess(self.files, 'cleanup',
+                                **self.settings.get('cleanup', {}))
 
     def close(self):
         """Close the file."""
-        self.save()
-        self._cubes = None
+        if self._cubes is not None:
+            self.save()
+            self._cubes = None
+            self.save_provenance()
 
     @property
     def is_closed(self):

commit e47208a4b1242be93746e52f2d5f0e8cfd8ebd8e

Fix problem with logging of missing data introduced in d83edb131318b312b0b4f805f461122f8f981415

esmvalcore/_recipe.py
@@ -1499,16 +1499,16 @@ class Recipe:
                     tasks.add(task)
                     priority += 1
 
-        # Resolve diagnostic ancestors
-        if self._cfg.get('run_diagnostic', True):
-            self._resolve_diagnostic_ancestors(tasks)
-
         if failed_tasks:
             recipe_error = RecipeError('Could not create all tasks')
             recipe_error.failed_tasks.extend(failed_tasks)
             raise recipe_error
 
         check.tasks_valid(tasks)
+
+        # Resolve diagnostic ancestors
+        if self._cfg.get('run_diagnostic', True):
+            self._resolve_diagnostic_ancestors(tasks)
 
         return tasks
 