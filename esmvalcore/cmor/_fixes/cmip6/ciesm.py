9844566addcc7a149c41b7e780e8d95c8657c68d
Axel Lauer <axel.lauer@dlr.de>
2021-10-12 12:08:45 +0200

    snapshot 2021-10-12

--- esmvalcore/cmor/_fixes/cmip6/ciesm.py
+++ esmvalcore/cmor/_fixes/cmip6/ciesm.py
@@ -1,8 +1,34 @@
 """Fixes for CIESM model."""
 from ..common import ClFixHybridPressureCoord
+from ..fix import Fix
 
 
 class Cl(ClFixHybridPressureCoord):
+    """Fixes for cl."""
+
+    def fix_data(self, cube):
+        """
+        Fix data.
+
+        Fixes discrepancy between declared units and real units.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+            Input cube.
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        metadata = cube.metadata
+        cube *= 100
+        cube.metadata = metadata
+        return cube
+
+
+class Clt(Fix):
     """Fixes for cl."""
 
     def fix_data(self, cube):
---
+++ esmvalcore/cmor/_fixes/obs4mips/airs_2_1.py
@@ -0,0 +1,35 @@
+
+"""Fixes for obs4mips dataset AIRS-2-1."""
+import iris
+from iris.cube import CubeList
+from cf_units import Unit
+
+from ..fix import Fix
+
+
+class AllVars(Fix):
+    """Common fixes to all vars."""
+
+    def fix_metadata(self, cubes):
+        """
+        Fix metadata.
+
+        Change unit of coordinate plev from hPa to Pa
+
+        Parameters
+        ----------
+        cube: iris.cube.CubeList
+
+        Returns
+        -------
+        iris.cube.Cube
+
+        """
+        for cube in cubes:
+            try:
+                plev = cube.coord('air_pressure')
+            except iris.exceptions.CoordinateNotFoundError:
+                continue
+            else:
+                plev.units = Unit('Pa')
+        return cubes
