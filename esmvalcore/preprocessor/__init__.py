2180c9ec9670616bc9d02aabd5963e2f6fd375a3
Manuel Schlund <manuel.schlund@dlr.de>
2021-12-08 16:47:22 +0100

    Improved error message when a preprocessor is failing

--- esmvalcore/preprocessor/__init__.py
+++ esmvalcore/preprocessor/__init__.py
@@ -277,18 +277,51 @@ def _get_multi_model_settings(products, step):
     return settings, exclude
 
 
-def _run_preproc_function(function, items, kwargs):
+def _run_preproc_function(function, items, kwargs, input_files=None):
     """Run preprocessor function."""
-    msg = "{}({}, {})".format(function.__name__, items, kwargs)
-    logger.debug("Running %s", msg)
+    if input_files is None:
+        file_msg = ""
+    else:
+        file_msg = f"\nOriginal input file(s):\n{pformat(input_files)}"
+    logger.debug("Running %s with option(s)\n%s\non argument(s)\n%s%s",
+                 function.__name__, pformat(kwargs), pformat(items), file_msg)
     try:
         return function(items, **kwargs)
     except Exception:
+        # To avoid very long error messages, we truncate the arguments and
+        # input files here at a given threshold
+        n_shown_args = 4
+        if input_files is None:
+            file_msg = ""
+        elif len(input_files) <= n_shown_args:
+            file_msg = f"\nOriginal input file(s):\n{pformat(input_files)}"
+        else:
+            n_not_shown_files = len(input_files) - n_shown_args
+            file_msg = (f"\nOriginal input file(s):\n"
+                        f"{pformat(input_files[:n_shown_args])}\n(and "
+                        f"{n_not_shown_files:d} further file(s) not shown "
+                        f"here; refer to the debug log for a full list)")
+
+        # Make sure that the arguments are indexable
+        if isinstance(items, (PreprocessorFile, Cube, str)):
+            items = [items]
+        if isinstance(items, set):
+            items = list(items)
+
+        if len(items) <= n_shown_args:
+            msg = (f"{function.__name__} with option(s)\n{pformat(kwargs)}\n"
+                   f"on argument(s)\n{pformat(items)}{file_msg}")
+        else:
+            n_not_shown_args = len(items) - n_shown_args
+            msg = (f"{function.__name__} with option(s)\n{pformat(kwargs)}\n"
+                   f"on argument(s)\n{pformat(items[:n_shown_args])}\n(and "
+                   f"{n_not_shown_args:d} further argument(s) not shown here; "
+                   f"refer to the debug log for a full list){file_msg}")
         logger.error("Failed to run %s", msg)
         raise
 
 
-def preprocess(items, step, **settings):
+def preprocess(items, step, input_files=None, **settings):
     """Run preprocessor."""
     logger.debug("Running preprocessor step %s", step)
     function = globals()[step]
@@ -296,10 +329,12 @@ def preprocess(items, step, **settings):
 
     result = []
     if itype.endswith('s'):
-        result.append(_run_preproc_function(function, items, settings))
+        result.append(_run_preproc_function(function, items, settings,
+                                            input_files=input_files))
     else:
         for item in items:
-            result.append(_run_preproc_function(function, item, settings))
+            result.append(_run_preproc_function(function, item, settings,
+                                                input_files=input_files))
 
     items = []
     for item in result:
@@ -338,10 +373,20 @@ class PreprocessorFile(TrackedFile):
             self.settings['save'] = {}
         self.settings['save']['filename'] = self.filename
 
-        self.files = [a.filename for a in ancestors or ()]
+        # self._input_files always contains the original input files;
+        # self.files may change in the preprocessing chain (e.g., by the step
+        # fix_file)
+        self._input_files = [a.filename for a in ancestors or ()]
+        self.files = copy.deepcopy(self._input_files)
 
         self._cubes = None
         self._prepared = False
+
+    def _input_files_for_log(self):
+        """Do not log input files twice in output log."""
+        if self.files == self._input_files:
+            return None
+        return self._input_files
 
     def check(self):
         """Check preprocessor settings."""
@@ -353,7 +398,9 @@ class PreprocessorFile(TrackedFile):
             raise ValueError(
                 "PreprocessorFile {} has no settings for step {}".format(
                     self, step))
-        self.cubes = preprocess(self.cubes, step, **self.settings[step])
+        self.cubes = preprocess(self.cubes, step,
+                                input_files=self._input_files,
+                                **self.settings[step])
         if debug:
             logger.debug("Result %s", self.cubes)
             filename = _get_debug_filename(self.filename, step)
@@ -364,8 +411,10 @@ class PreprocessorFile(TrackedFile):
         if not self._prepared:
             for step in DEFAULT_ORDER[:DEFAULT_ORDER.index('load')]:
                 if step in self.settings:
-                    self.files = preprocess(self.files, step,
-                                            **self.settings[step])
+                    self.files = preprocess(
+                        self.files, step,
+                        input_files=self._input_files_for_log(),
+                        **self.settings[step])
             self._prepared = True
 
     @property
@@ -374,6 +423,7 @@ class PreprocessorFile(TrackedFile):
         if self.is_closed:
             self.prepare()
             self._cubes = preprocess(self.files, 'load',
+                                     input_files=self._input_files_for_log(),
                                      **self.settings.get('load', {}))
         return self._cubes
 
@@ -384,8 +434,10 @@ class PreprocessorFile(TrackedFile):
     def save(self):
         """Save cubes to disk."""
         self.files = preprocess(self._cubes, 'save',
+                                input_files=self._input_files,
                                 **self.settings['save'])
         self.files = preprocess(self.files, 'cleanup',
+                                input_files=self._input_files,
                                 **self.settings.get('cleanup', {}))
 
     def close(self):
---
+++ tests/unit/preprocessor/test_error_logging.py
@@ -0,0 +1,242 @@
+"""Unit tests for error logging of :mod:`esmvalcore.preprocessor`."""
+
+from unittest import mock
+
+import pytest
+from iris.cube import Cube, CubeList
+
+from esmvalcore.preprocessor import PreprocessorFile, _run_preproc_function
+
+VALUE_ERROR_MSG = "This function is expected to fail with exactly this error"
+
+
+def failing_function(*_, **__):
+    """Raise ValueError."""
+    raise ValueError(VALUE_ERROR_MSG)
+
+
+def assert_debug_call_ok(mock_logger, items):
+    """Check debug call."""
+    mock_logger.debug.assert_called_once()
+    assert mock_logger.debug.call_args.kwargs == {}
+    debug_call_args = mock_logger.debug.call_args.args
+    assert debug_call_args[0] == ("Running %s with option(s)\n%s\non argument"
+                                  "(s)\n%s%s")
+    assert debug_call_args[1] == "failing_function"
+    assert debug_call_args[2] == "{'test': 42}"
+    if isinstance(items, (PreprocessorFile, Cube, str)):
+        assert debug_call_args[3] == repr(items)
+    else:
+        for item in items:
+            assert repr(item) in debug_call_args[3]
+
+
+def assert_error_call_ok(mock_logger):
+    """Check error call."""
+    mock_logger.error.assert_called_once()
+    assert mock_logger.error.call_args.kwargs == {}
+    error_call_args = mock_logger.error.call_args.args
+    assert error_call_args[0] == "Failed to run %s"
+    expected_str = ("failing_function with option(s)\n{'test': 42}\non "
+                    "argument(s)\n")
+    assert expected_str in error_call_args[1]
+
+
+KWARGS = {'test': 42}
+PREPROC_FILE = PreprocessorFile({'filename': 'a'}, {})
+TEST_ITEMS_SHORT = [
+    # Scalars
+    PREPROC_FILE,
+    Cube(0),
+    'a',
+    # 1-element lists
+    [PREPROC_FILE],
+    [Cube(0)],
+    ['a'],
+    # 1-element sets
+    set([PREPROC_FILE]),
+    set([Cube(0)]),
+    set(['a']),
+    # 1-element CubeList
+    CubeList([Cube(0)]),
+    # 4-element lists
+    [PREPROC_FILE] * 4,
+    [Cube(0)] * 4,
+    ['a'] * 4,
+    # 4-element sets
+    set(['a', 'b', 'c', 'd']),
+    # 4-element CubeList
+    CubeList([Cube(0), Cube(1), Cube(2), Cube(3)]),
+]
+TEST_ITEMS_LONG = [
+    # 6-element list
+    ['a', 'b', 'c', 'd', 'e', 'f'],
+    # 6-element set
+    set(['a', 'b', 'c', 'd', 'e', 'f']),
+]
+SHORT_INPUT_FILES = ['x', 'y', 'z', 'w']
+LONG_INPUT_FILES = ['x', 'y', 'z', 'w', 'v', 'u']
+
+
+@pytest.mark.parametrize('items', TEST_ITEMS_SHORT)
+@mock.patch('esmvalcore.preprocessor.logger', autospec=True)
+def test_short_items_no_input_files(mock_logger, items):
+    """Test short list of items and no input files."""
+    with pytest.raises(ValueError, match=VALUE_ERROR_MSG):
+        _run_preproc_function(failing_function, items, KWARGS)
+    assert len(mock_logger.mock_calls) == 2
+
+    # Debug call
+    assert_debug_call_ok(mock_logger, items)
+    assert mock_logger.debug.call_args.args[4] == ""
+
+    # Error call
+    assert_error_call_ok(mock_logger)
+    error_call_args = mock_logger.error.call_args.args
+    if isinstance(items, (PreprocessorFile, Cube, str)):
+        assert repr(items) in error_call_args[1]
+    else:
+        for item in items:
+            assert repr(item) in error_call_args[1]
+    assert "\nOriginal input file(s):\n" not in error_call_args[1]
+    assert "further file(s) not shown here;" not in error_call_args[1]
+    assert "further argument(s) not shown here;" not in error_call_args[1]
+
+
+@pytest.mark.parametrize('items', TEST_ITEMS_SHORT)
+@mock.patch('esmvalcore.preprocessor.logger', autospec=True)
+def test_short_items_short_input_files(mock_logger, items):
+    """Test short list of items and short list of input files."""
+    with pytest.raises(ValueError, match=VALUE_ERROR_MSG):
+        _run_preproc_function(failing_function, items, KWARGS,
+                              input_files=SHORT_INPUT_FILES)
+    assert len(mock_logger.mock_calls) == 2
+
+    # Debug call
+    assert_debug_call_ok(mock_logger, items)
+    assert mock_logger.debug.call_args.args[4] == (
+        "\nOriginal input file(s):\n['x', 'y', 'z', 'w']")
+
+    # Error call
+    assert_error_call_ok(mock_logger)
+    error_call_args = mock_logger.error.call_args.args
+    if isinstance(items, (PreprocessorFile, Cube, str)):
+        assert repr(items) in error_call_args[1]
+    else:
+        for item in items:
+            assert repr(item) in error_call_args[1]
+    assert ("\nOriginal input file(s):\n['x', 'y', 'z', 'w']" in
+            error_call_args[1])
+    assert "further file(s) not shown here;" not in error_call_args[1]
+    assert "further argument(s) not shown here;" not in error_call_args[1]
+
+
+@pytest.mark.parametrize('items', TEST_ITEMS_SHORT)
+@mock.patch('esmvalcore.preprocessor.logger', autospec=True)
+def test_short_items_long_input_files(mock_logger, items):
+    """Test short list of items and long list of input files."""
+    with pytest.raises(ValueError, match=VALUE_ERROR_MSG):
+        _run_preproc_function(failing_function, items, KWARGS,
+                              input_files=LONG_INPUT_FILES)
+    assert len(mock_logger.mock_calls) == 2
+
+    # Debug call
+    assert_debug_call_ok(mock_logger, items)
+    assert mock_logger.debug.call_args.args[4] == (
+        "\nOriginal input file(s):\n['x', 'y', 'z', 'w', 'v', 'u']")
+
+    # Error call
+    assert_error_call_ok(mock_logger)
+    error_call_args = mock_logger.error.call_args.args
+    if isinstance(items, (PreprocessorFile, Cube, str)):
+        assert repr(items) in error_call_args[1]
+    else:
+        for item in items:
+            assert repr(item) in error_call_args[1]
+    assert ("\nOriginal input file(s):\n['x', 'y', 'z', 'w']\n" in
+            error_call_args[1])
+    assert "['x', 'y', 'z', 'w', 'v', 'u']" not in error_call_args[1]
+    assert "\n(and 2 further file(s) not shown here;" in error_call_args[1]
+    assert "further argument(s) not shown here;" not in error_call_args[1]
+
+
+@pytest.mark.parametrize('items', TEST_ITEMS_LONG)
+@mock.patch('esmvalcore.preprocessor.logger', autospec=True)
+def test_long_items_no_input_files(mock_logger, items):
+    """Test long list of items and no input files."""
+    with pytest.raises(ValueError, match=VALUE_ERROR_MSG):
+        _run_preproc_function(failing_function, items, KWARGS)
+    assert len(mock_logger.mock_calls) == 2
+
+    # Debug call
+    assert_debug_call_ok(mock_logger, items)
+    assert mock_logger.debug.call_args.args[4] == ""
+
+    # Error call
+    assert_error_call_ok(mock_logger)
+    error_call_args = mock_logger.error.call_args.args
+    items = list(items)
+    for item in items[:4]:
+        assert repr(item) in error_call_args[1]
+    for item in items[4:]:
+        assert repr(item) not in error_call_args[1]
+    assert "\nOriginal input file(s):\n" not in error_call_args[1]
+    assert "further file(s) not shown here;" not in error_call_args[1]
+    assert "\n(and 2 further argument(s) not shown here;" in error_call_args[1]
+
+
+@pytest.mark.parametrize('items', TEST_ITEMS_LONG)
+@mock.patch('esmvalcore.preprocessor.logger', autospec=True)
+def test_long_items_short_input_files(mock_logger, items):
+    """Test long list of items and short list of input files."""
+    with pytest.raises(ValueError, match=VALUE_ERROR_MSG):
+        _run_preproc_function(failing_function, items, KWARGS,
+                              input_files=SHORT_INPUT_FILES)
+    assert len(mock_logger.mock_calls) == 2
+
+    # Debug call
+    assert_debug_call_ok(mock_logger, items)
+    assert mock_logger.debug.call_args.args[4] == (
+        "\nOriginal input file(s):\n['x', 'y', 'z', 'w']")
+
+    # Error call
+    assert_error_call_ok(mock_logger)
+    error_call_args = mock_logger.error.call_args.args
+    items = list(items)
+    for item in items[:4]:
+        assert repr(item) in error_call_args[1]
+    for item in items[4:]:
+        assert repr(item) not in error_call_args[1]
+    assert ("\nOriginal input file(s):\n['x', 'y', 'z', 'w']" in
+            error_call_args[1])
+    assert "further file(s) not shown here;" not in error_call_args[1]
+    assert "\n(and 2 further argument(s) not shown here;" in error_call_args[1]
+
+
+@pytest.mark.parametrize('items', TEST_ITEMS_LONG)
+@mock.patch('esmvalcore.preprocessor.logger', autospec=True)
+def test_long_items_long_input_files(mock_logger, items):
+    """Test long list of items and long list of input files."""
+    with pytest.raises(ValueError, match=VALUE_ERROR_MSG):
+        _run_preproc_function(failing_function, items, KWARGS,
+                              input_files=LONG_INPUT_FILES)
+    assert len(mock_logger.mock_calls) == 2
+
+    # Debug call
+    assert_debug_call_ok(mock_logger, items)
+    assert mock_logger.debug.call_args.args[4] == (
+        "\nOriginal input file(s):\n['x', 'y', 'z', 'w', 'v', 'u']")
+
+    # Error call
+    assert_error_call_ok(mock_logger)
+    error_call_args = mock_logger.error.call_args.args
+    items = list(items)
+    for item in items[:4]:
+        assert repr(item) in error_call_args[1]
+    for item in items[4:]:
+        assert repr(item) not in error_call_args[1]
+    assert ("\nOriginal input file(s):\n['x', 'y', 'z', 'w']\n" in
+            error_call_args[1])
+    assert "['x', 'y', 'z', 'w', 'v', 'u']" not in error_call_args[1]
+    assert "\n(and 2 further file(s) not shown here;" in error_call_args[1]
+    assert "\n(and 2 further argument(s) not shown here;" in error_call_args[1]
