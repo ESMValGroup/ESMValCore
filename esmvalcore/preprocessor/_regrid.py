7a776725fa55a6fceead0a98cbf80a5b0446927c
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-01-20 14:08:30 +0000

    Update esmvalcore/preprocessor/_regrid.py
    Co-authored-by: Manuel Schlund <32543114+schlunma@users.noreply.github.com>

--- esmvalcore/preprocessor/_regrid.py
+++ esmvalcore/preprocessor/_regrid.py
@@ -356,7 +356,7 @@ def extract_location(cube, location, scheme):
     geolocation = geolocator.geocode(location)
     if geolocation is None:
         raise ValueError(f'Requested location {location} can not be found.')
-    logger.debug("Extracting data for %s (%s ºN, %s ºW)", geolocation,
+    logger.debug("Extracting data for %s (%s °N, %s °E)", geolocation,
                  geolocation.latitude, geolocation.longitude)
 
     return extract_point(cube, geolocation.latitude, geolocation.longitude, scheme)
