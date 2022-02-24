ab1a5beb13b5e6bc7b590ec0ea311373380f3f99
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-02-24 15:43:08 +0000

    added test module for iris stuff

---
+++ tests/unit/test_iris_io.py
@@ -0,0 +1,37 @@
+import dask.array as da
+import iris
+import numpy as np
+from iris.coords import AuxCoord, DimCoord
+from iris.cube import Cube
+
+
+def create_fully_lazy_cube():
+    """Create cube with lazy aux coord and aggregate over this dimension."""
+    dim_coord = DimCoord(np.arange(10), var_name='time')
+    # fully lazy coord points
+    aux_coord = AuxCoord(da.arange(10), var_name='year')
+    cube = Cube(
+        np.arange(10),
+        var_name='x',
+        dim_coords_and_dims=[(dim_coord, 0)],
+        aux_coords_and_dims=[(aux_coord, 0)],
+    )
+
+    cube = cube.collapsed('time', iris.analysis.MEAN)
+
+    return cube
+
+
+def test_iris_save_with_lazy_coordinate(tmp_path):
+    """
+    Test saving a cube with fully lazy coords.
+
+    Motivated by https://github.com/SciTools/iris/issues/4599
+    """
+    print("iris version:", iris.__version__)
+    cube = create_fully_lazy_cube()
+    save_path = tmp_path / 'test_iris_v32.nc'
+    iris.save(cube, save_path)
+    print("Attempted to load ", save_path)
+    loaded_cube = iris.load_cube(save_path)
+    assert loaded_cube
