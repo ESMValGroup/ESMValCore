5ee22fd79d3ff81f768c318ffa50eaf62991ad11
Bouwe Andela <b.andela@esciencecenter.nl>
2022-05-12 16:52:17 +0200

    Add a warning about missing version info

--- esmvalcore/_version.py
+++ esmvalcore/_version.py
@@ -1,7 +1,10 @@
+import warnings
 from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("ESMValCore")
 except PackageNotFoundError:
     # package is not installed
+    warnings.warn(
+        "No version information available, please install the package.")
     __version__ = "unknown"
