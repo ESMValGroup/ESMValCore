d2e38ca0f4d5e703e0c3f6836bc4cfa441ff9e71
Elizaveta Malinina <elizaveta.malinina-rieger@ec.gc.ca>
2022-07-20 18:53:45 +0000

    added rolling_window_operator preprocessor function

---
+++ esmvalcore/preprocessor/_rolling_window.py
@@ -0,0 +1,48 @@
+"""Rolling-window operations on data cubes."""
+
+import logging
+import iris
+
+from ._shared import get_iris_analysis_operation
+
+logger = logging.getLogger(__name__)
+
+
+def rolling_window_statistics(cube, coordinate, operator, window_length):
+    """Compute rolling-window statistics over a coordinate.
+    
+    Parameters
+    ----------
+    cube : iris.cube.Cube
+        Input cube.
+    coordinate : str
+        Coordinate over which the rolling-window statistics is calculated.
+    operator : str
+        Select operator to apply. Available operators: ``'mean'``,
+        ``'median'``, ``'std_dev'``, ``'sum'``, ``'variance'``, ``'min'``,
+        ``'max'``.
+    window : int
+        Size of the window to use.
+    Returns
+    -------
+    iris.cube.Cube
+        Rolling-window statistics cube.
+    Raises
+    ------
+    iris.exceptions.CoordinateNotFoundError:
+        Cube does not have time coordinate.
+    ValueError:
+        Invalid ``'operator'`` given.
+        
+    """
+    try:
+        cube.coord(coordinate)
+    except iris.exceptions.CoordinateNotFoundError:
+        logger.error(f'Cube %s does not have {coordinate} coordinate', cube)
+        raise
+     
+    operation = get_iris_analysis_operation(operator)
+    # applying rolling wondow
+    cube = cube.rolling_window(coordinate, operation, window_length)
+
+    return cube
