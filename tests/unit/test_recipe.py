e3338785e874865efb935ab415a448355d030550
Bouwe Andela <b.andela@esciencecenter.nl>
2022-08-19 13:52:13 +0200

    Fix more tests

--- tests/unit/test_recipe.py
+++ tests/unit/test_recipe.py
@@ -282,8 +282,10 @@ def test_update_multiproduct_multi_model_statistics():
     assert len(output) == 2
 
     filenames = [p.filename for p in output]
-    assert '/preproc/d/var/CMIP6_MultiModelMean_2002-2004.nc' in filenames
-    assert '/preproc/d/var/CMIP6_MultiModelStd_Dev_2002-2004.nc' in filenames
+    assert Path(
+        '/preproc/d/var/CMIP6_MultiModelMean_2002-2004.nc') in filenames
+    assert Path(
+        '/preproc/d/var/CMIP6_MultiModelStd_Dev_2002-2004.nc') in filenames
 
     for product in output:
         for attr in common_attributes:
@@ -298,12 +300,12 @@ def test_update_multiproduct_multi_model_statistics():
             assert product.attributes['start_year'] == 2002
             assert 'end_year' in product.attributes
             assert product.attributes['end_year'] == 2004
-        if 'MultiModelStd_Dev' in product.filename:
+        if 'MultiModelStd_Dev' in str(product.filename):
             assert product.attributes['alias'] == 'MultiModelStd_Dev'
             assert product.attributes['dataset'] == 'MultiModelStd_Dev'
             assert (product.attributes['multi_model_statistics'] ==
                     'MultiModelStd_Dev')
-        elif 'MultiModelMean' in product.filename:
+        elif 'MultiModelMean' in str(product.filename):
             assert product.attributes['alias'] == 'MultiModelMean'
             assert product.attributes['dataset'] == 'MultiModelMean'
             assert (product.attributes['multi_model_statistics'] ==
@@ -316,8 +318,8 @@ def test_update_multiproduct_multi_model_statistics():
     assert len(stats) == 2
     assert 'mean' in stats
     assert 'std_dev' in stats
-    assert 'MultiModelMean' in stats['mean'].filename
-    assert 'MultiModelStd_Dev' in stats['std_dev'].filename
+    assert 'MultiModelMean' in str(stats['mean'].filename)
+    assert 'MultiModelStd_Dev' in str(stats['std_dev'].filename)
 
 
 def test_update_multiproduct_ensemble_statistics():
@@ -354,8 +356,8 @@ def test_update_multiproduct_ensemble_statistics():
 
     assert len(output) == 1
     product = list(output)[0]
-    assert (product.filename ==
-            '/preproc/d/var/CMIP6_CanESM2_EnsembleMedian_2000-2000.nc')
+    assert (product.filename == Path(
+        '/preproc/d/var/CMIP6_CanESM2_EnsembleMedian_2000-2000.nc'))
 
     for attr in common_attributes:
         assert attr in product.attributes
@@ -377,8 +379,8 @@ def test_update_multiproduct_ensemble_statistics():
     stats = output_products['CMIP6_CanESM2']
     assert len(stats) == 1
     assert 'median' in stats
-    assert (stats['median'].filename ==
-            '/preproc/d/var/CMIP6_CanESM2_EnsembleMedian_2000-2000.nc')
+    assert (stats['median'].filename == Path(
+        '/preproc/d/var/CMIP6_CanESM2_EnsembleMedian_2000-2000.nc'))
 
 
 def test_update_multiproduct_no_product():
