c3b1bc113a98abbbf0d5ee2aee5e82acadbbb4cd
sloosvel <saskia.loosveldt@bsc.es>
2022-10-19 14:45:27 +0200

    Remove dataset that may not need fix

--- esmvalcore/cmor/_fixes/cordex/mohc_hadgem2_es/ictp_regcm4_6.py
+++
@@ -1,6 +0,0 @@
-# 3hr data between historical and scenario cannot be concatenated because historical 
-# finishes at 
-# Cell(point=cftime.DatetimeGregorian(2005, 12, 31, 21, 0, 0, 0, has_year_zero=False), bound=None)
-# and scenario finishes at
-# Cell(point=cftime.DatetimeGregorian(2006, 1, 1, 3, 0, 0, 0, has_year_zero=False), bound=None)
-# which is 6h apart instead of 3h
