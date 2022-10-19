02e795abf6f94a95eb635a98fbda19f7abe04c8e
sloosvel <saskia.loosveldt@bsc.es>
2022-10-19 18:07:01 +0200

    Fix imports

--- esmvalcore/cmor/_fixes/cordex/miroc_miroc5/clmcom_cclm4_8_17.py
+++ esmvalcore/cmor/_fixes/cordex/miroc_miroc5/clmcom_cclm4_8_17.py
@@ -1,10 +1,4 @@
 """Fixes for rcm CLMcom-CCLM4-8-17 driven by MIROC-MIROC5."""
-from esmvalcore.cmor.fix import Fix
-
-from cf_units import Unit
-import numpy as np
-
-
 from ..cordex_fixes import CLMcomCCLM4817 as BaseFix
 
 AllVars = BaseFix
