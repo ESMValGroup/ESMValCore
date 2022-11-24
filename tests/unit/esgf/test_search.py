aa978175eefb9e8180beaebfa11cb7b251cd9317
Bouwe Andela <b.andela@esciencecenter.nl>
2022-11-24 21:07:50 +0100

    Add some tests

--- tests/unit/esgf/test_search.py
+++ tests/unit/esgf/test_search.py
@@ -55,6 +55,12 @@ OUR_FACETS = (
         'project': 'obs4MIPs',
         'short_name': 'rsutcs',
     },
+    {
+        'dataset': 'CERES-EBAF',
+        'frequency': '*',
+        'project': 'obs4MIPs',
+        'short_name': 'rsutcs',
+    },
 )
 
 ESGF_FACETS = (
@@ -97,6 +103,11 @@ ESGF_FACETS = (
         'project': 'obs4MIPs',
         'source_id': 'CERES-EBAF',
         'time_frequency': 'mon',
+        'variable': 'rsutcs',
+    },
+    {
+        'project': 'obs4MIPs',
+        'source_id': 'CERES-EBAF',
         'variable': 'rsutcs',
     },
 )
@@ -286,7 +297,23 @@ def test_esgf_search_fails(mocker):
     assert str(excinfo.value) == error_message
 
 
-def test_select_by_time():
+def test_select_latest_versions_filenotfound(mocker):
+    """Test `select_latest_versions` raises FileNotFoundError."""
+    file = mocker.create_autospec(ESGFFile, instance=True)
+    file.name = 'ta.nc'
+    file.dataset = 'CMIP6.MODEL.v1'
+    file.facets = {'version': 'v1'}
+    file.__repr__ = lambda _: 'ESGFFile:CMIP6/MODEL/v1/ta.nc'
+    with pytest.raises(FileNotFoundError):
+        _search.select_latest_versions(files=[file], versions='v2')
+
+
+@pytest.mark.parametrize('timerange,selection', [
+    ('1851/1852', slice(1, 3)),
+    ('1851/P1Y', slice(1, 3)),
+    ('*', slice(None)),
+])
+def test_select_by_time(timerange, selection):
     dataset_id = ('CMIP6.CMIP.AWI.AWI-ESM-1-1-LR.historical'
                   '.r1i1p1f1.Amon.tas.gn.v20200212')
     dataset_id_template = (
@@ -314,8 +341,8 @@ def test_select_by_time():
     ]
     files = [ESGFFile([r]) for r in results]
 
-    result = _search.select_by_time(files, '1851/1852')
-    reference = files[1:3]
+    result = _search.select_by_time(files, timerange)
+    reference = files[selection]
     assert sorted(result) == sorted(reference)
 
 
@@ -343,40 +370,6 @@ def test_select_by_time_nodate():
 
     result = _search.select_by_time(files, '1851/1852')
     assert result == files
-
-
-def test_select_by_time_period():
-
-    dataset_id = ('CMIP6.CMIP.AWI.AWI-ESM-1-1-LR.historical'
-                  '.r1i1p1f1.Amon.tas.gn.v20200212')
-    dataset_id_template = (
-        '%(mip_era)s.%(activity_drs)s.%(institution_id)s.'
-        '%(source_id)s.%(experiment_id)s.%(member_id)s.%(table_id)s.'
-        '%(variable_id)s.%(grid_label)s'
-    )
-    filenames = [
-        'tas_Amon_AWI-ESM-1-1-LR_historical_r1i1p1f1_gn_185001-185012.nc',
-        'tas_Amon_AWI-ESM-1-1-LR_historical_r1i1p1f1_gn_185101-185112.nc',
-        'tas_Amon_AWI-ESM-1-1-LR_historical_r1i1p1f1_gn_185201-185212.nc',
-        'tas_Amon_AWI-ESM-1-1-LR_historical_r1i1p1f1_gn_185301-185312.nc',
-    ]
-    results = [
-        FileResult(
-            json={
-                'title': filename,
-                'dataset_id': dataset_id + '|xyz.com',
-                'dataset_id_template_': [dataset_id_template],
-                'project': ['CMIP5'],
-                'size': 100,
-            },
-            context=None,
-        ) for filename in filenames
-    ]
-    files = [ESGFFile([r]) for r in results]
-
-    result = _search.select_by_time(files, '1851/P1Y')
-    reference = files[1:3]
-    assert sorted(result) == sorted(reference)
 
 
 def test_search_unknown_project():
