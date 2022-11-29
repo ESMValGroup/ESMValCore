0c2f14f1d9419bb3f2270c84830f3ce99ec87e20
Manuel Schlund <manuel.schlund@dlr.de>
2022-11-29 15:45:53 +0100

    Updated product attributes with cube metadata at closing

--- esmvalcore/preprocessor/__init__.py
+++ esmvalcore/preprocessor/__init__.py
@@ -228,8 +228,9 @@ def check_preprocessor_settings(settings):
     for step in settings:
         if step not in DEFAULT_ORDER:
             raise ValueError(
-                "Unknown preprocessor function '{}', choose from: {}".format(
-                    step, ', '.join(DEFAULT_ORDER)))
+                f"Unknown preprocessor function '{step}', choose from: "
+                f"{', '.join(DEFAULT_ORDER)}"
+            )
 
         function = function = globals()[step]
         argspec = inspect.getfullargspec(function)
@@ -239,9 +240,10 @@ def check_preprocessor_settings(settings):
             invalid_args = set(settings[step]) - set(args)
             if invalid_args:
                 raise ValueError(
-                    "Invalid argument(s): {} encountered for preprocessor "
-                    "function {}. \nValid arguments are: [{}]".format(
-                        ', '.join(invalid_args), step, ', '.join(args)))
+                    f"Invalid argument(s): {', '.join(invalid_args)} "
+                    f"encountered for preprocessor function {step}. \n"
+                    f"Valid arguments are: [{', '.join(args)}]"
+                )
 
         # Check for missing arguments
         defaults = argspec.defaults
@@ -249,8 +251,9 @@ def check_preprocessor_settings(settings):
         missing_args = set(args[:end]) - set(settings[step])
         if missing_args:
             raise ValueError(
-                "Missing required argument(s) {} for preprocessor "
-                "function {}".format(missing_args, step))
+                f"Missing required argument(s) {missing_args} for "
+                f"preprocessor function {step}"
+            )
         # Final sanity check in case the above fails to catch a mistake
         try:
             signature = inspect.Signature.from_callable(function)
@@ -276,11 +279,10 @@ def _check_multi_model_settings(products):
                 reference = product
             elif reference.settings[step] != settings:
                 raise ValueError(
-                    "Unable to combine differing multi-dataset settings for "
-                    "{} and {}, {} and {}".format(reference.filename,
-                                                  product.filename,
-                                                  reference.settings[step],
-                                                  settings))
+                    f"Unable to combine differing multi-dataset settings for "
+                    f"{reference.filename} and {product.filename}, "
+                    f"{reference.settings[step]} and {settings}"
+                )
 
 
 def _get_multi_model_settings(products, step):
@@ -416,8 +418,8 @@ class PreprocessorFile(TrackedFile):
         """Apply preprocessor step to product."""
         if step not in self.settings:
             raise ValueError(
-                "PreprocessorFile {} has no settings for step {}".format(
-                    self, step))
+                f"PreprocessorFile {self} has no settings for step {step}"
+            )
         self.cubes = preprocess(self.cubes, step,
                                 input_files=self._input_files,
                                 **self.settings[step])
@@ -463,9 +465,25 @@ class PreprocessorFile(TrackedFile):
     def close(self):
         """Close the file."""
         if self._cubes is not None:
+            self.update_attributes()
             self.save()
             self._cubes = None
             self.save_provenance()
+
+    def update_attributes(self):
+        """Update product attributes from cube metadata."""
+        if not self._cubes:
+            return
+        ref_cube = self._cubes[0]
+        names = {
+            'standard_name': 'standard_name',
+            'long_name': 'long_name',
+            'var_name': 'short_name',
+        }
+        for (name_in, name_out) in names.items():
+            cube_val = getattr(ref_cube, name_in)
+            self.attributes[name_out] = '' if cube_val is None else cube_val
+        self.attributes['units'] = str(ref_cube.units)
 
     @property
     def is_closed(self):
@@ -625,11 +643,10 @@ class PreprocessingTask(BaseTask):
         ]
         products = '\n\n'.join('\n'.join([str(p), pformat(p.settings)])
                                for p in self.products)
-        txt = "{}: {}\norder: {}\n{}\n{}".format(
-            self.__class__.__name__,
-            self.name,
-            order,
-            products,
-            self.print_ancestors(),
+        txt = (
+            f"{self.__class__.__name__}: {self.name}\n"
+            f"order: {order}\n"
+            f"{products}\n"
+            f"{self.print_ancestors()}"
         )
         return txt
---
+++ tests/unit/preprocessor/test_preprocessor_file.py
@@ -0,0 +1,125 @@
+"""Unit tests for :class:`esmvalcore.preprocessor.PreprocessorFile`."""
+
+from unittest import mock
+
+import pytest
+from iris.cube import Cube, CubeList
+
+from esmvalcore.preprocessor import PreprocessorFile
+
+ATTRIBUTES = {
+    'filename': 'file.nc',
+    'standard_name': 'precipitation',
+    'long_name': 'Precipitation',
+    'short_name': 'pr',
+    'units': 'kg m-2 s-1',
+}
+
+
+@pytest.fixture
+def product():
+    """PreprocessorFile object used for testing."""
+    cube = Cube(
+        0,
+        var_name='tas',
+        standard_name='air_temperature',
+        long_name='Near-Surface Air Temperature',
+        units='K',
+    )
+    product = PreprocessorFile(attributes=ATTRIBUTES, settings={})
+    product._cubes = CubeList([cube, cube, cube])
+    return product
+
+
+def test_update_attributes_empty_cubes(product):
+    """Test ``update_attributes``."""
+    product._cubes = CubeList([])
+    product.update_attributes()
+
+    assert not product._cubes
+    assert product.attributes == ATTRIBUTES
+
+
+def test_update_attributes(product):
+    """Test ``update_attributes``."""
+    product.update_attributes()
+
+    assert product.attributes == {
+        'filename': 'file.nc',
+        'standard_name': 'air_temperature',
+        'long_name': 'Near-Surface Air Temperature',
+        'short_name': 'tas',
+        'units': 'K',
+    }
+    assert isinstance(product.attributes['units'], str)
+
+
+def test_update_attributes_empty_standard_name(product):
+    """Test ``update_attributes``."""
+    product._cubes[0].standard_name = None
+    product.update_attributes()
+
+    assert product.attributes == {
+        'filename': 'file.nc',
+        'standard_name': '',
+        'long_name': 'Near-Surface Air Temperature',
+        'short_name': 'tas',
+        'units': 'K',
+    }
+    assert isinstance(product.attributes['units'], str)
+
+
+def test_update_attributes_empty_long_name(product):
+    """Test ``update_attributes``."""
+    product._cubes[0].long_name = None
+    product.update_attributes()
+
+    assert product.attributes == {
+        'filename': 'file.nc',
+        'standard_name': 'air_temperature',
+        'long_name': '',
+        'short_name': 'tas',
+        'units': 'K',
+    }
+    assert isinstance(product.attributes['units'], str)
+
+
+def test_update_attributes_empty_var_name(product):
+    """Test ``update_attributes``."""
+    product._cubes[0].var_name = None
+    product.update_attributes()
+
+    assert product.attributes == {
+        'filename': 'file.nc',
+        'standard_name': 'air_temperature',
+        'long_name': 'Near-Surface Air Temperature',
+        'short_name': '',
+        'units': 'K',
+    }
+    assert isinstance(product.attributes['units'], str)
+
+
+def test_close_no_cubes():
+    """Test ``close``."""
+    product = mock.create_autospec(PreprocessorFile, instance=True)
+    product._cubes = None
+
+    PreprocessorFile.close(product)
+
+    product.update_attributes.assert_not_called()
+    product.save.assert_not_called()
+    product.save_provenance.assert_not_called()
+    assert product._cubes is None
+
+
+def test_close():
+    """Test ``close``."""
+    product = mock.create_autospec(PreprocessorFile, instance=True)
+    product._cubes = CubeList([Cube(0)])
+
+    PreprocessorFile.close(product)
+
+    product.update_attributes.assert_called_once_with()
+    product.save.assert_called_once_with()
+    product.save_provenance.assert_called_once_with()
+    assert product._cubes is None
