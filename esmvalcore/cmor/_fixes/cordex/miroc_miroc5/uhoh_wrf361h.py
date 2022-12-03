c63e76af75720a65ed7c2efd86957bbc06804e7c
sloosvel <saskia.loosveldt@bsc.es>
2022-12-03 23:22:23 +0100

    Add tas fix for UHOH-WRF361H

---
+++ esmvalcore/cmor/_fixes/cordex/miroc_miroc5/uhoh_wrf361h.py
@@ -0,0 +1,17 @@
+"""Fixes for rcm UHOH-WRF361H driven by MIROC-MIROC5."""
+import iris
+from esmvalcore.cmor.fix import Fix
+
+class Tas(Fix):
+    def fix_metadata(self, cubes):
+        fixed_cubes = iris.cube.CubeList()
+        for cube in cubes:
+            height = cube.coord('height')
+            if isinstance(height, iris.coords.DimCoord):
+                iris.util.demote_dim_coord_to_aux_coord(
+                    cube,
+                    height
+                )
+            fixed_cubes.append(iris.util.squeeze(cube))
+        return fixed_cubes
+
