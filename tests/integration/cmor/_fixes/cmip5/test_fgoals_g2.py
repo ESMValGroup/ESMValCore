8044d5bc061b8abb2654c7b0547289bd16f41345
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-12-08 15:01:13 +0000

    fix cmor tests in light of new cf-units

--- tests/integration/cmor/_fixes/cmip5/test_fgoals_g2.py
+++ tests/integration/cmor/_fixes/cmip5/test_fgoals_g2.py
@@ -1,6 +1,5 @@
 """Test FGOALS-g2 fixes."""
-import unittest
-
+import pytest
 from cf_units import Unit
 from iris.coords import DimCoord
 from iris.cube import Cube
@@ -9,47 +8,54 @@ from esmvalcore.cmor._fixes.cmip5.fgoals_g2 import AllVars
 from esmvalcore.cmor.fix import Fix
 
 
-class TestAll(unittest.TestCase):
+@pytest.fixture
+def cube():
+    """Cube for testing."""
+    test_cube = Cube([[1.0, 2.0]], var_name='co2', units='J')
+    test_cube.add_dim_coord(
+        DimCoord(
+            [0.0, 1.0],
+            standard_name='time',
+            units=Unit('days since 0001-01', calendar='gregorian')),
+        1)
+    test_cube.add_dim_coord(
+        DimCoord(
+            [180],
+            standard_name='longitude',
+            units=Unit('degrees')),
+        0)
+    return test_cube
+
+
+class TestAll:
     """Test fixes for all vars."""
 
-    def setUp(self):
-        """Prepare tests."""
-        self.cube = Cube([[1.0, 2.0]], var_name='co2', units='J')
-        self.cube.add_dim_coord(
-            DimCoord(
-                [0.0, 1.0],
-                standard_name='time',
-                units=Unit('days since 0001-01', calendar='gregorian')),
-            1)
-        self.cube.add_dim_coord(
-            DimCoord(
-                [180],
-                standard_name='longitude',
-                units=Unit('degrees')),
-            0)
-        self.fix = AllVars(None)
-
-    def test_get(self):
+    @staticmethod
+    def test_get():
         """Test fix get."""
-        self.assertListEqual(
-            Fix.get_fixes('CMIP5', 'FGOALS-G2', 'Amon', 'tas'),
-            [AllVars(None)])
+        assert (Fix.get_fixes('CMIP5', 'FGOALS-G2', 'Amon', 'tas')
+                == [AllVars(None)])
 
-    def test_fix_metadata(self):
+    @staticmethod
+    def test_fix_metadata(cube):
         """Test calendar fix."""
-        cube = self.fix.fix_metadata([self.cube])[0]
+        fix = AllVars(None)
+        cube = fix.fix_metadata([cube])[0]
 
         time = cube.coord('time')
-        self.assertEqual(time.units.origin,
-                         'day since 1-01-01 00:00:00.000000')
-        self.assertEqual(time.units.calendar, 'gregorian')
+        assert time.units.origin == 'day since 1-01-01 00:00:00.000000'
+        assert time.units.calendar in ('standard', 'gregorian')
 
-    def test_fix_metadata_dont_fail_if_not_longitude(self):
+    @staticmethod
+    def test_fix_metadata_dont_fail_if_not_longitude(cube):
         """Test calendar fix."""
-        self.cube.remove_coord('longitude')
-        self.fix.fix_metadata([self.cube])
+        cube.remove_coord('longitude')
+        fix = AllVars(None)
+        fix.fix_metadata([cube])
 
-    def test_fix_metadata_dont_fail_if_not_time(self):
+    @staticmethod
+    def test_fix_metadata_dont_fail_if_not_time(cube):
         """Test calendar fix."""
-        self.cube.remove_coord('time')
-        self.fix.fix_metadata([self.cube])
+        cube.remove_coord('time')
+        fix = AllVars(None)
+        fix.fix_metadata([cube])
