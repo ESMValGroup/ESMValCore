4623207038cf5b7c884513697c86fb2d6cbb7cc7
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-04-13 15:42:39 +0200

    Guess location

--- doc/conf.py
+++ doc/conf.py
@@ -31,11 +31,10 @@ from esmvalcore import __version__
 # docs.readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if on_rtd:
-    os.environ["ESMFMKFILE"] = f"{os.environ['CONDA_PREFIX']}/lib/esmf.mk"
-
 # This is used for linking and such so we link to the thing we're building
 rtd_version = os.environ.get("READTHEDOCS_VERSION", "latest")
+if on_rtd:
+    os.environ["ESMFMKFILE"] = f"/home/docs/checkouts/readthedocs.org/user_builds/esmvalcore/conda/{rtd_version}/lib/esmf.mk"
 if rtd_version not in ["latest", "stable", "doc"]:
     rtd_version = "latest"
 
