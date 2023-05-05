73e3b6e3d287e33abd643e70f7848f559cadcc9f
jcos <josep.cos@bsc.es>
2023-05-05 16:19:10 +0200

    nesm3 calendar fix

--- esmvalcore/cmor/_fixes/cmip6/nesm3.py
+++ esmvalcore/cmor/_fixes/cmip6/nesm3.py
@@ -1,5 +1,6 @@
 """Fixes for NESM3 model."""
 from ..common import ClFixHybridPressureCoord
+from ..fix import Fix
 
 
 Cl = ClFixHybridPressureCoord
@@ -9,3 +10,31 @@ Cli = ClFixHybridPressureCoord
 
 
 Clw = ClFixHybridPressureCoord
+
+
+class AllVars(Fix):
+    """Fixes for all vars."""
+
+    def fix_metadata(self, cubes):
+        """Fix parent time units.
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        parent_units = 'parent_time_units'
+        bad_units = 'days since 2015-01-01'
+        branch_parent = 'branch_time_in_parent'
+        bad_value = 1000000
+        for cube in cubes:
+            try:
+                if bad_units in cube.attributes[parent_units] and cube.attributes[branch_parent] > bad_value:
+                    cube.attributes[branch_parent] = 0
+            except AttributeError:
+                pass
+        return cubes
