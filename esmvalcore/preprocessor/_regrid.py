Fix issue with masking and update tests to accept Dask arrays

esmvalcore/preprocessor/_regrid.py
@@ -796,7 +796,7 @@ def _vertical_interpolate(cube, src_levels, levels, interpolation,
                                     extrapolation=extrapolation)
 
     # Calculate the mask based on the any NaN values in the interpolated data.
-    new_data = da.ma.masked_equal(new_data, np.nan)
+    new_data = da.ma.masked_where(da.isnan(new_data), new_data)
 
     # Construct the resulting cube with the interpolated data.
     return _create_cube(cube, new_data, src_levels, levels.astype(float))

tests/unit/preprocessor/_regrid/test_extract_levels.py
@@ -264,7 +264,7 @@ class Test(tests.Test):
         new_data_mask = np.zeros(new_data.shape, bool)
         new_data_mask[new_data == _MDI] = True
         new_data = np.ma.array(new_data, mask=new_data_mask)
-        self.assert_array_equal(args[1], new_data)
+        self.assert_array_equal(args[1].compute(), new_data)
         self.assert_array_equal(args[2],
                                 self.cube.coord(axis='z', dim_coords=True))
         self.assert_array_equal(args[3], levels)
@@ -318,9 +318,10 @@ class Test(tests.Test):
         new_data_mask = np.zeros(new_data.shape, bool)
         new_data_mask[new_data == _MDI] = True
         new_data = np.ma.array(new_data, mask=new_data_mask)
-        self.assert_array_equal(args[1], new_data)
-        self.assertTrue(ma.isMaskedArray(args[1]))
-        self.assert_array_equal(args[1].mask, new_data_mask)
+        arg1 = args[1].compute()
+        self.assert_array_equal(arg1, new_data)
+        self.assertTrue(ma.isMaskedArray(arg1))
+        self.assert_array_equal(arg1.mask, new_data_mask)
         self.assert_array_equal(args[2],
                                 self.cube.coord(axis='z', dim_coords=True))
         self.assert_array_equal(args[3], levels)