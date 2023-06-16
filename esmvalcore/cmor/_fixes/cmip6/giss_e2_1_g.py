c1703ec4610b6f4910cbf0b3754a0b0911f76ba7
Bouwe Andela <b.andela@esciencecenter.nl>
2023-06-16 18:50:27 +0200

    Add a fix for GISS-E2-1-G tos

--- esmvalcore/cmor/_fixes/cmip6/giss_e2_1_g.py
+++ esmvalcore/cmor/_fixes/cmip6/giss_e2_1_g.py
@@ -1,11 +1,19 @@
 """Fixes for GISS-E2-1-G model."""
 from ..common import ClFixHybridPressureCoord
-
+from ..fix import Fix
 
 Cl = ClFixHybridPressureCoord
 
-
 Cli = ClFixHybridPressureCoord
 
-
 Clw = ClFixHybridPressureCoord
+
+
+class Tos(Fix):
+
+    def fix_metadata(self, cubes):
+        for cube in cubes:
+            if cube.core_data().ravel()[:1000].max() > 100.:
+                cube.units = 'K'
+                cube.convert_units(self.vardef.units)
+        return cubes
