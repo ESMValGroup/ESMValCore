85a1892fc484d476ab4f0aaac9ef201a478bf601
Bouwe Andela <b.andela@esciencecenter.nl>
2023-06-16 16:59:05 +0200

    Fix typo

--- tests/unit/test_citation.py
+++ tests/unit/test_citation.py
@@ -2,6 +2,6 @@ from esmvalcore import _citation
 
 
 def test_extract_tags():
-    tags = "['example1', 'example_2', 'example-3]"
+    tags = "['example1', 'example_2', 'example-3']"
     result = _citation._extract_tags(tags)
     assert result == {'example1', 'example_2', 'example-3'}
