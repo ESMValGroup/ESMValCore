0bb1101842782ad1fcd51e38b1ebad059b5d6c54
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-08-09 14:24:21 +0100

    add time dim to test cube

--- tests/integration/preprocessor/_extract_region/test_intersect.py
+++ tests/integration/preprocessor/_extract_region/test_intersect.py
@@ -6,6 +6,7 @@ https://github.com/SciTools/iris/issues/5413 .
 """
 import iris
 import numpy as np
+from cf_units import Unit
 from iris.fileformats.pp import EARTH_RADIUS
 
 from esmvalcore.preprocessor._area import extract_region
@@ -15,7 +16,12 @@ from esmvalcore.preprocessor._shared import guess_bounds
 def make_cube():
     """Make a realistic cube with cell measure and ancil var."""
     coord_sys = iris.coord_systems.GeogCS(EARTH_RADIUS)
-    data = np.ones((192, 288), dtype=np.float32)
+    data = np.ones((10, 192, 288), dtype=np.float32)
+    time = iris.coords.DimCoord(
+        np.arange(0., 10., 1.),
+        standard_name='time',
+        units=Unit('days since 1950-01-01 00:00:00',
+        calendar='360_day'))
     lons = iris.coords.DimCoord(
         [i + .5 for i in range(288)],
         standard_name='longitude',
@@ -29,7 +35,7 @@ def make_cube():
         units='degrees_north',
         coord_system=coord_sys,
     )
-    coords_spec = [(lats, 0), (lons, 1)]
+    coords_spec = [(time, 0), (lats, 1), (lons, 2)]
     simple_cube = iris.cube.Cube(data, dim_coords_and_dims=coords_spec)
 
     # add a cell measure
@@ -48,7 +54,8 @@ def make_cube():
         standard_name='land_ice_area_fraction',
         var_name='sftgif',
         units='%')
-    simple_cube.add_ancillary_variable(ancillary_var, (0, 1))
+    simple_cube.add_ancillary_variable(ancillary_var,
+                                       range(0, simple_cube.ndim))
 
     return simple_cube
 
@@ -79,11 +86,13 @@ def test_extract_region_cell_ancil():
     assert result_cm
     assert expected_cm.measure == result_cm[0].measure
     assert expected_cm.var_name == result_cm[0].var_name
+    np.testing.assert_array_equal(result_cm[0].shape, (10, 60, 58))
     assert expected_cm.standard_name == result_cm[0].standard_name
     expected_ancil = cube.ancillary_variables()[0]
     result_ancil = ex1.ancillary_variables()
     assert result_ancil
     assert expected_ancil.var_name == result_ancil[0].var_name
     assert expected_ancil.standard_name == result_ancil[0].standard_name
+    np.testing.assert_array_equal(result_ancil[0].shape, (10, 60, 58))
     assert len(ex2.cell_measures()) == 1
     assert len(ex2.ancillary_variables()) == 1
