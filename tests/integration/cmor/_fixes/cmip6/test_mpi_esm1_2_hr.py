f42320cb3f8c020483dbecf699ad2cf05d081cc2
Karen Garcia Perdomo <karen.garciaperdomo@ec.gc.ca>
2023-12-12 21:19:48 +0000

    changed_flake_8_issues

--- tests/integration/cmor/_fixes/cmip6/test_mpi_esm1_2_hr.py
+++ tests/integration/cmor/_fixes/cmip6/test_mpi_esm1_2_hr.py
@@ -2,10 +2,11 @@
 import iris
 import pytest
 
-from esmvalcore.cmor._fixes.cmip6.mpi_esm1_2_hr import AllVars, Cl, Cli, Clw, SfcWind, Tas
+from esmvalcore.cmor._fixes.cmip6.mpi_esm1_2_hr import AllVars, Cl, Cli, Clw
+from esmvalcore.cmor._fixes.cmip6.mpi_esm1_2_hr import SfcWind, Tas
 from esmvalcore.cmor._fixes.common import ClFixHybridPressureCoord
-from esmvalcore.cmor._fixes.fix import Fix, GenericFix
-from esmvalcore.cmor.table import get_var_info 
+from esmvalcore.cmor._fixes.fix import Fix
+from esmvalcore.cmor.table import get_var_info
 from cf_units import Unit
 
 
@@ -47,53 +48,53 @@ def test_allvars_r2i1p1f1():
 
 @pytest.fixture
 def sfcwind_cubes():
-    correct_lat_coord = iris.coords.DimCoord([0.0], 
-                                             var_name='lat', 
+    correct_lat_coord = iris.coords.DimCoord([0.0],
+                                             var_name='lat',
                                              standard_name='latitude')
-    wrong_lat_coord = iris.coords.DimCoord([0.0], 
-                                           var_name ='latitudeCoord', 
+    wrong_lat_coord = iris.coords.DimCoord([0.0],
+                                           var_name='latitudeCoord',
                                            standard_name='latitude')
     correct_lon_coord = iris.coords.DimCoord([0.0],
-                                             var_name ='lon', 
+                                             var_name='lon',
                                              standard_name='longitude')
     wrong_lon_coord = iris.coords.DimCoord([0.0],
-                                           var_name ='longitudeCoord', 
+                                           var_name='longitudeCoord',
                                            standard_name='longitude')
-    correct_cube = iris.cube.Cube([[10.0]], 
-                                  var_name = 'sfcWind', 
-                                  dim_coords_and_dims=[(correct_lat_coord, 0), (correct_lon_coord,1)]
+    correct_cube = iris.cube.Cube([[10.0]], var_name='sfcWind',
+                                  dim_coords_and_dims=[(correct_lat_coord, 0),
+                                                       (correct_lon_coord, 1)]
                                   )
-    wrong_cube =  iris.cube.Cube([[10.0]], 
-                                 var_name = 'ta', 
-                                 dim_coords_and_dims=[(wrong_lat_coord, 0), (wrong_lon_coord,1)], 
-                                 attributes= {'parent_time_units': 'days since 1850-1-1 00:00:00'}
-                                 )
+    wrong_cube = iris.cube.Cube([[10.0]],
+                                var_name='ta',
+                                dim_coords_and_dims=[(wrong_lat_coord, 0),
+                                                     (wrong_lon_coord, 1)])
     scalar_cube = iris.cube.Cube(0.0, var_name='ps')
     return iris.cube.CubeList([correct_cube, wrong_cube, scalar_cube])
 
 
 @pytest.fixture
 def tas_cubes():
-    correct_lat_coord = iris.coords.DimCoord([0.0], 
-                                             var_name = 'lat', 
+    correct_lat_coord = iris.coords.DimCoord([0.0],
+                                             var_name='lat',
                                              standard_name='latitude')
-    wrong_lat_coord = iris.coords.DimCoord([0.0], 
-                                           var_name ='latitudeCoord', 
+    wrong_lat_coord = iris.coords.DimCoord([0.0],
+                                           var_name='latitudeCoord',
                                            standard_name='latitude')
-    correct_lon_coord = iris.coords.DimCoord([0.0], 
-                                             var_name = 'lon', 
+    correct_lon_coord = iris.coords.DimCoord([0.0],
+                                             var_name='lon',
                                              standard_name='longitude')
-    wrong_lon_coord = iris.coords.DimCoord([0.0], 
-                                           var_name ='longitudeCoord', 
+    wrong_lon_coord = iris.coords.DimCoord([0.0],
+                                           var_name='longitudeCoord',
                                            standard_name='longitude')
-    correct_cube = iris.cube.Cube([[10.0]], 
-                                  var_name = 'tas', 
-                                  dim_coords_and_dims=[(correct_lat_coord, 0), (correct_lon_coord,1)]
-                        )
-    wrong_cube =  iris.cube.Cube([[10.0]], 
-                                 var_name = 'ta', 
-                                 dim_coords_and_dims=[(wrong_lat_coord, 0), (wrong_lon_coord,1)])
-    scalar_cube = iris.cube.Cube(0.0, var_name='ps') #why is called ps 
+    correct_cube = iris.cube.Cube([[10.0]],
+                                  var_name='tas',
+                                  dim_coords_and_dims=[(correct_lat_coord, 0),
+                                                       (correct_lon_coord, 1)])
+    wrong_cube = iris.cube.Cube([[10.0]],
+                                var_name='ta',
+                                dim_coords_and_dims=[(wrong_lat_coord, 0),
+                                                     (wrong_lon_coord, 1)])
+    scalar_cube = iris.cube.Cube(0.0, var_name='ps')
     return iris.cube.CubeList([correct_cube, wrong_cube, scalar_cube])
 
 
@@ -131,8 +132,8 @@ def test_clw_fix():
 
 
 def test_get_sfcwind_fix():
-	fixes = Fix.get_fixes('CMIP6', 'MPI_ESM1_2_HR', 'day', 'sfcWind')
-	assert SfcWind(None) in fixes
+    fixes = Fix.get_fixes('CMIP6', 'MPI_ESM1_2_HR', 'day', 'sfcWind')
+    assert SfcWind(None) in fixes
 
 
 def test_sfcwind_fix_metadata(sfcwind_cubes):
@@ -140,10 +141,10 @@ def test_sfcwind_fix_metadata(sfcwind_cubes):
         with pytest.raises(iris.exceptions.CoordinateNotFoundError):
             cube.coord('height')
     height_coord = iris.coords.AuxCoord(10.0,
-                                        var_name = 'height',   
-                                        standard_name = 'height', 
-                                        long_name = 'height',
-                                        units = Unit('m'),
+                                        var_name='height',
+                                        standard_name='height',
+                                        long_name='height',
+                                        units=Unit('m'),
                                         attributes={'positive': 'up'})
     vardef = get_var_info('CMIP6', 'day', 'sfcWind')
     fix = SfcWind(vardef)
@@ -155,8 +156,8 @@ def test_sfcwind_fix_metadata(sfcwind_cubes):
 
 
 def test_get_tas_fix():
-	fixes = Fix.get_fixes('CMIP6', 'MPI_ESM1_2_HR', 'day', 'tas')
-	assert Tas(None) in fixes
+    fixes = Fix.get_fixes('CMIP6', 'MPI_ESM1_2_HR', 'day', 'tas')
+    assert Tas(None) in fixes
 
 
 def test_tas_fix_metadata(tas_cubes):
@@ -164,10 +165,10 @@ def test_tas_fix_metadata(tas_cubes):
         with pytest.raises(iris.exceptions.CoordinateNotFoundError):
             cube.coord('height')
     height_coord = iris.coords.AuxCoord(2.0,
-                                        var_name = 'height',   
-                                        standard_name = 'height', 
-                                        long_name = 'height',
-                                        units = Unit('m'),
+                                        var_name='height',
+                                        standard_name='height',
+                                        long_name='height',
+                                        units=Unit('m'),
                                         attributes={'positive': 'up'})
     vardef = get_var_info('CMIP6', 'day', 'tas')
     fix = Tas(vardef)
