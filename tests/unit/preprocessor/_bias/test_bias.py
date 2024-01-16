2d0a3f5a4d464ae54c0864b1fa218e6941bd15f1
Manuel Schlund <manuel.schlund@dlr.de>
2024-01-16 14:29:58 +0100

    Added and optimized tests

--- tests/unit/preprocessor/_bias/test_bias.py
+++ tests/unit/preprocessor/_bias/test_bias.py
@@ -66,45 +66,15 @@ def ref_cubes():
     return CubeList([cube])
 
 
-def test_no_reference_for_bias(regular_cubes, ref_cubes):
-    """Test fail when no reference_for_bias is given."""
-    products = {
-        PreprocessorFile(regular_cubes, 'A', {}),
-        PreprocessorFile(regular_cubes, 'B', {}),
-        PreprocessorFile(ref_cubes, 'REF', {}),
-    }
-    msg = "Expected exactly 1 dataset with 'reference_for_bias: true', found 0"
-    with pytest.raises(ValueError, match=msg):
-        bias(products)
+TEST_BIAS = [
+    ('absolute', [[[-2.0, -1.0], [0.0, 1.0]], [[2.0, 3.0], [4.0, 3.0]]], 'K'),
+    ('relative', [[[-1.0, -0.5], [0.0, 0.5]], [[1.0, 1.5], [2.0, 0.75]]], '1'),
+]
 
 
-def test_two_reference_for_bias(regular_cubes, ref_cubes):
-    """Test fail when two reference_for_bias is given."""
-    products = {
-        PreprocessorFile(regular_cubes, 'A', {'reference_for_bias': False}),
-        PreprocessorFile(ref_cubes, 'REF1', {'reference_for_bias': True}),
-        PreprocessorFile(ref_cubes, 'REF2', {'reference_for_bias': True}),
-    }
-    msg = "Expected exactly 1 dataset with 'reference_for_bias: true', found 2"
-    with pytest.raises(ValueError, match=msg):
-        bias(products)
-
-
-def test_invalid_bias_type(regular_cubes, ref_cubes):
-    """Test fail when invalid bias_type is given."""
-    products = {
-        PreprocessorFile(regular_cubes, 'A', {}),
-        PreprocessorFile(regular_cubes, 'B', {}),
-        PreprocessorFile(ref_cubes, 'REF', {'reference_for_bias': True}),
-    }
-    msg = (r"Expected one of \['absolute', 'relative'\] for bias_type, got "
-           r"'invalid_bias_type'")
-    with pytest.raises(ValueError, match=msg):
-        bias(products, bias_type='invalid_bias_type')
-
-
-def test_absolute_bias(regular_cubes, ref_cubes):
-    """Test calculation of absolute bias."""
+@pytest.mark.parametrize('bias_type,data,units', TEST_BIAS)
+def test_bias_products(regular_cubes, ref_cubes, bias_type, data, units):
+    """Test calculation of bias with products."""
     ref_product = PreprocessorFile(ref_cubes, 'REF',
                                    {'reference_for_bias': True})
     products = {
@@ -112,7 +82,7 @@ def test_absolute_bias(regular_cubes, ref_cubes):
         PreprocessorFile(regular_cubes, 'B', {'dataset': 'b'}),
         ref_product,
     }
-    out_products = bias(products)
+    out_products = bias(products, bias_type=bias_type)
 
     assert isinstance(out_products, set)
     out_dict = products_set_to_dict(out_products)
@@ -120,17 +90,13 @@ def test_absolute_bias(regular_cubes, ref_cubes):
 
     product_a = out_dict['A']
     assert product_a.filename == 'A'
-    assert product_a.attributes == {'units': 'K', 'dataset': 'a'}
+    assert product_a.attributes == {'units': units, 'dataset': 'a'}
     assert len(product_a.cubes) == 1
     out_cube = product_a.cubes[0]
-    expected_data = [[[-2.0, -1.0],
-                      [0.0, 1.0]],
-                     [[2.0, 3.0],
-                      [4.0, 3.0]]]
-    assert_array_equal(out_cube.data, expected_data)
+    assert_array_equal(out_cube.data, data)
     assert out_cube.var_name == 'tas'
     assert out_cube.standard_name == 'air_temperature'
-    assert out_cube.units == 'K'
+    assert out_cube.units == units
     assert out_cube.dim_coords == regular_cubes[0].dim_coords
     assert out_cube.aux_coords == regular_cubes[0].aux_coords
     product_a.wasderivedfrom.assert_called_once()
@@ -138,77 +104,65 @@ def test_absolute_bias(regular_cubes, ref_cubes):
 
     product_b = out_dict['B']
     assert product_b.filename == 'B'
-    assert product_b.attributes == {'units': 'K', 'dataset': 'b'}
+    assert product_b.attributes == {'units': units, 'dataset': 'b'}
     assert len(product_b.cubes) == 1
     out_cube = product_b.cubes[0]
-    expected_data = [[[-2.0, -1.0],
-                      [0.0, 1.0]],
-                     [[2.0, 3.0],
-                      [4.0, 3.0]]]
-    assert_array_equal(out_cube.data, expected_data)
+    assert_array_equal(out_cube.data, data)
     assert out_cube.var_name == 'tas'
     assert out_cube.standard_name == 'air_temperature'
-    assert out_cube.units == 'K'
+    assert out_cube.units == units
     assert out_cube.dim_coords == regular_cubes[0].dim_coords
     assert out_cube.aux_coords == regular_cubes[0].aux_coords
     product_b.wasderivedfrom.assert_called_once()
     assert product_b.mock_ancestors == {ref_product}
 
 
-def test_relative_bias(regular_cubes, ref_cubes):
-    """Test calculation of relative bias."""
-    ref_product = PreprocessorFile(ref_cubes, 'REF',
-                                   {'reference_for_bias': True})
-    products = {
-        PreprocessorFile(regular_cubes, 'A', {'dataset': 'a'}),
-        PreprocessorFile(regular_cubes, 'B', {'dataset': 'b'}),
-        ref_product,
-    }
-    out_products = bias(products, bias_type='relative')
+@pytest.mark.parametrize('bias_type,data,units', TEST_BIAS)
+def test_bias_cubes(regular_cubes, ref_cubes, bias_type, data, units):
+    """Test calculation of bias with cubes."""
+    ref_cube = ref_cubes[0]
+    out_cubes = bias(regular_cubes, ref_cube, bias_type=bias_type)
 
-    assert isinstance(out_products, set)
-    out_dict = products_set_to_dict(out_products)
-    assert len(out_dict) == 2
+    assert isinstance(out_cubes, CubeList)
+    assert len(out_cubes) == 1
+    out_cube = out_cubes[0]
 
-    product_a = out_dict['A']
-    assert product_a.filename == 'A'
-    assert product_a.attributes == {'units': '1', 'dataset': 'a'}
-    assert len(product_a.cubes) == 1
-    out_cube = product_a.cubes[0]
-    expected_data = [[[-1.0, -0.5],
-                      [0.0, 0.5]],
-                     [[1.0, 1.5],
-                      [2.0, 0.75]]]
-    assert_array_equal(out_cube.data, expected_data)
+    assert_array_equal(out_cube.data, data)
     assert out_cube.var_name == 'tas'
     assert out_cube.standard_name == 'air_temperature'
-    assert out_cube.units == '1'
+    assert out_cube.units == units
     assert out_cube.dim_coords == regular_cubes[0].dim_coords
     assert out_cube.aux_coords == regular_cubes[0].aux_coords
-    product_a.wasderivedfrom.assert_called_once()
-    assert product_a.mock_ancestors == {ref_product}
 
-    product_b = out_dict['B']
-    assert product_b.filename == 'B'
-    assert product_b.attributes == {'units': '1', 'dataset': 'b'}
-    assert len(product_b.cubes) == 1
-    out_cube = product_b.cubes[0]
-    expected_data = [[[-1.0, -0.5],
-                      [0.0, 0.5]],
-                     [[1.0, 1.5],
-                      [2.0, 0.75]]]
-    assert_array_equal(out_cube.data, expected_data)
+
+TEST_BIAS_BROADCASTABLE = [
+    ('absolute', [[[-2.0, -1.0], [0.0, 1.0]], [[2.0, 3.0], [4.0, 5.0]]], 'K'),
+    ('relative', [[[-1.0, -0.5], [0.0, 0.5]], [[1.0, 1.5], [2.0, 2.5]]], '1'),
+]
+
+
+@pytest.mark.parametrize('bias_type,data,units', TEST_BIAS_BROADCASTABLE)
+def test_bias_cubes_broadcastable(
+    regular_cubes, ref_cubes, bias_type, data, units
+):
+    """Test calculation of bias with cubes."""
+    ref_cube = ref_cubes[0][0]  # only select one time step
+    out_cubes = bias(regular_cubes, ref_cube, bias_type=bias_type)
+
+    assert isinstance(out_cubes, CubeList)
+    assert len(out_cubes) == 1
+    out_cube = out_cubes[0]
+
+    assert_array_equal(out_cube.data, data)
     assert out_cube.var_name == 'tas'
     assert out_cube.standard_name == 'air_temperature'
-    assert out_cube.units == '1'
+    assert out_cube.units == units
     assert out_cube.dim_coords == regular_cubes[0].dim_coords
     assert out_cube.aux_coords == regular_cubes[0].aux_coords
-    product_b.wasderivedfrom.assert_called_once()
-    assert product_b.mock_ancestors == {ref_product}
 
 
-def test_denominator_mask_threshold(regular_cubes, ref_cubes):
-    """Test denominator_mask_threshold argument."""
+def test_denominator_mask_threshold_products(regular_cubes, ref_cubes):
+    """Test denominator_mask_threshold argument with products."""
     ref_product = PreprocessorFile(ref_cubes, 'REF',
                                    {'reference_for_bias': True})
     products = {
@@ -242,81 +196,6 @@ def test_denominator_mask_threshold(regular_cubes, ref_cubes):
     assert product_a.mock_ancestors == {ref_product}
 
 
-def test_keep_reference_dataset(regular_cubes, ref_cubes):
-    """Test denominator_mask_threshold argument."""
-    products = {
-        PreprocessorFile(regular_cubes, 'A', {'dataset': 'a'}),
-        PreprocessorFile(ref_cubes, 'REF', {'reference_for_bias': True})
-    }
-    out_products = bias(products, keep_reference_dataset=True)
-
-    assert isinstance(out_products, set)
-    out_dict = products_set_to_dict(out_products)
-    assert len(out_dict) == 2
-
-    product_ref = out_dict['REF']
-    assert product_ref.filename == 'REF'
-    assert product_ref.attributes == {'reference_for_bias': True}
-    assert len(product_ref.cubes) == 1
-    out_cube = product_ref.cubes[0]
-    expected_data = [[[2.0, 2.0],
-                      [2.0, 2.0]],
-                     [[2.0, 2.0],
-                      [2.0, 4.0]]]
-    assert_array_equal(out_cube.data, expected_data)
-    assert out_cube.var_name == 'tas'
-    assert out_cube.standard_name == 'air_temperature'
-    assert out_cube.units == 'K'
-    assert out_cube.dim_coords == ref_cubes[0].dim_coords
-    assert out_cube.aux_coords == ref_cubes[0].aux_coords
-
-
-def test_absolute_bias_cubes(regular_cubes, ref_cubes):
-    """Test calculation of absolute bias with cubes."""
-    ref_cube = ref_cubes[0]
-    out_cubes = bias(regular_cubes, ref_cube)
-
-    assert isinstance(out_cubes, CubeList)
-    assert len(out_cubes) == 1
-    out_cube = out_cubes[0]
-
-    expected_data = [[[-2.0, -1.0],
-                      [0.0, 1.0]],
-                     [[2.0, 3.0],
-                      [4.0, 3.0]]]
-    assert_array_equal(out_cube.data, expected_data)
-    assert out_cube.var_name == 'tas'
-    assert out_cube.standard_name == 'air_temperature'
-    assert out_cube.units == 'K'
-    assert out_cube.dim_coords == regular_cubes[0].dim_coords
-    assert out_cube.aux_coords == regular_cubes[0].aux_coords
-
-
-def test_relative_bias_cubes(regular_cubes, ref_cubes):
-    """Test calculation of relative bias with cubes."""
-    ref_cube = ref_cubes[0]
-    out_cubes = bias(
-        [regular_cubes[0], regular_cubes[0]],
-        ref_cube,
-        bias_type='relative',
-    )
-
-    assert isinstance(out_cubes, CubeList)
-    assert len(out_cubes) == 2
-
-    for out_cube in out_cubes:
-        expected_data = [[[-1.0, -0.5],
-                          [0.0, 0.5]],
-                         [[1.0, 1.5],
-                          [2.0, 0.75]]]
-        assert_array_equal(out_cube.data, expected_data)
-        assert out_cube.var_name == 'tas'
-        assert out_cube.standard_name == 'air_temperature'
-        assert out_cube.units == '1'
-        assert out_cube.dim_coords == regular_cubes[0].dim_coords
-        assert out_cube.aux_coords == regular_cubes[0].aux_coords
-
-
 def test_denominator_mask_threshold_cubes(regular_cubes, ref_cubes):
     """Test denominator_mask_threshold argument with cubes."""
     ref_cube = ref_cubes[0]
@@ -343,7 +222,109 @@ def test_denominator_mask_threshold_cubes(regular_cubes, ref_cubes):
     assert out_cube.aux_coords == regular_cubes[0].aux_coords
 
 
+@pytest.mark.parametrize('bias_type', ['absolute', 'relative'])
+def test_keep_reference_dataset(regular_cubes, ref_cubes, bias_type):
+    """Test denominator_mask_threshold argument."""
+    products = {
+        PreprocessorFile(regular_cubes, 'A', {'dataset': 'a'}),
+        PreprocessorFile(ref_cubes, 'REF', {'reference_for_bias': True})
+    }
+    out_products = bias(
+        products, bias_type=bias_type, keep_reference_dataset=True
+    )
+
+    assert isinstance(out_products, set)
+    out_dict = products_set_to_dict(out_products)
+    assert len(out_dict) == 2
+
+    product_ref = out_dict['REF']
+    assert product_ref.filename == 'REF'
+    assert product_ref.attributes == {'reference_for_bias': True}
+    assert len(product_ref.cubes) == 1
+    out_cube = product_ref.cubes[0]
+    expected_data = [[[2.0, 2.0], [2.0, 2.0]], [[2.0, 2.0], [2.0, 4.0]]]
+    assert_array_equal(out_cube.data, expected_data)
+    assert out_cube.var_name == 'tas'
+    assert out_cube.standard_name == 'air_temperature'
+    assert out_cube.units == 'K'
+    assert out_cube.dim_coords == ref_cubes[0].dim_coords
+    assert out_cube.aux_coords == ref_cubes[0].aux_coords
+
+
+@pytest.mark.parametrize('bias_type,data,units', TEST_BIAS)
+@pytest.mark.parametrize('keep_ref', [True, False])
+def test_bias_products_and_ref_cube(
+    regular_cubes, ref_cubes, keep_ref, bias_type, data, units
+):
+    """Test calculation of bias with products and ref_cube given."""
+    ref_cube = ref_cubes[0]
+    products = set([PreprocessorFile(regular_cubes, 'A', {'dataset': 'a'})])
+
+    out_products = bias(
+        products,
+        ref_cube=ref_cube,
+        bias_type=bias_type,
+        keep_reference_dataset=keep_ref,
+    )
+
+    assert isinstance(out_products, set)
+    out_dict = products_set_to_dict(out_products)
+    assert len(out_dict) == 1
+
+    product_a = out_dict['A']
+    assert product_a.filename == 'A'
+    assert product_a.attributes == {'units': units, 'dataset': 'a'}
+    assert len(product_a.cubes) == 1
+    out_cube = product_a.cubes[0]
+    assert_array_equal(out_cube.data, data)
+    assert out_cube.var_name == 'tas'
+    assert out_cube.standard_name == 'air_temperature'
+    assert out_cube.units == units
+    assert out_cube.dim_coords == regular_cubes[0].dim_coords
+    assert out_cube.aux_coords == regular_cubes[0].aux_coords
+    product_a.wasderivedfrom.assert_not_called()
+    assert product_a.mock_ancestors == set()
+
+
+def test_no_reference_for_bias(regular_cubes, ref_cubes):
+    """Test fail when no reference_for_bias is given."""
+    products = {
+        PreprocessorFile(regular_cubes, 'A', {}),
+        PreprocessorFile(regular_cubes, 'B', {}),
+        PreprocessorFile(ref_cubes, 'REF', {}),
+    }
+    msg = "Expected exactly 1 dataset with 'reference_for_bias: true', found 0"
+    with pytest.raises(ValueError, match=msg):
+        bias(products)
+
+
+def test_two_references_for_bias(regular_cubes, ref_cubes):
+    """Test fail when two reference_for_bias products is given."""
+    products = {
+        PreprocessorFile(regular_cubes, 'A', {'reference_for_bias': False}),
+        PreprocessorFile(ref_cubes, 'REF1', {'reference_for_bias': True}),
+        PreprocessorFile(ref_cubes, 'REF2', {'reference_for_bias': True}),
+    }
+    msg = "Expected exactly 1 dataset with 'reference_for_bias: true', found 2"
+    with pytest.raises(ValueError, match=msg):
+        bias(products)
+
+
+def test_invalid_bias_type(regular_cubes, ref_cubes):
+    """Test fail when invalid bias_type is given."""
+    products = {
+        PreprocessorFile(regular_cubes, 'A', {}),
+        PreprocessorFile(regular_cubes, 'B', {}),
+        PreprocessorFile(ref_cubes, 'REF', {'reference_for_bias': True}),
+    }
+    msg = (r"Expected one of \['absolute', 'relative'\] for bias_type, got "
+           r"'invalid_bias_type'")
+    with pytest.raises(ValueError, match=msg):
+        bias(products, bias_type='invalid_bias_type')
+
+
 def test_ref_cube_non_cubes(regular_cubes):
     """Test ref_cube=None with with cubes."""
-    with pytest.raises(ValueError):
+    msg = "`ref_cube` cannot be `None` when `products` is an iterable of Cubes"
+    with pytest.raises(ValueError, match=msg):
         bias(regular_cubes)
