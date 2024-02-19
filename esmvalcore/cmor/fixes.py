dadc1a410f91b84e97842600e59d81f2e3d13c0a
Franziska Winterstein <franziska.winterstein@dlr.de>
2024-02-19 15:50:01 +0100

    added public version of add_model_level

--- esmvalcore/cmor/fixes.py
+++ esmvalcore/cmor/fixes.py
@@ -3,6 +3,7 @@
 from ._fixes.shared import (
     add_altitude_from_plev,
     add_plev_from_altitude,
+    add_model_level,
     get_next_month,
     get_time_bounds,
 )
@@ -10,6 +11,7 @@ from ._fixes.shared import (
 __all__ = [
     'add_altitude_from_plev',
     'add_plev_from_altitude',
+    'add_model_level',
     'get_time_bounds',
     'get_next_month',
 ]
