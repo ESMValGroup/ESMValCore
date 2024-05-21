a0a05bc92b9b97738577fb30fbbbada79359160c
Yousong Zeng <Yousong>
2024-05-21 14:35:15 +1000

    comments added, ready for pull request

--- esmvalcore/cmor/_fixes/access/hi_cn_05.py
+++ esmvalcore/cmor/_fixes/access/hi_cn_05.py
@@ -8,63 +8,93 @@ from ..native_datasets import NativeDatasetFix
 
 from esmvalcore.cmor.check import cmor_check
 
+import os
+
 logger = logging.getLogger(__name__)
 
 class tas(NativeDatasetFix):
+    '''
+    Fix variable(tas) only
+    '''
 
-    def __init__(self,vardef,
-        extra_facets,
-        session,
-        frequency):
+    def __init__(self, vardef, extra_facets, session, frequency):
+        '''
+        Initialise some class variable
+        Heritage from native_dataset
+        '''
 
         super().__init__(vardef,extra_facets,session,frequency)
 
         self.cube=None
 
-    def fix_height2m(self,cube,cubes):
-        if cube.coords('height'):
-            # In case a scalar height is required, remove it here (it is added
-            # at a later stage). The step _fix_height() is designed to fix
-            # non-scalar height coordinates.
-            if (cube.coord('height').shape[0] == 1 and (
-                    'height2m' in self.vardef.dimensions or
-                    'height10m' in self.vardef.dimensions)):
-                # If height is a dimensional coordinate with length 1, squeeze
-                # the cube.
-                # Note: iris.util.squeeze is not used here since it might
-                # accidentally squeeze other dimensions.
-                if cube.coords('height', dim_coords=True):
-                    slices = [slice(None)] * cube.ndim
-                    slices[cube.coord_dims('height')[0]] = 0
-                    cube = cube[tuple(slices)]
-                cube.remove_coord('height')
-            else:
-                cube = self._fix_height(cube, cubes)
-            return cube
+        self.current_dir=os.path.dirname(__file__)
         
-    def fix_height_name(self):
+    def _fix_height_name(self):
+        '''
+        Fix variable name of coordinate 'height' 
+        '''
         if self.cube.coord('height').var_name!='height':
             self.cube.coord('height').var_name='height'
     
-    def fix_long_name(self):
+    def _fix_long_name(self):
+        '''
+        Fix variable long_name
+        '''
         self.cube.long_name ='Near-Surface Air Temperature'
 
-    def fix_var_name(self):
+    def _fix_var_name(self):
+        '''
+        Fix variable long_name
+        '''
         self.cube.var_name='tas'
+    
+    def fix_coord_system(self):
+        '''
+        delete coord_system to make it cna be merged with other cmip dataset by iris.CubeList.merge_dube
+        '''
+        for dim in self.cube.dim_coords:
+            if dim.coord_system!=None:
+                self.cube.coord(dim.standard_name).coord_system=None
+    
+    def _load_master_map(self,short_name):
+        '''
+        Master map is a supplimentary file for how to convert access variable to cmip data
+        
+        Parameters
+        ----------
+        short_name : str
+            short name of variable.
 
-    def fix_metadata(self, cubes):
+        Returns
+        -------
+        list which contain supplimentary imformation of the variable
 
-        master_map_path='./master_map.csv'
-
+        '''
+        master_map_path=f'{self.current_dir}/master_map.csv'
         with open (master_map_path,'r') as map:
             reader=csv.reader(map, delimiter=',')
             for raw in reader:
-                if raw[0]=='tas':
-                    tas_map=raw
-                    break
+                if raw[0]==short_name:
+                    return raw
+    
+    def fix_metadata(self, cubes):
+        """
+        Fix name of coordinate(height), long name and variable name of variable(tas).
 
-        # original_short_name='air_temperature'
-        original_short_name='fld_s03i236'
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+
+        """
+
+        row=self._load_master_map(self.vardef.short_name)
+
+        original_short_name=row[0]
 
         self.cube= self.get_cube(cubes, var_name=original_short_name)
 
@@ -74,80 +104,94 @@ class tas(NativeDatasetFix):
 
         self.fix_var_name()
 
+        self.fix_coord_system()
+
         cube_checked= cmor_check(cube=self.cube,cmor_table='CMIP6',mip='Amon',short_name='tas',check_level=1)
-        
-        print('Successfully get the cube(tas)')
 
         return CubeList([cube_checked])
 
 
 class pr(NativeDatasetFix):
+    '''
+    Fix variable(pr) only
+    '''
 
     def __init__(self,vardef,
         extra_facets,
         session,
         frequency):
+        '''
+        Initialise some class variable
+        Heritage from native_dataset
+        '''
 
         super().__init__(vardef,extra_facets,session,frequency)
 
         self.cube=None
 
-    def fix_height2m(self,cube,cubes):
-        if cube.coords('height'):
-            # In case a scalar height is required, remove it here (it is added
-            # at a later stage). The step _fix_height() is designed to fix
-            # non-scalar height coordinates.
-            if (cube.coord('height').shape[0] == 1 and (
-                    'height2m' in self.vardef.dimensions or
-                    'height10m' in self.vardef.dimensions)):
-                # If height is a dimensional coordinate with length 1, squeeze
-                # the cube.
-                # Note: iris.util.squeeze is not used here since it might
-                # accidentally squeeze other dimensions.
-                if cube.coords('height', dim_coords=True):
-                    slices = [slice(None)] * cube.ndim
-                    slices[cube.coord_dims('height')[0]] = 0
-                    cube = cube[tuple(slices)]
-                cube.remove_coord('height')
-            else:
-                cube = self._fix_height(cube, cubes)
-            return cube
-        else:
-            return cube
-    # def fix_height_name(self, cube):
-    #     for coord in cube.dim_coords:
-    #         if coord.var_name=='height':
-    #             if cube.coord('height').var_name!='height':
-    #                 cube.coord('height').var_name='height'
-    #     return cube
+        self.current_dir=os.path.dirname(__file__)
+    
 
     def fix_var_name(self):
+        '''
+        Fix variable long_name
+        '''
         self.cube.var_name='pr'
     
     def fix_long_name(self):
+        '''
+        Fix variable long_name
+        '''
         self.cube.long_name ='Precipitation'
 
-    # def fix_coord_system(self,cube):
-    #     cube.coords('latitude')[0].coord_system=
 
     def fix_coord_system(self):
+        '''
+        delete coord_system to make it cna be merged with other cmip dataset by iris.CubeList.merge_dube
+        '''
         for dim in self.cube.dim_coords:
             if dim.coord_system!=None:
                 self.cube.coord(dim.standard_name).coord_system=None
+    
+    def _load_master_map(self,short_name):
+        '''
+        Master map is a supplimentary file for how to convert access variable to cmip data
+        
+        Parameters
+        ----------
+        short_name : str
+            short name of variable.
 
-    def fix_metadata(self, cubes):
+        Returns
+        -------
+        list which contain supplimentary imformation of the variable
 
-        master_map_path='./master_map.csv'
-
+        '''
+        master_map_path=f'{self.current_dir}/master_map.csv'
         with open (master_map_path,'r') as map:
             reader=csv.reader(map, delimiter=',')
             for raw in reader:
-                if raw[0]=='pr':
-                    pr_map=raw
-                    break
+                if raw[0]==short_name:
+                    return raw
 
-        # original_short_name='air_temperature'
-        original_short_name='fld_s05i216'
+    def fix_metadata(self, cubes):
+        """
+        Fix name of coordinate(height), long name and variable name of variable(tas).
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+
+        """
+
+        row=self._load_master_map(self.vardef.short_name)
+
+        original_short_name=row[1]
 
         cube= self.get_cube(cubes, var_name=original_short_name)
 
@@ -158,8 +202,6 @@ class pr(NativeDatasetFix):
         self.fix_coord_system()
 
         cube_checked= cmor_check(cube=cube,cmor_table='CMIP6',mip='Amon',short_name='pr',check_level=1)
-
-        print('Successfully get the cube(pr)')
 
         return CubeList([cube_checked])
 
