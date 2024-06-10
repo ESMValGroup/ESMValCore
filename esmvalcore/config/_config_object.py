1c7fb302e4e11891e3366844dd7fde14691b0d88
Manuel Schlund <manuel.schlund@dlr.de>
2024-06-10 09:37:07 +0200

    Proper indentation

--- esmvalcore/config/_config_object.py
+++ esmvalcore/config/_config_object.py
@@ -264,11 +264,11 @@ class Config(ValidatedConfig):
         filename:
             YAML file to load.
 
-            .. deprecated:: 2.12.0
-                This method has been deprecated in ESMValCore version 2.14.0
-                and is scheduled for removal in version 2.14.0. Please update
-                `esmvalcore.config.CFG` directly instead using `CFG.update()`
-                or `CFG[...] = ...`.
+        .. deprecated:: 2.12.0
+            This method has been deprecated in ESMValCore version 2.14.0 and is
+            scheduled for removal in version 2.14.0. Please update
+            `esmvalcore.config.CFG` directly instead using `CFG.update()` or
+            `CFG[...] = ...`.
 
         """
         msg = (
