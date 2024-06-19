6a8fe23f61ad295f3d8ab191666a6dcee3952005
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-06-19 16:13:19 +0100

    fix for new output of np.ogrid

--- esmvalcore/preprocessor/_time.py
+++ esmvalcore/preprocessor/_time.py
@@ -1559,7 +1559,9 @@ def _transform_to_lst_eager(
     """
     # Apart from the time index, all other dimensions will stay the same; this
     # is ensured with np.ogrid
-    idx = np.ogrid[tuple(slice(0, d) for d in data.shape)]
+    # NOTE: in numpy 2.0 np.ogrid returns ndarray or tuple of ndarrays
+    # so item assignment can only be done after converting to a list
+    idx = list(np.ogrid[tuple(slice(0, d) for d in data.shape)])
     time_index = broadcast_to_shape(
         time_index, data.shape, (time_dim, lon_dim)
     )
