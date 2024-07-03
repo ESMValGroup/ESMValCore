597daf7d09a6798ee3a56438ab55658fa0c8b9a2
Lisa Bock <lisa.bock@dlr.de>
2024-07-03 14:24:34 +0200

    New custom variable for tos uncertainty (#2470)

--- tests/integration/cmor/test_table.py
+++ tests/integration/cmor/test_table.py
@@ -477,13 +477,22 @@ class TestCustomInfo(unittest.TestCase):
         self.assertEqual(var.units, 'K')
 
     def test_get_variable_ch4s(self):
-        """Get tas variable."""
+        """Get ch4s variable."""
         CustomInfo()
         var = self.variables_info.get_variable('Amon', 'ch4s')
         self.assertEqual(var.short_name, 'ch4s')
         self.assertEqual(var.long_name,
                          'Atmosphere CH4 surface')
         self.assertEqual(var.units, '1e-09')
+
+    def test_get_variable_tosStderr(self):
+        """Get tosStderr variable."""
+        CustomInfo()
+        var = self.variables_info.get_variable('Omon', 'tosStderr')
+        self.assertEqual(var.short_name, 'tosStderr')
+        self.assertEqual(var.long_name,
+                         'Sea Surface Temperature Error')
+        self.assertEqual(var.units, 'K')
 
 
 @pytest.mark.parametrize(
