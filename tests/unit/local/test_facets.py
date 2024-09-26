52d39511893ae4d573bfbfd83f6300c6adac0971
Bouwe Andela <b.andela@esciencecenter.nl>
2024-09-26 22:06:14 +0200

    Use ruff formatting

--- tests/unit/local/test_facets.py
+++ tests/unit/local/test_facets.py
@@ -6,34 +6,38 @@ from esmvalcore.local import LocalFile, _path2facets, _str2facets
 
 
 @pytest.mark.parametrize(
-    'path,template,facets',
+    "path,template,facets",
     [
         (
             "value1/value2",
             "{facet1}/{facet2.lower}",
             {
-                'facet1': 'value1',
+                "facet1": "value1",
             },
         ),
         (
             "Tier3/dataset1",
             "Tier{tier}/{dataset}",
             {
-                'tier': '3',
-                'dataset': 'dataset1',
+                "tier": "3",
+                "dataset": "dataset1",
             },
         ),
         (
             "value1/xyz/value2",
             "{facet1}/xyz/{facet2}",
             {
-                'facet1': 'value1',
-                'facet2': 'value2',
+                "facet1": "value1",
+                "facet2": "value2",
             },
         ),
-        ("value1/value2value3", "{facet1}/{facet2}{facet3}", {
-            'facet1': 'value1',
-        }),
+        (
+            "value1/value2value3",
+            "{facet1}/{facet2}{facet3}",
+            {
+                "facet1": "value1",
+            },
+        ),
         (
             "value1/value2",
             "",
@@ -45,24 +49,25 @@ from esmvalcore.local import LocalFile, _path2facets, _str2facets
             "value1_value2.nc",
             "{facet1}_{facet2}[_.]*nc",
             {
-                'facet1': 'value1',
+                "facet1": "value1",
             },
         ),
         (
             "value1/value2/xyz/value3",
             "{facet1}/{facet2}/*/{facet3}",
             {
-                'facet1': 'value1',
+                "facet1": "value1",
             },
         ),
         (
             "value1/value2value3/value4",
             "{facet1}/{facet2}{facet3}/{facet4}",
             {
-                'facet1': 'value1',
+                "facet1": "value1",
             },
         ),
-    ])
+    ],
+)
 def test_str2facets(path, template, facets):
     """Test `_str2facets."""
     result = _str2facets(path, template)
@@ -70,17 +75,17 @@ def test_str2facets(path, template, facets):
 
 
 def test_path2facets():
-    file = Path('/climate_data/value1/value2/value2_2000-2001.nc')
+    file = Path("/climate_data/value1/value2/value2_2000-2001.nc")
     facets = _path2facets(
         file,
-        dirname_template='{facet1}/{facet2}',
-        filename_template='{facet2}_*.nc',
+        dirname_template="{facet1}/{facet2}",
+        filename_template="{facet2}_*.nc",
     )
-    assert facets == {'facet1': 'value1', 'facet2': 'value2'}
+    assert facets == {"facet1": "value1", "facet2": "value2"}
 
 
 def test_localfile():
-    file = LocalFile('/a/b.nc')
-    file.facets = {'a': 'A'}
-    assert Path(file) == Path('/a/b.nc')
-    assert file.facets == {'a': 'A'}
+    file = LocalFile("/a/b.nc")
+    file.facets = {"a": "A"}
+    assert Path(file) == Path("/a/b.nc")
+    assert file.facets == {"a": "A"}
