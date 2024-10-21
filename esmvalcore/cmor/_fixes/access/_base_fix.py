6aff095c651c0ff900d9562dae41d300329ea238
rhaegar325 <rhaegar227@gmail.com>
2024-10-21 22:58:15 +1100

    add ocean variables

--- esmvalcore/cmor/_fixes/access/_base_fix.py
+++ esmvalcore/cmor/_fixes/access/_base_fix.py
@@ -2,6 +2,8 @@
 
 import logging
 
+import numpy as np
+
 from iris.cube import CubeList
 
 from esmvalcore.cmor._fixes.native_datasets import NativeDatasetFix
@@ -27,3 +29,37 @@ class AccessFix(NativeDatasetFix):
         for name in name_list:
             data_list.append(self.get_cube(cubes, name))
         return CubeList(data_list)
+    
+    def fix_ocean_dim_coords(self, cube):
+        """Fix dim coords of ocean variables"""
+        cube.dim_coords[-2].points = np.array([int(i) for i in range(300)])
+        cube.dim_coords[-2].standard_name = None
+        cube.dim_coords[-2].var_name = 'j'
+        cube.dim_coords[-2].long_name = 'cell index along second dimension'
+        cube.dim_coords[-2].attributes = None
+
+        cube.dim_coords[-1].points = np.array([int(i) for i in range(360)])
+        cube.dim_coords[-1].standard_name = None
+        cube.dim_coords[-1].var_name = 'i'
+        cube.dim_coords[-1].long_name = 'cell index along first dimension'
+        cube.dim_coords[-1].attributes = None
+    
+    def fix_ocean_aux_coords(self, cube):
+        """Fix aux coords of ocean variables"""
+        temp_points=[]
+        for i in cube.aux_coords[-1].points:
+            temp_points.append([j + 360 for j in i if j < 0]+[j for j in i if j >= 0])
+        cube.aux_coords[-1].points = np.array(temp_points)
+        cube.aux_coords[-1].standard_name = 'longitude'
+        cube.aux_coords[-1].long_name = 'longitude'
+        cube.aux_coords[-1].var_name = 'longitude'
+        cube.aux_coords[-1].attributes = None
+
+        temp_points=[]
+        for i in cube.aux_coords[-2].points:
+            temp_points.append([j.astype(np.float64) for j in i])
+        cube.aux_coords[-2].points = np.array(temp_points)
+        cube.aux_coords[-2].standard_name = 'latitude'
+        cube.aux_coords[-2].long_name = 'latitude'
+        cube.aux_coords[-2].var_name = 'latitude'
+        cube.aux_coords[-2].attributes = None
--- esmvalcore/cmor/_fixes/access/access_esm1_5.py
+++ esmvalcore/cmor/_fixes/access/access_esm1_5.py
@@ -5,6 +5,8 @@ from iris.cube import CubeList
 
 from ._base_fix import AccessFix
 
+from cf_units import Unit
+
 logger = logging.getLogger(__name__)
 
 
@@ -125,3 +127,65 @@ class Tas(AccessFix):
         """Fix height value to make it comparable to other dataset."""
         if cube.coord('height').points[0] != 2:
             cube.coord('height').points = [2]
+
+class Tos(AccessFix):
+    """Fixes for Tos"""
+
+    def fix_metadata(self, cubes):
+        """Fix metadata.
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+
+        cube = self.get_cube(cubes)
+
+        self.fix_ocean_dim_coords(cube)
+        self.fix_ocean_aux_coords(cube)
+
+        return CubeList([cube])
+
+
+class So(AccessFix):
+    """FIxes for So"""
+
+    def fix_metadata(self, cubes):
+        """Fix metadata.
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+
+        cube = self.get_cube(cubes)
+
+        self.fix_ocean_dim_coords(cube)
+        self.fix_ocean_aux_coords(cube)
+        self.fix_depth_metadata(cube)
+        self.fix_so_units(cube)
+
+        return CubeList([cube])
+    
+    def fix_depth_metadata(self, cube):
+        """fix depth metadata"""
+        cube.dim_coords[1].standard_name = 'depth'
+        cube.dim_coords[1].long_name = 'ocean depth coordinate'
+        cube.dim_coords[1].var_name = 'lev'
+        cube.dim_coords[1].attributes = {'positive':'down'}  
+
+    def fix_so_units(self, cube):
+        """fix units of so"""
+        cube.attributes.pop('invalid_units')
+        cube.units=Unit(0.001)     
+    
