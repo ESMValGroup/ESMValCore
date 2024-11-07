3f2cd5c55d70eea9d2ad5b7790e213f36b5ceaa5
Axel Lauer <axel.lauer@dlr.de>
2024-11-07 15:44:32 +0100

    fixed derivation script for siextent

--- esmvalcore/preprocessor/_derive/siextent.py
+++ esmvalcore/preprocessor/_derive/siextent.py
@@ -1,4 +1,4 @@
-"""Derivation of variable `sithick`."""
+"""Create mask for derivation of variable `siextent`."""
 
 import logging
 
@@ -14,14 +14,15 @@ logger = logging.getLogger(__name__)
 
 
 class DerivedVariable(DerivedVariableBase):
-    """Derivation of variable `siextent`."""
+    """Create mask for derivation of variable `siextent`."""
 
     @staticmethod
     def required(project):
-        """Declare the variables needed for derivation."""
+        """Declare the variable needed for derivation."""
+        # 'sic' only is sufficient as there is already an entry 
+        # in the mapping table esmvalcore/cmor/variable_alt_names.yml
         required = [
-            {"short_name": "sic", "optional": "true"},
-            {"short_name": "siconca", "optional": "true"},
+            {"short_name": "sic"},
         ]
         return required
 
@@ -30,11 +31,11 @@ class DerivedVariable(DerivedVariableBase):
         """Compute sea ice extent.
 
         Returns an array of ones in every grid point where
-        the sea ice area fraction has values > 15 .
+        the sea ice area fraction has values > 15% .
 
         Use in combination with the preprocessor
         `area_statistics(operator='sum')` to weigh by the area and
-        compute global or regional sea ice extent values.
+        compute global or regional sea ice extent values (in m2).
 
         Arguments
         ---------
@@ -44,20 +45,24 @@ class DerivedVariable(DerivedVariableBase):
         -------
             Cube containing sea ice extent.
         """
+        print(cubes)
         try:
             sic = cubes.extract_cube(Constraint(name="sic"))
         except iris.exceptions.ConstraintMismatchError:
             try:
-                sic = cubes.extract_cube(Constraint(name="siconca"))
+                sic = cubes.extract_cube(Constraint(name="siconc"))
             except iris.exceptions.ConstraintMismatchError as exc:
                 raise RecipeError(
                     "Derivation of siextent failed due to missing variables "
-                    "sic and siconca."
+                    "sic and siconc."
                 ) from exc
 
         ones = da.ones_like(sic)
         siextent_data = da.ma.masked_where(sic.lazy_data() < 15.0, ones)
         siextent = sic.copy(siextent_data)
-        siextent.units = "m2"
+        siextent.units = "1"  # unit is 1 as this is just a mask
+                              # that has to be used with preprocessor
+                              # area_statistics(operator='sum') to
+                              # obtain the sea ice extent (m2)
 
         return siextent
