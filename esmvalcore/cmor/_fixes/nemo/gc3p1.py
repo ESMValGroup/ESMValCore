14787d2afbb6a21d5055defd29256aae8cc8fb9a
Sophie Hall <sophie.hall@metoffice.gov.uk>
2024-11-15 16:25:48 +0000

    #2572: Emma Hogans fix

--- esmvalcore/cmor/_fixes/nemo/gc3p1.py
+++ esmvalcore/cmor/_fixes/nemo/gc3p1.py
@@ -19,3 +19,20 @@ class AllVars(Fix):
                     break
 
         return cubes
+
+#    def remove_extra_time_axis(cube):
+#        count_time_axes = 0
+#        time_axes_names = []
+#        for coord in cube.coords():
+#            if iris.util.guess_coord_axis(coord) == 'T':
+#                count_time_axes += 1
+#                time_axes_names.append(coord.standard_name)
+#
+#        if count_time_axes >= 2 and len(set(time_axes_names)) == 1:
+#            for aux_coord in cube.coords(dim_coords=False):
+#                if iris.util.guess_coord_axis(aux_coord) == 'T':
+#                    cube.remove_coord(aux_coord)
+#        else:
+#            for coord in cube.coords():
+#                if coord.var_name == 'time_counter':
+#                    cube.remove_coord(coord)
--- esmvalcore/cmor/_fixes/nemo/gc5.py
+++ esmvalcore/cmor/_fixes/nemo/gc5.py
@@ -4,16 +4,14 @@ from esmvalcore.cmor.fix import Fix
 from iris.util import promote_aux_coord_to_dim_coord
 
 
+
 class AllVars(Fix):
     """"""
 
     def fix_metadata(self, cubes):
         """"""
-        # Correct variable name
+        # Replace time_counter with time_centered/time_instant
         for cube in cubes:
-            if cube.var_name.endswith('_con'):
-                cube.var_name = cube.var_name[:-4]
-
             for coordinate in cube.coords(dim_coords=True):
                 if coordinate.var_name == 'time_counter':
                     cube.remove_coord(coordinate)
@@ -23,4 +21,19 @@ class AllVars(Fix):
 
         return cubes
 
-#check with emma about adding in the fix
+#    def remove_extra_time_axis(cube):
+#        count_time_axes = 0
+#        time_axes_names = []
+#        for coord in cube.coords():
+#            if iris.util.guess_coord_axis(coord) == 'T':
+#                count_time_axes += 1
+#                time_axes_names.append(coord.standard_name)
+#
+#        if count_time_axes >= 2 and len(set(time_axes_names)) == 1:
+#            for aux_coord in cube.coords(dim_coords=False):
+#                if iris.util.guess_coord_axis(aux_coord) == 'T':
+#                    cube.remove_coord(aux_coord)
+#        else:
+#            for coord in cube.coords():
+#                if coord.var_name == 'time_counter':
+#                    cube.remove_coord(coord)
