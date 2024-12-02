4a15aa5ba7684b64d9f8360de7dbd4183bbf0ed9
rhaegar325 <rhaegar227@gmail.com>
2024-12-02 16:28:13 +1100

    fix test script

--- tests/integration/cmor/_fixes/access/test_access_esm1_5.py
+++ tests/integration/cmor/_fixes/access/test_access_esm1_5.py
@@ -268,8 +268,8 @@ def check_ocean_aux_coords(cube):
     assert cube.aux_coords[-2].attributes == {}
 
     assert cube.aux_coords[-1].shape == (300, 360)
-    assert int(cube.aux_coords[-1].points.max()) <= 360
-    assert min(cube.aux_coords[-1].points) > 0
+    assert (cube.aux_coords[-1].points.max() <= 360 < 360).all()
+    assert (cube.aux_coords[-1].points > 0).all()
     assert cube.aux_coords[-1].standard_name == 'longitude'
     assert cube.aux_coords[-1].long_name == 'longitude'
     assert cube.aux_coords[-1].var_name == 'lon'
