e82ecdd06d5d4404076332eccc60920c787d5dec
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-01-16 14:24:30 +0000

    add fix for oh

--- esmvalcore/cmor/_fixes/cmip6/ec_earth3.py
+++ esmvalcore/cmor/_fixes/cmip6/ec_earth3.py
@@ -75,3 +75,35 @@ class Tas(Fix):
             latitude.bounds = np.round(latitude.core_bounds(), 8)
 
         return cubes
+
+
+class Oh(Fix):
+    """Fixes for oh."""
+
+    def fix_metadata(self, cubes):
+        """Fix standard name for ps.
+
+        Fix standard_name for Surface Air Pressure (ps).
+        See discussion in
+        https://github.com/ESMValGroup/ESMValCore/issues/2613
+        Cube has two coordinates called air_pressure: an AuxCoord ps
+        and a DerivedCoord that is 4D and derived using formula terms,
+        we are setting the former's standard_name to None.
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+        """
+        cube = self.get_cube_from_list(cubes)
+
+        for cube in cubes:
+            for coord in cube.coords():
+                if coord.var_name == "ps":
+                    coord.standard_name = None
+
+        return cubes
