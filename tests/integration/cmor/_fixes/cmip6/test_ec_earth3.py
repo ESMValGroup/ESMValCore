cbddd0e747a9d47268a9e7b6513a66481a85c1c8
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-01-16 15:05:51 +0000

    remove ox fix test from ec-earth3

--- tests/integration/cmor/_fixes/cmip6/test_ec_earth3.py
+++ tests/integration/cmor/_fixes/cmip6/test_ec_earth3.py
@@ -9,7 +9,7 @@ import pytest
 from cf_units import Unit
 from iris.cube import Cube
 
-from esmvalcore.cmor._fixes.cmip6.ec_earth3 import AllVars, Oh, Siconca, Tas
+from esmvalcore.cmor._fixes.cmip6.ec_earth3 import AllVars, Siconca, Tas
 from esmvalcore.cmor.fix import Fix
 from esmvalcore.cmor.table import get_var_info
 
@@ -77,23 +77,6 @@ def tas_cubes():
     )
 
     return iris.cube.CubeList([tas_cube, tas_cube_to_round])
-
-
-@pytest.fixture
-def oh_cubes():
-    air_pressure_coord = iris.coords.DimCoord(
-        [1000.09, 600.6, 200.0],
-        bounds=[[1200.00001, 800], [800, 400.8], [400.8, 1.9]],
-        var_name="ps",
-        standard_name="air_pressure",
-        units="pa",
-    )
-    oh_cube = iris.cube.Cube(
-        [0.0, 1.0, 2.0],
-        var_name="oh",
-        dim_coords_and_dims=[(air_pressure_coord, 0)],
-    )
-    return iris.cube.CubeList([oh_cube])
 
 
 def test_get_tas_fix():
@@ -165,19 +148,3 @@ def test_allvars_r3i1p1f1_fix_latitude():
 
     assert fixed_cubes[0].coord("latitude").points[0] == -71.228
     assert fixed_cubes[1].coord("latitude").points[0] == -71.228
-
-
-def test_get_oh_fix():
-    """Test getting of fix."""
-    fix = Fix.get_fixes("CMIP6", "EC-Earth3", "AERmonZ", "oh")
-    assert Oh(None) in fix
-
-
-def test_oh_fix_metadata(oh_cubes):
-    """Test ``fix_metadata`` for ``oh``."""
-    vardef = get_var_info("CMIP6", "AERmonZ", "oh")
-    fix = Oh(vardef)
-    fixed_cubes = fix.fix_metadata(oh_cubes)
-    for coord in fixed_cubes[0].coords():
-        if coord.var_name == "ps":
-            assert coord.standard_name is None
