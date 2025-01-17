7554c6de3ee4d2c7285434292d7795a0363dc61b
Bouwe Andela <b.andela@esciencecenter.nl>
2025-01-17 23:05:27 +0100

    Another attempt

--- tests/unit/task/test_available_cpu_count.py
+++ tests/unit/task/test_available_cpu_count.py
@@ -4,12 +4,8 @@ from esmvalcore import _task
 
 
 def test_available_cpu_count_linux(mocker):
-    mocker.patch.object(
-        _task.os,
-        "sched_getaffinity",
-        create=True,
-        return_value={0, 1},
-    )
+    mocker.patch.object(_task, "os")
+    _task.os.sched_getaffinity.return_value = {0, 1}
     result = _task.available_cpu_count()
     assert result == 2
     _task.os.sched_getaffinity.assert_called_once_with(0)
@@ -22,9 +18,10 @@ def test_available_cpu_count_linux(mocker):
         (2, 2),
     ],
 )
-def test_available_cpu_count_osx(monkeypatch, mocker, cpu_count, expected):
-    monkeypatch.delattr(_task.os, "sched_getaffinity")
-    mocker.patch.object(_task.os, "cpu_count", return_value=cpu_count)
+def test_available_cpu_count_osx(mocker, cpu_count, expected):
+    mocker.patch.object(_task, "os")
+    del _task.os.sched_getaffinity
+    _task.os.cpu_count.return_value = cpu_count
     result = _task.available_cpu_count()
     assert result == expected
     _task.os.cpu_count.assert_called_once_with()
