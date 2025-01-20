0dad428343b227cf65e5581e36de18eea06f47e1
Manuel Schlund <manuel.schlund@dlr.de>
2025-01-20 13:45:18 +0100

    Make sure that supplementary variables have same chunks as parent cube

--- esmvalcore/preprocessor/_supplementary_vars.py
+++ esmvalcore/preprocessor/_supplementary_vars.py
@@ -1,22 +1,26 @@
 """Preprocessor functions for ancillary variables and cell measures."""
 
 import logging
-from typing import Iterable
+from collections.abc import Callable
+from typing import Iterable, Literal
 
 import iris.coords
-import iris.cube
+from iris.cube import Cube
 
 logger = logging.getLogger(__name__)
 
 PREPROCESSOR_SUPPLEMENTARIES = {}
 
 
-def register_supplementaries(variables, required):
+def register_supplementaries(
+    variables: list[str],
+    required: Literal["require_at_least_one", "prefer_at_least_one"],
+) -> Callable:
     """Register supplementary variables required for a preprocessor function.
 
     Parameters
     ----------
-    variables: :obj:`list` of :obj`str`
+    variables:
         List of variable names.
     required:
         How strong the requirement is. Can be 'require_at_least_one' if at
@@ -39,16 +43,25 @@ def register_supplementaries(variables, required):
     return wrapper
 
 
-def add_cell_measure(cube, cell_measure_cube, measure):
+def add_cell_measure(
+    cube: Cube,
+    cell_measure_cube: Cube,
+    measure: Literal["area", "volume"],
+) -> None:
     """Add a cube as a cell_measure in the cube containing the data.
+
+    Note
+    ----
+    This assumes that the cell measure spans the rightmost dimensions of the
+    cube.
 
     Parameters
     ----------
-    cube: iris.cube.Cube
+    cube:
         Iris cube with input data.
-    cell_measure_cube: iris.cube.Cube
+    cell_measure_cube:
         Iris cube with cell measure data.
-    measure: str
+    measure:
         Name of the measure, can be 'area' or 'volume'.
 
     Returns
@@ -65,16 +78,22 @@ def add_cell_measure(cube, cell_measure_cube, measure):
         raise ValueError(
             f"measure name must be 'area' or 'volume', got {measure} instead"
         )
-    measure = iris.coords.CellMeasure(
-        cell_measure_cube.core_data(),
+    coord_dims = tuple(
+        range(cube.ndim - len(cell_measure_cube.shape), cube.ndim)
+    )
+    cell_measure_data = cell_measure_cube.core_data()
+    if cell_measure_cube.has_lazy_data():
+        cube_chunks = tuple(cube.lazy_data().chunks[d] for d in coord_dims)
+        cell_measure_data = cell_measure_data.rechunk(cube_chunks)
+    cell_measure = iris.coords.CellMeasure(
+        cell_measure_data,
         standard_name=cell_measure_cube.standard_name,
         units=cell_measure_cube.units,
         measure=measure,
         var_name=cell_measure_cube.var_name,
         attributes=cell_measure_cube.attributes,
     )
-    start_dim = cube.ndim - len(measure.shape)
-    cube.add_cell_measure(measure, range(start_dim, cube.ndim))
+    cube.add_cell_measure(cell_measure, coord_dims)
     logger.debug(
         "Added %s as cell measure in cube of %s.",
         cell_measure_cube.var_name,
@@ -82,14 +101,19 @@ def add_cell_measure(cube, cell_measure_cube, measure):
     )
 
 
-def add_ancillary_variable(cube, ancillary_cube):
+def add_ancillary_variable(cube: Cube, ancillary_cube: Cube) -> None:
     """Add cube as an ancillary variable in the cube containing the data.
+
+    Note
+    ----
+    This assumes that the ancillary variable spans the rightmost dimensions of
+    the cube.
 
     Parameters
     ----------
-    cube: iris.cube.Cube
+    cube:
         Iris cube with input data.
-    ancillary_cube: iris.cube.Cube
+    ancillary_cube:
         Iris cube with ancillary data.
 
     Returns
@@ -97,15 +121,19 @@ def add_ancillary_variable(cube, ancillary_cube):
     iris.cube.Cube
         Cube with added ancillary variables
     """
+    coord_dims = tuple(range(cube.ndim - len(ancillary_cube.shape), cube.ndim))
+    ancillary_data = ancillary_cube.core_data()
+    if ancillary_cube.has_lazy_data():
+        cube_chunks = tuple(cube.lazy_data().chunks[d] for d in coord_dims)
+        ancillary_data = ancillary_data.rechunk(cube_chunks)
     ancillary_var = iris.coords.AncillaryVariable(
-        ancillary_cube.core_data(),
+        ancillary_data,
         standard_name=ancillary_cube.standard_name,
         units=ancillary_cube.units,
         var_name=ancillary_cube.var_name,
         attributes=ancillary_cube.attributes,
     )
-    start_dim = cube.ndim - len(ancillary_var.shape)
-    cube.add_ancillary_variable(ancillary_var, range(start_dim, cube.ndim))
+    cube.add_ancillary_variable(ancillary_var, coord_dims)
     logger.debug(
         "Added %s as ancillary variable in cube of %s.",
         ancillary_cube.var_name,
@@ -114,9 +142,9 @@ def add_ancillary_variable(cube, ancillary_cube):
 
 
 def add_supplementary_variables(
-    cube: iris.cube.Cube,
-    supplementary_cubes: Iterable[iris.cube.Cube],
-) -> iris.cube.Cube:
+    cube: Cube,
+    supplementary_cubes: Iterable[Cube],
+) -> Cube:
     """Add ancillary variables and/or cell measures.
 
     Parameters
@@ -131,7 +159,7 @@ def add_supplementary_variables(
     iris.cube.Cube
         Cube with added ancillary variables and/or cell measures.
     """
-    measure_names = {
+    measure_names: dict[str, Literal["area", "volume"]] = {
         "areacella": "area",
         "areacello": "area",
         "volcello": "volume",
@@ -145,7 +173,7 @@ def add_supplementary_variables(
     return cube
 
 
-def remove_supplementary_variables(cube: iris.cube.Cube):
+def remove_supplementary_variables(cube: Cube) -> Cube:
     """Remove supplementary variables.
 
     Strip cell measures or ancillary variables from the cube containing the
@@ -153,7 +181,7 @@ def remove_supplementary_variables(cube: iris.cube.Cube):
 
     Parameters
     ----------
-    cube: iris.cube.Cube
+    cube:
         Iris cube with data and cell measures or ancillary variables.
 
     Returns
