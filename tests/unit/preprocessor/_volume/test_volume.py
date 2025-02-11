0b21807cc481d960b4de44b1975caa218038118a
jlenh <julien.lenhardt@gmail.com>
2025-02-11 11:51:40 +0100

    Update tests for _get_first_unmasked_data and extract_surface_from_atm

--- tests/unit/preprocessor/_volume/test_volume.py
+++ tests/unit/preprocessor/_volume/test_volume.py
@@ -19,7 +19,8 @@ from esmvalcore.preprocessor._volume import (
     extract_transect,
     extract_volume,
     volume_statistics,
-    extract_surface_from_atm
+    extract_surface_from_atm,
+    _get_first_unmasked_data
 )
 
 
@@ -205,15 +206,11 @@ class Test(tests.Test):
             units="kg m-3",
             var_name="var"
         )
-        ps_ancillary = iris.coords.AncillaryVariable(
-            np.broadcast_to([[[0.25]], [[0.5]], [[5.0]], [[27.5]]], (4, 2, 2)),
-            standard_name="surface_air_pressure",
-            units='Pa',
-            var_name='ps'
-        )
-        self.grid_4d_ps.add_ancillary_variable(
-            ps_ancillary,
-            [0, 2, 3]
+        self.grid_4d_ps_mask = iris.cube.Cube(
+            data3,
+            dim_coords_and_dims=coords_spec_ps,
+            units="kg m-3",
+            var_name="var"
         )
 
         # allow iris to figure out the axis='z' coordinate
@@ -223,6 +220,9 @@ class Test(tests.Test):
         iris.util.guess_coord_axis(self.grid_4d_z.coord("zcoord"))
         iris.util.guess_coord_axis(
             self.grid_4d_ps.coord("air_pressure")
+        )
+        iris.util.guess_coord_axis(
+            self.grid_4d_ps_mask.coord("air_pressure")
         )
 
     def test_add_axis_stats_weights_coord(self):
@@ -784,8 +784,35 @@ class Test(tests.Test):
         expected = np.ones((3, 2))
         self.assert_array_equal(result.data, expected)
 
+    def test__get_first_unmasked_data(self):
+        """Test to get first unmasked value of an array along an axis."""
+        (z_axis,) = self.grid_4d_2.coord_dims(
+            self.grid_4d_2.coord(axis="Z", dim_coords=True)
+        )
+        result = _get_first_unmasked_data(
+            self.grid_4d_2.core_data(), axis=z_axis
+        )
+        expected = np.ones((4, 2, 2))
+        self.assert_array_equal(result, expected)
+
     def test_extract_surface_from_atm(self):
         """Test to extract surface from a (4, 3, 2, 2) cube."""
+        # Create surface air pressure ancillary variable for interpolation
+        ps_ancillary = iris.coords.AncillaryVariable(
+            np.broadcast_to([[[0.25]], [[0.5]], [[5.0]], [[27.5]]], (4, 2, 2)),
+            standard_name="surface_air_pressure",
+            units='Pa',
+            var_name='ps'
+        )
+        # Test without air pressure ancillary variable
+        with self.assertRaises(ValueError) as err:
+            extract_surface_from_atm(self.grid_4d_ps)
+        assert "Surface air pressure could not be found" in str(err.exception)
+        # Test with ancillary variable
+        self.grid_4d_ps.add_ancillary_variable(
+            ps_ancillary,
+            [0, 2, 3]
+        )
         result = extract_surface_from_atm(self.grid_4d_ps)
         expected = np.ma.array(
             np.broadcast_to(
@@ -793,6 +820,15 @@ class Test(tests.Test):
                 (4, 2, 2)
             )
         )
+        self.assert_array_equal(result.data, expected)
+        assert result.var_name == "vars"
+        # Test with ancillary variable and masked input
+        self.grid_4d_ps_mask.add_ancillary_variable(
+            ps_ancillary,
+            [0, 2, 3]
+        )
+        result = extract_surface_from_atm(self.grid_4d_ps_mask)
+        expected = np.ma.array(np.ones((4, 2, 2)))
         self.assert_array_equal(result.data, expected)
         assert result.var_name == "vars"
 
