c40fd052b603ba8bf3cd1308ecdf43d79bdb60d4
David Hohn <dhohn@geomar.de>
2025-03-12 16:47:34 +0100

    fix units and values of tos variable in GISS-E2-1-G-CC esm-piControl

---
+++ esmvalcore/cmor/_fixes/cmip6/giss_e2_1_g_cc.py
@@ -0,0 +1,14 @@
+"""Fixes for GISS-E2-1-G-CC model."""
+from ..common import ClFixHybridPressureCoord
+from .giss_e2_1_g import Tos as BaseTos
+
+Cl = ClFixHybridPressureCoord
+
+
+Cli = ClFixHybridPressureCoord
+
+
+Clw = ClFixHybridPressureCoord
+
+
+Tos = BaseTos
