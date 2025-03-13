2b91fecce36d10de25d75bdc3870a5208d561255
Charles Turner <charles.turner@anu.edu.au>
2025-03-13 09:43:52 +0800

    Cleanup - mypy & ruff errors

--- esmvalcore/intake/_dataset.py
+++ esmvalcore/intake/_dataset.py
@@ -1,27 +1,21 @@
 import logging
 from pathlib import Path
 from typing import Sequence
-# import isodate
 
+# import isodate
 import intake
 import intake_esm
-import iris
-# from ncdata.iris_xarray import cubes_from_xarray
 
-from esmvalcore.config._config import get_project_config
 from esmvalcore.config import CFG
-from esmvalcore.preprocessor import clip_timerange, fix_metadata
-# from esmvalcore.cmor.table import VariableInfo
+from esmvalcore.config._config import get_project_config
 from esmvalcore.dataset import Dataset, File
 from esmvalcore.local import LocalFile
-# from esmvalcore.typing import Facets, FacetValue
 
-__all__ = ["IntakeDataset", "get_project_config", "_select_files"]
+__all__ = ["IntakeDataset", "load_catalogs", "clear_catalog_cache"]
 
 logger = logging.getLogger(__name__)
 
 _CACHE: dict[Path, intake_esm.core.esm_datastore] = {}
-
 
 
 def clear_catalog_cache():
@@ -29,7 +23,9 @@ def clear_catalog_cache():
     _CACHE.clear()
 
 
-def load_catalogs(project, drs) -> tuple[list[intake_esm.core.esm_datastore], list[dict]]:
+def load_catalogs(
+    project, drs
+) -> tuple[list[intake_esm.core.esm_datastore], list[dict]]:
     """Load all intake-esm catalogs for a project and their associated facet mappings.
 
     Parameters
@@ -51,14 +47,13 @@ def load_catalogs(project, drs) -> tuple[list[intake_esm.core.esm_datastore], li
     catalog_info = get_project_config(project).get("catalogs", {})
     site = drs.get(project, "default")
     if site not in catalog_info:
-        return None, {}
+        return [None], [{}]
 
     catalog_urls = [
-     Path(catalog.get('file')).expanduser() for catalog in catalog_info[site]
+        Path(catalog.get("file")).expanduser()
+        for catalog in catalog_info[site]
     ]
-    facet_list = [
-        catalog.get('facets') for catalog in catalog_info[site]
-    ]
+    facet_list = [catalog.get("facets") for catalog in catalog_info[site]]
 
     for catalog_url in catalog_urls:
         if catalog_url not in _CACHE:
@@ -69,18 +64,13 @@ def load_catalogs(project, drs) -> tuple[list[intake_esm.core.esm_datastore], li
             _CACHE[catalog_url] = intake.open_esm_datastore(catalog_url)
             logger.info("Successfully loaded catalog %s", catalog_url)
 
-    return (
-         [_CACHE[cat_url] for cat_url in catalog_urls], facet_list
-    )
-
+    return ([_CACHE[cat_url] for cat_url in catalog_urls], facet_list)
 
 
 class IntakeDataset(Dataset):
-    """
-    Class to handle loading data using Intake-esm.
-    """
+    """Load data using Intake-ESM."""
 
-    def __init__(self,  **facets):
+    def __init__(self, **facets):
         project = facets["project"]
         self.catalog, self._facets = load_catalogs(project, CFG["drs"])
         super().__init__(**facets)
@@ -91,17 +81,19 @@ class IntakeDataset(Dataset):
             self._files = self._find_files(self.facets, CFG["drs"])
         return self._files
 
+    @files.setter
+    def files(self, value):
+        self._files = value
+
     @property
     def filenames(self) -> Sequence[str]:
         return [str(f) for f in self.files]
 
-    def _find_files(self, variable : dict , drs) -> Sequence[File]:
-        """Find files for variable in all intake-esm catalogs associated with a
-        project.
+    def _find_files(self, variable: dict, drs) -> Sequence[File]:  # type: ignore[override]
+        """Find files for variable in all intake-esm catalogs associated with a project.
 
         As a side effect, sets the unmapped_facets attribute - this is used to
         cache facets which are not in the datastore.
-
 
         Parameters
         ----------
@@ -118,7 +110,6 @@ class IntakeDataset(Dataset):
         drs : dict
             The DRS configuration. Can be obtained from the global configuration drs
             field, eg. CFG['drs'].
-
         """
         catalogs, facets_list = load_catalogs(variable["project"], drs)
         if not catalogs:
@@ -126,11 +117,13 @@ class IntakeDataset(Dataset):
 
         files = []
 
-        for catalog, facets in zip(catalogs, facets_list):
-            query = {val : variable.get(key) for key, val in facets.items()}
-            query = {key : val for key, val in query.items() if val is not None}
+        for catalog, facets in zip(catalogs, facets_list, strict=False):
+            query = {val: variable.get(key) for key, val in facets.items()}
+            query = {key: val for key, val in query.items() if val is not None}
 
-            unmapped = {key: val for key, val in variable.items() if key not in facets}
+            unmapped = {
+                key: val for key, val in variable.items() if key not in facets
+            }
             unmapped.pop("project", None)
 
             self._unmapped_facets = unmapped
@@ -138,16 +131,17 @@ class IntakeDataset(Dataset):
             selection = catalog.search(**query)
 
             # Select latest version
-            if 'version' in facets and 'version' not in variable:
-                latest_version = max(selection.unique().version) # These are strings - so wtf doe this mean?
-                variable['version'] = latest_version
+            if "version" in facets and "version" not in variable:
+                latest_version = max(
+                    selection.unique().version
+                )  # These are strings - need to double check the sorting here.
+                variable["version"] = latest_version
                 query = {
-                    facets['version']: latest_version,
+                    facets["version"]: latest_version,
                 }
                 selection = selection.search(**query)
 
                 files += [LocalFile(f) for f in selection.unique().path]
-
 
         self.augment_facets()
         return files
