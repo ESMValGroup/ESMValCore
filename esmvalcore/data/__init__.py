2527059f389aad335d35ae0621bfe31c492a48d8
Charles Turner <charles.turner@anu.edu.au>
2025-05-13 09:50:12 +0800

    Add `merge_intake_seach_history` function (see https://github.com/intake/intake-esm/pull/717/commits/73f150e33724010a98b6aed15a39b2c09676a442)

--- esmvalcore/data/__init__.py
+++ esmvalcore/data/__init__.py
@@ -1,5 +1,10 @@
 """Find files using an intake-esm catalog and load them."""
 
 from .intake._intake_dataset import clear_catalog_cache, load_catalogs
+from .intake._interface import merge_intake_search_history
 
-__all__ = ["load_catalogs", "clear_catalog_cache"]
+__all__ = [
+    "load_catalogs",
+    "clear_catalog_cache",
+    "merge_intake_search_history",
+]
