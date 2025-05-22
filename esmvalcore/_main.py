afa713bc7582c7cf3bd57567941c629693335064
Manuel Schlund <manuel.schlund@dlr.de>
2025-05-22 11:27:35 +0200

    Ignore config_file when ESMVALTOOL_CONFIG_DIR is given

--- esmvalcore/_main.py
+++ esmvalcore/_main.py
@@ -28,11 +28,14 @@ from __future__ import annotations
 import logging
 import os
 import sys
+import warnings
 from importlib.metadata import entry_points
 from pathlib import Path
 from typing import Optional
 
 import fire
+
+from esmvalcore.exceptions import ESMValCoreDeprecationWarning
 
 # set up logging
 logger = logging.getLogger(__name__)
@@ -87,7 +90,7 @@ def process_recipe(recipe_file: Path, session):
             errno.ENOENT, "Specified recipe file does not exist", recipe_file
         )
 
-    timestamp1 = datetime.datetime.utcnow()
+    timestamp1 = datetime.datetime.now(datetime.UTC)
     timestamp_format = "%Y-%m-%d %H:%M:%S"
 
     logger.info(
@@ -134,7 +137,7 @@ def process_recipe(recipe_file: Path, session):
     # run
     recipe.run()
     # End time timing
-    timestamp2 = datetime.datetime.utcnow()
+    timestamp2 = datetime.datetime.now(datetime.UTC)
     logger.info(
         "Ending the Earth System Model Evaluation Tool at time: %s UTC",
         timestamp2.strftime(timestamp_format),
@@ -399,11 +402,25 @@ class ESMValTool:
         # https://github.com/ESMValGroup/ESMValCore/issues/2280), but no error
         # has been raised if the file does not exist. Thus, reload the file
         # here with `load_from_file` to make sure a proper error is raised.
-        if "config_file" in kwargs and not os.environ.get(
-            "ESMVALTOOL_CONFIG_DIR"
-        ):
-            cli_config_dir = kwargs["config_file"]
-            CFG.load_from_file(kwargs["config_file"])
+        if "config_file" in kwargs:
+            if os.environ.get("ESMVALTOOL_CONFIG_DIR"):
+                deprecation_msg = (
+                    "Usage of a single configuration file specified via CLI "
+                    "argument `--config_file` has been deprecated in "
+                    "ESMValCore version 2.12.0 and is scheduled for removal "
+                    "in version 2.14.0. Since the environment variable "
+                    "ESMVALTOOL_CONFIG_DIR is set, old configuration files "
+                    "present at ~/.esmvaltool/config-user.yml and/or "
+                    "specified via `--config_file` are ignored. To silence "
+                    "this warning, omit CLI argument `--config_file`."
+                )
+                warnings.warn(
+                    deprecation_msg, ESMValCoreDeprecationWarning, stacklevel=2
+                )
+                kwargs.pop("config_file")
+            else:
+                cli_config_dir = kwargs["config_file"]
+                CFG.load_from_file(kwargs["config_file"])
 
         # New in v2.12.0: read additional configuration directory given by CLI
         # argument
