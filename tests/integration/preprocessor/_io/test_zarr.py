37fcffff3eab58441753ee2bf6e89ad58bd732f6
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-07-29 13:25:40 +0100

    add xr to ncdata test

--- tests/integration/preprocessor/_io/test_zarr.py
+++ tests/integration/preprocessor/_io/test_zarr.py
@@ -5,6 +5,8 @@ This is a dedicated test module for Zarr files IO; we have identified
 a number of issues with Zarr IO so it deserves its own test module.
 """
 
+import iris
+import ncdata
 import xarray as xr
 
 
@@ -12,9 +14,16 @@ def test_load_zarr_xarray():
     """
     Test loading a Zarr store from a https Object Store.
 
-    This is a Zarr Group (multivariate file).
     This tests only the Xarray load, and not the iris cube
     conversion via ``ncdata``.
+
+    This is a Zarr Group (multivariate file: 24 variables).
+    It is on a HEALPIX grid.
+
+    $ mc du bryan/esmvaltool-zarr/um.PT1H.hp_z2.zarr
+    125MiB	132 objects	esmvaltool-zarr/um.PT1H.hp_z2.zarr
+    Loading in Xarry takes 1.5-2s and ingests a max RES mem 430MB.
+    No issues on CircleCI.
     """
     zarr_path = (
         "https://uor-aces-o.s3-ext.jc.rl.ac.uk/"
@@ -30,3 +39,31 @@ def test_load_zarr_xarray():
     )
 
     assert zarr_xr["tas"].any()
+
+
+def test_load_zarr_to_iris_via_ncdata():
+    """
+    Test loading a Zarr store from a https Object Store.
+
+    Same test as ``test_load_zarr_xarray`` only this time we are
+    passing the Xarray Dataset to ``ncdata``.
+
+    Test needs about 700MB max RES memory, takes 5.5-6s.
+    """
+    zarr_path = (
+        "https://uor-aces-o.s3-ext.jc.rl.ac.uk/"
+        "esmvaltool-zarr/um.PT1H.hp_z2.zarr"
+    )
+
+    zarr_xr = xr.open_dataset(
+        zarr_path,
+        consolidated=True,
+        use_cftime=True,
+        engine="zarr",
+        backend_kwargs={},
+    )
+
+    conversion_func = ncdata.iris_xarray.cubes_from_xarray
+    cubes = conversion_func(zarr_xr)
+
+    assert isinstance(cubes, iris.cube.CubeList)
