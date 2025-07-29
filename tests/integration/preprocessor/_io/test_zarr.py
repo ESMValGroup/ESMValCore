b1fe4b8561e7fd87be40e2968cba813b4cfeaab3
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-07-29 16:54:52 +0100

    add zar3 test and fixed deprecated call with cftime

--- tests/integration/preprocessor/_io/test_zarr.py
+++ tests/integration/preprocessor/_io/test_zarr.py
@@ -5,10 +5,13 @@ This is a dedicated test module for Zarr files IO; we have identified
 a number of issues with Zarr IO so it deserves its own test module.
 """
 
+import cf_units
 import iris
 import ncdata
 import pytest
 import xarray as xr
+
+from esmvalcore.preprocessor._io import load
 
 
 def test_load_zarr_xarray():
@@ -31,10 +34,11 @@ def test_load_zarr_xarray():
         "esmvaltool-zarr/um.PT1H.hp_z2.zarr"
     )
 
+    time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
     zarr_xr = xr.open_dataset(
         zarr_path,
         consolidated=True,
-        use_cftime=True,
+        decode_times=time_coder,
         engine="zarr",
         backend_kwargs={},
     )
@@ -67,10 +71,11 @@ def test_load_zarr_to_iris_via_ncdata_consolidated_false():
         "esmvaltool-zarr/um.PT1H.hp_z2.zarr"
     )
 
+    time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
     zarr_xr = xr.open_dataset(
         zarr_path,
         consolidated=False,
-        use_cftime=True,
+        decode_times=time_coder,
         engine="zarr",
         backend_kwargs={},
     )
@@ -108,10 +113,11 @@ def test_load_zarr_to_iris_via_ncdata_consolidated_true():
         "esmvaltool-zarr/um.PT1H.hp_z2.zarr"
     )
 
+    time_coder = xr.coders.CFDatetimeCoder(use_cftime=True)
     zarr_xr = xr.open_dataset(
         zarr_path,
         consolidated=True,
-        use_cftime=True,
+        decode_times=time_coder,
         engine="zarr",
         backend_kwargs={},
     )
@@ -121,3 +127,35 @@ def test_load_zarr_to_iris_via_ncdata_consolidated_true():
 
     assert isinstance(cubes, iris.cube.CubeList)
     assert len(cubes) == 24
+
+
+def test_load_zarr_3():
+    """
+    Test loading a Zarr3 store from a https Object Store.
+
+    We have a permanent bucket: esmvaltool-zarr at CEDA's object store
+    "url": "https://uor-aces-o.s3-ext.jc.rl.ac.uk",
+    where will host a number of test files like this one.
+    """
+    zarr_path = (
+        "https://uor-aces-o.s3-ext.jc.rl.ac.uk/"
+        "esmvaltool-zarr/example_field_0.zarr3"
+    )
+
+    # with "dummy" storage options
+    cubes = load(
+        zarr_path,
+        ignore_warnings=None,
+        backend_kwargs={"storage_options": {}},
+    )
+
+    assert len(cubes) == 1
+    cube = cubes[0]
+    assert cube.var_name == "q"
+    assert cube.standard_name == "specific_humidity"
+    assert cube.long_name is None
+    assert cube.units == cf_units.Unit("1")
+    coords = cube.coords()
+    coord_names = [coord.standard_name for coord in coords]
+    assert "longitude" in coord_names
+    assert "latitude" in coord_names
