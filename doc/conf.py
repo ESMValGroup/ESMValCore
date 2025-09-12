42baf1a99095b32156d57812c95f6a4fd79c5ead
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-09-12 13:37:45 +0100

    replace nbsphinx with myst-nb and remove pin on ipython

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ if rtd_version not in ["latest", "stable", "doc"]:
 # ones.
 extensions = [
     'autodocsumm',
-    'nbsphinx',
+    'myst-nb',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.extlinks',
