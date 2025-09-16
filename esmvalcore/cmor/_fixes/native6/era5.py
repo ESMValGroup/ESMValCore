7b92edee110f3a4dc0b4bb603c9bf6ca807b116f
Felicity Chun <felicity.chun@anu.edu.au>
2025-09-16 14:36:03 +1000

    update get time frequency

--- esmvalcore/cmor/_fixes/native6/era5.py
+++ esmvalcore/cmor/_fixes/native6/era5.py
@@ -16,6 +16,7 @@ from esmvalcore.iris_helpers import (
     safe_convert_units,
 )
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -27,22 +28,21 @@ def get_frequency(cube):
         return "fx"
 
     time.convert_units("days since 1850-1-1 00:00:00.0")
-
-    if len(time.points) == 1 and cube.long_name == "Geopotential":
+    if len(time.points) == 1:
+        acceptable_long_names = (
+            "Geopotential",
+            "Percentage of the Grid Cell Occupied by Land (Including Lakes)",
+        )
+        if cube.long_name not in acceptable_long_names:
+            return "monthly"
         return "fx"
 
-    if len(time.points) > 1:
-        interval = time.points[1] - time.points[0]
-        if interval - 1 / 24 < 1e-4:
-            return "hourly"
-    else:
-        logger.warning(
-            "Unable to infer frequency of cube "
-            "with length 1 time dimension: %s,"
-            "assuming 'monthly' frequency",
-            str(cube),
-        )
+    interval = time.points[1] - time.points[0]
 
+    if interval - 1 / 24 < 1e-4:
+        return "hourly"
+    if interval - 1.0 < 1e-4:
+        return "daily"
     return "monthly"
 
 
