d81ce6b78e71bf1bcb912123aa84ac41589bb4f9
Jan-Hendrik Malles <b382555@levante0.lvt.dkrz.de>
2025-11-28 13:59:55 +0100

    Make pre-commit run

--- esmvalcore/cmor/_fixes/native6/oras5.py
+++ esmvalcore/cmor/_fixes/native6/oras5.py
@@ -1,6 +1,9 @@
 """On-the-fly CMORizer for ORAS5."""
 
+from __future__ import annotations
+
 import logging
+from typing import TYPE_CHECKING
 
 import dask.array as da
 import iris
@@ -8,18 +11,196 @@ import numpy as np
 from iris import Constraint
 from iris.coords import DimCoord
 from iris.cube import CubeList
+from iris.mesh import Connectivity, MeshXY
 
-from ..icon.icon import AllVars as AllVars_ICON
-from ..shared import fix_ocean_depth_coord
-from ._base_fixes import Oras5Fix
+from esmvalcore.cmor._fixes.icon.icon import AllVars as AllVars_ICON
+from esmvalcore.cmor._fixes.icon.icon import IconFix
+from esmvalcore.cmor._fixes.shared import fix_ocean_depth_coord
+
+if TYPE_CHECKING:
+    from iris.cube import Cube
 
 logger = logging.getLogger(__name__)
+
+
+class Oras5Fix(IconFix):
+    """Base class for all ORAS5 fixes."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize ORAS5 fix."""
+        super().__init__(*args, **kwargs)
+        self._horizontal_grids: dict[str, CubeList] = {}
+        self._meshes: dict[str, MeshXY] = {}
+
+    def _create_mesh(self, cube: Cube) -> MeshXY:
+        """Create mesh from horizontal grid file."""
+        # Get coordinates
+        face_lon = cube.coord("longitude")
+        face_lat = cube.coord("latitude")
+        node_lon = cube.coord("longitude").bounds.T.flatten()
+        node_lat = cube.coord("latitude").bounds.T.flatten()
+
+        # Make the node locations a 2D array
+        nodes_flat = np.stack([node_lon, node_lat], axis=1)
+
+        # Find the unique nodes to be able to associate them with the faces
+        # Unfortunately, dask does not support the axis parameter...
+        nodes_unique, indices = np.unique(
+            nodes_flat,
+            return_inverse=True,
+            axis=0,
+        )
+
+        # Get the unique nodes as dask arrays
+        node_lon = da.from_array(nodes_unique[:, 0])
+        node_lat = da.from_array(nodes_unique[:, 1])
+
+        # Get dimensions (N_faces x M_nodes)
+        n_faces = len(face_lat.core_points())
+        n_nodes = int(len(indices) / n_faces)
+
+        # Reshape indices to N_faces x M_nodes dask array
+        indices = da.reshape(da.from_array(indices), (n_nodes, n_faces)).T
+
+        # Create the necessary mask
+        mask = da.full(da.shape(indices), False)
+
+        # Define the connectivity
+        connectivity = Connectivity(
+            indices=da.ma.masked_array(indices, mask=mask),
+            cf_role="face_node_connectivity",
+            start_index=0,
+            location_axis=0,
+        )
+
+        # Put everything together to get a U-Grid style mesh
+        node_lat = iris.coords.AuxCoord(
+            node_lat,
+            standard_name="latitude",
+            var_name="nlat",
+            long_name="node latitude",
+            units="degrees",
+        )
+        node_lon = iris.coords.AuxCoord(
+            node_lon,
+            standard_name="longitude",
+            var_name="nlon",
+            long_name="node longitude",
+            units="degrees",
+        )
+
+        return MeshXY(
+            topology_dimension=2,
+            node_coords_and_axes=[(node_lat, "y"), (node_lon, "x")],
+            connectivities=[connectivity],
+            face_coords_and_axes=[(face_lat, "y"), (face_lon, "x")],
+        )
+
+    def get_horizontal_grid(self, cube: Cube) -> CubeList:
+        """Get copy of ORAS5 horizontal grid.
+
+        If given, retrieve grid from `horizontal_grid` facet specified by the
+        user.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+            Cube for which the ORS5 horizontal grid is retrieved. If the facet
+            `horizontal_grid` is not specified by the user, it raises a
+            NotImplementedError.
+
+        Returns
+        -------
+        iris.cube.CubeList
+            Copy of ORAS5 horizontal grid.
+
+        Raises
+        ------
+        FileNotFoundError
+            Path specified by `horizontal_grid` facet (absolute or relative to
+            `auxiliary_data_dir`) does not exist.
+        NotImplementedError
+            No `horizontal_grid` facet is defined.
+
+        """
+        if self.extra_facets.get("horizontal_grid") is not None:
+            grid = self._get_grid_from_facet()
+        else:
+            msg = (
+                f"Full path to suitable ORAS5 grid must be specified in facet "
+                f"'horizontal_grid' for cube: {cube}"
+            )
+            raise NotImplementedError(
+                msg,
+            )
+
+        return grid
+
+    def _get_grid_from_facet(self) -> CubeList:
+        """Get horizontal grid from user-defined facet `horizontal_grid`."""
+        grid_path = self._get_path_from_facet(
+            "horizontal_grid",
+            "Horizontal grid file",
+        )
+        grid_name = grid_path.name
+
+        # If already loaded, return the horizontal grid
+        if grid_name in self._horizontal_grids:
+            return self._horizontal_grids[grid_name]
+
+        # Load file
+        self._horizontal_grids[grid_name] = self._load_cubes(grid_path)
+        logger.debug("Loaded ORAS5 grid file from %s", grid_path)
+        return self._horizontal_grids[grid_name]
+
+    def get_mesh(self, cube: Cube) -> MeshXY:
+        """Get mesh.
+
+        Note
+        ----
+        If possible, this function uses a cached version of the mesh to save
+        time.
+
+        Parameters
+        ----------
+        cube: iris.cube.Cube
+            Cube for which the mesh is retrieved.
+
+        Returns
+        -------
+        iris.mesh.MeshXY
+            Mesh of the cube.
+
+        Raises
+        ------
+        FileNotFoundError
+            Path specified by `horizontal_grid` facet (absolute or relative to
+            `auxiliary_data_dir`) does not exist.
+        NotImplementedError
+            No `horizontal_grid` facet is defined.
+
+        """
+        # Use `horizontal_grid` facet to determine grid name
+        grid_path = self._get_path_from_facet(
+            "horizontal_grid",
+            "Horizontal grid file",
+        )
+        grid_name = grid_path.name
+
+        # Reuse mesh if possible
+        if grid_name in self._meshes:
+            logger.debug("Reusing ORAS5 mesh for grid %s", grid_name)
+        else:
+            logger.debug("Creating ORAS5 mesh for grid %s", grid_name)
+            self._meshes[grid_name] = self._create_mesh(cube)
+
+        return self._meshes[grid_name]
 
 
 class AllVars(Oras5Fix, AllVars_ICON):
     """Fixes for all variables."""
 
-    def fix_metadata(self, cubes):
+    def fix_metadata(self, cubes: CubeList) -> CubeList:
         """Fix metadata."""
         cubes = self.add_additional_cubes(cubes)
         cube = self.get_cube(cubes)
@@ -54,7 +235,7 @@ class AllVars(Oras5Fix, AllVars_ICON):
 
         return CubeList([cube])
 
-    def _fix_cube(self, cube):
+    def _fix_cube(self, cube: Cube) -> Cube:
         """Remove redundant cells and predetermine how to handle grid."""
         # Remove redundant cells
         cube = cube[..., :-1, 1:-1]
@@ -96,7 +277,7 @@ class AllVars(Oras5Fix, AllVars_ICON):
         data = np.reshape(data, dim_shape + data_shape)
         return iris.cube.Cube(data, dim_coords_and_dims=coords_add)
 
-    def _add_coord_from_grid_file(self, cube, coord_name):
+    def _add_coord_from_grid_file(self, cube: Cube, coord_name: str) -> None:
         """Add coordinate from grid file to cube.
 
         Note.
@@ -143,12 +324,15 @@ class AllVars(Oras5Fix, AllVars_ICON):
         # Find index of mesh dimension (= single unnamed dimension)
         n_unnamed_dimensions = cube.ndim - len(cube.dim_coords)
         if n_unnamed_dimensions != 1:
-            raise ValueError(
+            msg = (
                 f"Cannot determine coordinate dimension for coordinate "
                 f"'{coord_name}', cube does not contain a single unnamed "
                 f"dimension:\n{cube}"
             )
-        coord_dims = ()
+            raise ValueError(
+                msg,
+            )
+        coord_dims: tuple[()] | tuple[int] = ()
         for idx in range(cube.ndim):
             if not cube.coords(dimensions=idx, dim_coords=True):
                 coord_dims = (idx,)
@@ -160,7 +344,7 @@ class AllVars(Oras5Fix, AllVars_ICON):
         coord.long_name = coord_name
         cube.add_aux_coord(coord, coord_dims)
 
-    def _fix_lat(self, cube):
+    def _fix_lat(self, cube: Cube) -> tuple[int, ...]:
         """Fix latitude coordinate of cube."""
         lat_name = self.extra_facets.get("latitude", "latitude")
 
@@ -177,7 +361,7 @@ class AllVars(Oras5Fix, AllVars_ICON):
 
         return cube.coord_dims(lat)
 
-    def _fix_lon(self, cube):
+    def _fix_lon(self, cube: Cube) -> tuple[int, ...]:
         """Fix longitude coordinate of cube."""
         lon_name = self.extra_facets.get("longitude", "longitude")
 
@@ -195,7 +379,7 @@ class AllVars(Oras5Fix, AllVars_ICON):
 
         return cube.coord_dims(lon)
 
-    def _fix_time(self, cube, cubes):
+    def _fix_time(self, cube: Cube, cubes: CubeList) -> Cube:
         """Fix time coordinate of cube."""
         # Add time coordinate if not already present
         if not cube.coords("time"):
