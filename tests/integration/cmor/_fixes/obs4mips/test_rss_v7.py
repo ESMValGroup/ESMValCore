7c20c3d8556cc862029aa789b445135e470a5beb
Bouwe Andela <b.andela@esciencecenter.nl>
2026-02-09 09:30:41 +0100

    Rename obs4MIPs dataset SSMI RSSv07r00 to RSS-v7

---
+++ tests/integration/cmor/_fixes/obs4mips/test_rss_v7.py
@@ -0,0 +1,18 @@
+"""Test SSMI fixes."""
+
+import unittest
+
+from esmvalcore.cmor._fixes.fix import GenericFix
+from esmvalcore.cmor._fixes.obs4mips.rss_v7 import Prw
+from esmvalcore.cmor.fix import Fix
+
+
+class TestPrw(unittest.TestCase):
+    """Test prw fixes."""
+
+    def test_get(self):
+        """Test fix get."""
+        self.assertListEqual(
+            Fix.get_fixes("obs4MIPs", "RSS-v7", "Amon", "prw"),
+            [Prw(None), GenericFix(None)],
+        )
