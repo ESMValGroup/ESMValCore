2664c3f21f6a40bdd1c98bf4a988e4d97ffa2088
Bouwe Andela <b.andela@esciencecenter.nl>
2026-05-20 17:10:54 +0200

    Update tests

--- tests/integration/cmor/_fixes/test_fix.py
+++ tests/integration/cmor/_fixes/test_fix.py
@@ -10,10 +10,14 @@ from esmvalcore.cmor._fixes.cmip5.bnu_esm import Ch4
 from esmvalcore.cmor._fixes.cmip5.canesm2 import FgCo2
 from esmvalcore.cmor._fixes.cmip5.cesm1_bgc import Gpp
 from esmvalcore.cmor._fixes.cmip6.cesm2 import Omon, Tos
-from esmvalcore.cmor._fixes.cordex.cnrm_cerfacs_cnrm_cm5.aladin63 import (
-    Tas,
+from esmvalcore.cmor._fixes.cordex.cnrm_cerfacs_cnrm_cm5.aladin53 import (
+    AllVars as Aladin53AllVars,
 )
-from esmvalcore.cmor._fixes.cordex.cordex_fixes import AllVars
+from esmvalcore.cmor._fixes.cordex.cnrm_cerfacs_cnrm_cm5.aladin63 import (
+    AllVars as Aladin63AllVars,
+)
+from esmvalcore.cmor._fixes.cordex.cnrm_cerfacs_cnrm_cm5.aladin63 import Tas
+from esmvalcore.cmor._fixes.cordex.cordex_fixes import AllVars as CordexAllVars
 from esmvalcore.cmor._fixes.fix import GenericFix
 from esmvalcore.cmor.fix import Fix
 from esmvalcore.cmor.table import get_var_info
@@ -42,7 +46,12 @@ def test_get_fix_cordex():
         "tas",
         extra_facets={"driver": "CNRM-CERFACS-CNRM-CM5"},
     )
-    assert fix == [Tas(None), AllVars(None), GenericFix(None)]
+    assert fix == [
+        Tas(None),
+        Aladin63AllVars(None),
+        CordexAllVars(None),
+        GenericFix(None),
+    ]
 
 
 def test_get_grid_fix_cordex():
@@ -53,7 +62,11 @@ def test_get_grid_fix_cordex():
         "tas",
         extra_facets={"driver": "CNRM-CERFACS-CNRM-CM5"},
     )
-    assert fix == [AllVars(None), GenericFix(None)]
+    assert fix == [
+        Aladin53AllVars(None),
+        CordexAllVars(None),
+        GenericFix(None),
+    ]
 
 
 def test_get_fixes_with_replace():
