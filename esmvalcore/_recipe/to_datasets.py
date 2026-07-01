1711bb88cc1323d984e654f10adf406063a763db
Bouwe Andela <b.andela@esciencecenter.nl>
2026-07-01 20:50:36 +0200

    Add more facets to alias

--- esmvalcore/_recipe/to_datasets.py
+++ esmvalcore/_recipe/to_datasets.py
@@ -33,12 +33,19 @@ logger = logging.getLogger(__name__)
 
 _ALIAS_INFO_KEYS: tuple[str, ...] = (
     "project",
-    "activity",
-    "driver",
+    "mip",
+    "short_name",
+    "branding_suffix",
     "dataset",
+    "rcm_version",
+    "driver",
+    "ensemble",
     "exp",
     "sub_experiment",
-    "ensemble",
+    "frequency",
+    "domain",
+    "region",
+    "grid",
     "version",
 )
 """List of keys to be used to compose the alias, ordered by priority."""
