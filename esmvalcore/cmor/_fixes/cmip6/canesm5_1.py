1ed57ccd1f8682560f0ba81825b0ef9d9a457d46
rkw001 <kristi.webb@ec.gc.ca>
2026-07-13 22:52:02 +0000

    Add imports for CanESM5-1 fix file

--- esmvalcore/cmor/_fixes/cmip6/canesm5_1.py
+++ esmvalcore/cmor/_fixes/cmip6/canesm5_1.py
@@ -1,1 +1,27 @@
 """Fixes for CanESM5-1 model."""
+
+from esmvalcore.cmor._fixes.cmip6.canesm5 import Cl, Cli, Clw, Co2, Gpp, Ps
+
+
+class Cl(Cl):
+    """Fixes for cl."""
+
+
+class Cli(Cli):
+    """Fixes for cli."""
+
+
+class Clw(Clw):
+    """Fixes for clw."""
+
+
+class Ps(Ps):
+    """Fixes for ps."""
+
+
+class Co2(Co2):
+    """Fixes for co2."""
+
+
+class Gpp(Gpp):
+    """Fixes for gpp."""
