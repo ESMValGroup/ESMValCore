5cf8234249393446a0ca156703167f94459c19ad
Britta Grusdt <bgrusdt@gmail.com>
2026-07-23 08:58:24 +0200

    squeeze length-1 dimension awi-esm-1-1-lr sea ice variables

--- esmvalcore/cmor/_fixes/cmip6/awi_esm_1_1_lr.py
+++ esmvalcore/cmor/_fixes/cmip6/awi_esm_1_1_lr.py
@@ -30,3 +30,39 @@ class AllVars(Fix):
             except AttributeError:
                 pass
         return cubes
+
+
+class FesomSeaIceScalar(Fix):
+    """Shared fix for FESOM sea-ice hemispheric scalar diagnostics.
+
+    AWI-ESM's sea-ice/ocean grid (FESOM) has a spurious length-1 'nodes' dimension that isn't part of the CMOR spec
+    (a scalar hemispheric integral over time only).
+    """
+
+    def fix_metadata(self, cubes):
+        """Squeeze out the spurious length-1 dimension named nodes."""
+        fixed_cubes = []
+        for cube in cubes:
+            if cube.coords("nodes"):
+                indices = [slice(None)] * cube.ndim
+                indices[cube.coord_dims("nodes")[0]] = 0
+                fixed_cubes.append(cube[tuple(indices)])
+            else:
+                fixed_cubes.append(cube)
+        return fixed_cubes
+
+
+class Siextentn(FesomSeaIceScalar):
+    """Fixes for siextentn."""
+
+
+class Siextents(FesomSeaIceScalar):
+    """Fixes for siextents."""
+
+
+class Siarean(FesomSeaIceScalar):
+    """Fixes for siarean."""
+
+
+class Siareas(FesomSeaIceScalar):
+    """Fixes for siareas."""
--- tests/integration/cmor/_fixes/cmip6/test_awi_esm_1_1_lr.py
+++ tests/integration/cmor/_fixes/cmip6/test_awi_esm_1_1_lr.py
@@ -1,11 +1,17 @@
 """Tests for the fixes of AWI-ESM-1-1-LR."""
 
 import iris
+import numpy as np
 import pytest
 
-from esmvalcore.cmor._fixes.cmip6.awi_esm_1_1_lr import AllVars
+from esmvalcore.cmor._fixes.cmip6.awi_esm_1_1_lr import (
+    AllVars,
+    FesomSeaIceScalar,
+)
 from esmvalcore.cmor._fixes.fix import GenericFix
 from esmvalcore.cmor.fix import Fix
+
+SEA_ICE_SCALAR_VARS = ["siextentn", "siextents", "siarean", "siareas"]
 
 
 @pytest.fixture
@@ -15,9 +21,39 @@ def sample_cubes():
     return iris.cube.CubeList([ta_cube, tas_cube])
 
 
+@pytest.fixture(params=SEA_ICE_SCALAR_VARS)
+def sea_ice_scalar_cubes(request):
+    var_name = request.param
+    time_coord = iris.coords.DimCoord(
+        np.arange(3),  # random small number of timesteps
+        standard_name="time",
+    )
+    cube_ok = iris.cube.Cube(
+        np.ones((3,)),
+        var_name=var_name,
+        dim_coords_and_dims=[(time_coord, 0)],
+    )
+    nodes_coord = iris.coords.DimCoord(
+        np.arange(1),
+        var_name="nodes",
+    )
+    cube_bad = iris.cube.Cube(
+        np.ones((1, 3)),
+        var_name=var_name,
+        dim_coords_and_dims=[(time_coord, 1), (nodes_coord, 0)],
+    )
+    return iris.cube.CubeList([cube_ok, cube_bad])
+
+
 def test_get_tas_fix():
     fix = Fix.get_fixes("CMIP6", "AWI-ESM-1-1-LR", "Amon", "tas")
     assert fix == [AllVars(None), GenericFix(None)]
+
+
+@pytest.mark.parametrize("short_name", SEA_ICE_SCALAR_VARS)
+def test_get_sea_ice_scalar_fixes(short_name):
+    fixes = Fix.get_fixes("CMIP6", "AWI-ESM-1-1-LR", "SImon", short_name)
+    assert fixes == [FesomSeaIceScalar(None), AllVars(None), GenericFix(None)]
 
 
 def test_allvars_fix_metadata(sample_cubes):
@@ -42,3 +78,9 @@ def test_allvars_no_need_tofix_metadata(sample_cubes):
             cube.attributes["parent_time_units"]
             == "days since 0001-01-01 00:00:00"
         )
+
+
+def test_sea_ice_scalar_fix_metadata(sea_ice_scalar_cubes):
+    out_cubes = FesomSeaIceScalar(None).fix_metadata(sea_ice_scalar_cubes)
+    for cube in out_cubes:
+        assert cube.ndim == 1
