3932c2dc3b1aae9e3c0b2e779cc6311ad4b3d86d
Manuel Schlund <manuel.schlund@dlr.de>
2026-08-26 12:26:29 +0200

    Remove DataSource class

--- tests/unit/io/local/test_get_data_sources.py
+++ tests/unit/io/local/test_get_data_sources.py
@@ -9,7 +9,7 @@ import esmvalcore
 import esmvalcore.cmor.table
 from esmvalcore.config import CFG
 from esmvalcore.io.local import LocalDataSource
-from esmvalcore.local import DataSource, _get_data_sources
+from esmvalcore.local import _get_data_sources
 
 if TYPE_CHECKING:
     import pytest_mock
@@ -70,25 +70,3 @@ def test_get_data_sources_nodefault(monkeypatch):
     )
     with pytest.raises(KeyError):
         _get_data_sources("CMIP6")
-
-
-def test_data_source_deprecated(mocker: pytest_mock.MockerFixture) -> None:
-    """Test that DataSource is deprecated."""
-    mocker.patch.object(DataSource, "_path2facets")
-    mocker.patch.object(DataSource, "find_data")
-    with pytest.deprecated_call():
-        data_source = DataSource(
-            name="test",
-            project="CMIP6",
-            priority=1,
-            rootpath=Path("/climate_data"),
-            dirname_template="/",
-            filename_template="*.nc",
-        )
-
-    assert data_source.regex_pattern
-    assert data_source.get_glob_patterns() == [Path("/climate_data/*.nc")]
-    data_source.path2facets(Path("/climate_data/some_file.nc"), False)
-    data_source._path2facets.assert_called()  # type: ignore[attr-defined]
-    data_source.find_files(dataset="a")
-    data_source.find_data.assert_called()  # type: ignore[attr-defined]
